975979abeb61149b20a658ab12bf107d33cd4806
salviorodrigo <rodrigosalvio@hotmail.com>
2024-11-07 14:46:28 -0400

    feat: add Result object

---
+++ src/Common/Result.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+namespace BradiNfeApi\Common;
+
+class Result {
+    private bool $success;
+    private mixed $data;
+    private string $error;
+
+    private function __construct(bool $success, mixed $data = null, string $error = 'Result is not an error.') {
+        $this->success = $success;
+        $this->data = $data;
+        $this->error = $error;
+    }
+
+    public function isSuccess(): bool {
+        return $this->success;
+    }
+
+    public function getData(): mixed {
+        if (!$this->isSuccess()) {
+            throw new Exception('Result is an error.');
+        };
+        return $this->data;
+    }
+
+    public function getError(): Exception {
+        if ($this->isSuccess()) {
+            throw new Exception($this->error);
+        };
+        return $this->error;
+    }
+
+    public static function makeSuccess(mixed $value): self {
+        return new self(true, $value);
+    }
+}
