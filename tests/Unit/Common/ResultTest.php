92751a2fd66f04ba122b30f49dd0e6d6802a9723
salviorodrigo <rodrigosalvio@hotmail.com>
2024-11-16 19:44:42 -0400

    refactor: improve code readability

--- tests/Unit/Common/ResultTest.php
+++ tests/Unit/Common/ResultTest.php
@@ -4,9 +4,9 @@ use BradiNfeApi\Common\Result;
 describe('Result', function () {
     describe('.makeSuccess()', function () {
         test('Should be return a Result with a value on success', function () {
-            $successResult = Result::makeSuccess('validValue');
-            expect($successResult->isSuccess())->toBeTruthy();
-            expect($successResult->getData())->toBe('validValue');
+            $sut = Result::makeSuccess('validValue');
+            expect($sut->isSuccess())->toBeTruthy();
+            expect($sut->getData())->toBe('validValue');
         });
     });
  });
