1b1208268547c0ae8a5266566c75c4ba38ce6e6d
salviorodrigo <rodrigosalvio@hotmail.com>
2024-11-16 20:31:05 -0400

    test: ensure Result.getError throws if Result is a success

--- tests/Unit/Common/ResultTest.php
+++ tests/Unit/Common/ResultTest.php
@@ -8,5 +8,11 @@ describe('Result', function () {
             expect($sut->isSuccess())->toBeTruthy();
             expect($sut->getData())->toBe('validValue');
         });
+
+        test('Should throw an error if user try get an error when Result is a success', function () {
+            $sut = Result::makeSuccess('validValue');
+            expect($sut->isSuccess())->toBeTruthy();
+            $sut->getError();
+        })->throws(Exception::class, 'Result is not an error.');
     });
  });
