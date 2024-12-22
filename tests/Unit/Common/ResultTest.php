7844bb13ea24c34af46767e354b68b2703976a39
salviorodrigo <rodrigosalvio@hotmail.com>
2024-12-22 12:13:20 -0400

    test: ensure `Result.makeSuccess()` does not accept `ApiError`

--- tests/Unit/Common/ResultTest.php
+++ tests/Unit/Common/ResultTest.php
@@ -12,17 +12,21 @@ describe('Result', function () {
             expect($sut->getData())->toBe('validValue');
         });
 
+        test('Should be return a Result without value when nothing is provided', function () {
+            $sut = Result::makeSuccess();
+            expect($sut->isSuccess())->toBeTruthy();
+            expect($sut->getData())->toBe(null);
+        });
+
         test('Should throw an error if user try get an error when Result is a success', function () {
             $sut = Result::makeSuccess('validValue');
             expect($sut->isSuccess())->toBeTruthy();
             $sut->getError();
         })->throws(Exception::class, 'Result is not an error.');
 
-        test('Should be return a Result without value when nothing is provided', function () {
-            $sut = Result::makeSuccess();
-            expect($sut->isSuccess())->toBeTruthy();
-            expect($sut->getData())->toBe(null);
-        });
+        test('Should throw an error if user try make a success with ApiError', function () {
+            Result::makeSuccess(new GenericApiError);
+        })->throws(Exception::class, 'This method doesn\'t accept ApiError\'s.');
     });
 
     describe('.makeFailure()', function () {
