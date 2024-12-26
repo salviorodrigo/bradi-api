6cfc6bb085ff0e64b884d87606829afc104eda5c
salviorodrigo <rodrigosalvio@hotmail.com>
2024-12-25 23:26:42 -0400

    test: ensure `ValidationError` does not contain duplicated validator error messages

--- tests/Unit/Common/Exceptions/ValidationErrorTest.php
+++ tests/Unit/Common/Exceptions/ValidationErrorTest.php
@@ -22,4 +22,14 @@ describe('ValidationError', function () {
         expect($sut->details['message']['fieldTwo'])->toBe(['A error message']);
         expect($sut->details['message']['fieldThree'])->toBe(['A error message', 'A second error message']);
     });
+
+    test('Should be return a ValidationError without duplicated ValidatorError messages', function () {
+        $sut = new ValidationError([
+            new FakeValidatorError('fieldOne', 'A error message'),
+            new FakeValidatorError('fieldOne', 'A error message'),
+        ]);
+        expect($sut->httpStatus)->toBe('400');
+        expect($sut->details['message'])->toBeArray();
+        expect($sut->details['message']['fieldOne'])->toBe(['A error message']);
+    });
 });
