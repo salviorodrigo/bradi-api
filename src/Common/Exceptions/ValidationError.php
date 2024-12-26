36f4aab80ad5d5bb3b41d3a1507377c0ac20c11d
salviorodrigo <rodrigosalvio@hotmail.com>
2024-12-25 23:18:38 -0400

    feat: add `ValidationError`

---
+++ src/Common/Exceptions/ValidationError.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BradiNfeApi\Common\Exceptions;
+
+use BradiNfeApi\Common\ApiError;
+use BradiNfeApi\Domain\Common\Protocols\ValidatorError;
+use InvalidArgumentException;
+
+class ValidationError extends ApiError
+{
+    public string $httpStatus = '400';
+    public string $title = 'Bad Request';
+    public array $details = ['message' => []];
+
+    public function __construct(array $validatorErrors)
+    {
+        foreach ($validatorErrors as $validatorError) {
+            if (! is_a($validatorError, ValidatorError::class)) {
+                throw new InvalidArgumentException('Validation error just accepts Validator errors.');
+            }
+            if (! array_key_exists($validatorError->fieldName, $this->details['message'])) {
+                $this->details['message'][$validatorError->fieldName] = [];
+            }
+            if (! in_array($validatorError->getMessage(), $this->details['message'][$validatorError->fieldName])) {
+                $this->details['message'][$validatorError->fieldName] = [...$this->details['message'][$validatorError->fieldName], $validatorError->getMessage()];
+            }
+        }
+    }
+}
