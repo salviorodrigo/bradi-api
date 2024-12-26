269269b9adeace281b1e677aa63331fe6ce1fc42
salviorodrigo <rodrigosalvio@hotmail.com>
2024-12-25 23:17:53 -0400

    fix: implements `Exception` constructor corretly

--- src/Common/ApiError.php
+++ src/Common/ApiError.php
@@ -5,10 +5,16 @@ declare(strict_types=1);
 namespace BradiNfeApi\Common;
 
 use Exception;
+use Throwable;
 
 abstract class ApiError extends Exception
 {
     public string $httpStatus;
     public string $title;
     public array $details;
+
+    public function __construct(string $message = 'This is an ApiError', int $code = 0, ?Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
 }
