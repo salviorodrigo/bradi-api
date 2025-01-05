02c5c9bc9400fd2c8b0d77b68adfd89744a48be4
salviorodrigo <rodrigosalvio@hotmail.com>
2025-01-05 14:12:32 -0400

    test: ensure `ValidationService.construct()` throws if a non `Validator` is provided

--- tests/Unit/Domain/Common/Services/ValidationServiceTest.php
+++ tests/Unit/Domain/Common/Services/ValidationServiceTest.php
@@ -9,5 +9,9 @@ describe('ValidationService', function () {
         test('Should throw if validators is not provided', function () {
             new ValidationService([]);
         })->throws(InvalidArgumentException::class, 'Validators must be provided.');
+
+        test('Should throw if an array with one ou more value that does not implement Validator interface is provided', function () {
+            new ValidationService([new \stdClass]);
+        })->throws(InvalidArgumentException::class, 'Concrete validators must implement IValidator interface.');
     });
 });
