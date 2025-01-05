24ed5c90951b92b97b67041cec87c832515ac62b
salviorodrigo <rodrigosalvio@hotmail.com>
2025-01-05 19:34:03 -0400

    feat: add `ValueObject` protocol

---
+++ src/Domain/Common/Protocols/ValueObject.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BradiNfeApi\Domain\Common\Protocols;
+
+use BradiNfeApi\Common\Result;
+
+abstract class ValueObject
+{
+    public readonly mixed $value;
+
+    abstract public static function parse(mixed $rawData): Result;
+}
