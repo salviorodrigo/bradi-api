23b06f525188f38310cd94c0c958137827c1982e
salviorodrigo <rodrigosalvio@hotmail.com>
2025-01-05 14:15:13 -0400

    test: ensure `ValidationService.construct()` create a `ValidationService` class on succed

--- tests/Unit/Domain/Common/Services/ValidationServiceTest.php
+++ tests/Unit/Domain/Common/Services/ValidationServiceTest.php
@@ -3,9 +3,15 @@
 declare(strict_types=1);
 
 use BradiNfeApi\Domain\Common\Services\ValidationService;
+use BradiNfeApi\Tests\Doubles\Domain\Common\Validators\FakeValidator;
 
 describe('ValidationService', function () {
     describe('.construct()', function () {
+        test('Should be create a ValidationService on success', function () {
+            $sut = new ValidationService([new FakeValidator('fieldName')]);
+            expect($sut)->toBeInstanceOf(ValidationService::class);
+        });
+
         test('Should throw if validators is not provided', function () {
             new ValidationService([]);
         })->throws(InvalidArgumentException::class, 'Validators must be provided.');
