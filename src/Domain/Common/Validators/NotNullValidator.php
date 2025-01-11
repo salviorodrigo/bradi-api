a8126f1e2824997c90af8a9ad1a5435ca78ee9d2
salviorodrigo <rodrigosalvio@hotmail.com>
2025-01-11 11:47:53 -0400

    feat: add `NotNullValidator`

---
+++ src/Domain/Common/Validators/NotNullValidator.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BradiNfeApi\Domain\Common\Validators;
+
+use BradiNfeApi\Common\Result;
+use BradiNfeApi\Domain\Common\Protocols\Validator;
+use BradiNfeApi\Domain\Common\Validators\Exceptions\IsNullError;
+
+final class NotNullValidator extends Validator
+{
+    public function __construct(public readonly string $fieldName) {}
+
+    public function validate(mixed $candidate): Result
+    {
+        if (empty($candidate)) {
+            return Result::makeFailure(new IsNullError($this->fieldName));
+        }
+
+        return Result::makeSuccess();
+    }
+}
