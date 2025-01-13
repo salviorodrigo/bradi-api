3458be7e267d1ee42dd855c9df24f66153fec6ae
salviorodrigo <rodrigosalvio@hotmail.com>
2025-01-12 20:59:14 -0400

    refactor: ensure `NotNullValidator.validate()` returns a success if a zero is provided

--- src/Domain/Common/Validators/NotNullValidator.php
+++ src/Domain/Common/Validators/NotNullValidator.php
@@ -14,7 +14,7 @@ final class NotNullValidator extends Validator
 
     public function validate(mixed $candidate): Result
     {
-        if (empty($candidate)) {
+        if (! isset($candidate) || $candidate == '' || $candidate == []) {
             return Result::makeFailure(new IsNullError($this->fieldName));
         }
 
