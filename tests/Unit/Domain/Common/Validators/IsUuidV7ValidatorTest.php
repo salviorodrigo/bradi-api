13c6a64c262b429b61de9c2fa5b7dbf86853b747
salviorodrigo <rodrigosalvio@hotmail.com>
2025-01-21 13:21:52 -0400

    test: ensure `IsUuidV7Validator.validate()` to be succeed if a valid UUID v7 string is provided

---
+++ tests/Unit/Domain/Common/Validators/IsUuidV7ValidatorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+use BradiNfeApi\Domain\Common\Validators\IsUuidV7Validator;
+
+describe('IsUuidV7Validator', function () {
+    describe('.validate()', function () {
+        test('Should be return a success Result if a valid uuid version 7 is provided', function () {
+            $sut = new IsUuidV7Validator('testField');
+            $sutResponse = $sut->validate('01947ac8-cc4d-7ebc-af74-85ab74ae80da');
+            expect($sutResponse->isSuccess())->toBeTruthy();
+            expect($sutResponse->hasValue())->toBeFalsy();
+        });
+    });
+});
