af10754a9e9c2272497ce03457eef08a67d84882
salviorodrigo <rodrigosalvio@hotmail.com>
2025-09-19 16:46:27 -0400

    feat: add CodigoUF value object

---
+++ src/Domain/Invoices/NFe/v4_00/ValueObjects/CodigoUF.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * MOC      7.0
+ * #        6
+ * ID       B02
+ * Campo    cUF
+ * Desc     Código da UF do emitente do Documento Fiscal
+ * Tam      2
+ * OBS:
+ * Utilizar a Tabela do IBGE de código de unidades da federação
+ * (Seção 8.1 do MOC – Visão Geral, Tabela de UF, Município e País).
+ */
+
+namespace BradiNfeApi\Domain\Invoices\NFe\v4_00\ValueObjects\GrupoB;
+
+use BradiNfeApi\Common\Result;
+use BradiNfeApi\Domain\Common\Services\ValidationService;
+use BradiNfeApi\Domain\Common\Validators\IsStringValidator;
+use BradiNfeApi\Domain\Common\Validators\NotNullValidator;
+use BradiNfeApi\Domain\Invoices\Protocols\DFeElement;
+
+final class CodigoUF extends DFeElement
+{
+    private function __construct(
+        public readonly string $xmlString,
+        public readonly bool $isAutoClosedTag = false)
+    {
+        $this->value = DFeElement::xmlParser()->getTagValue($xmlString, 'cUF');
+    }
+
+    public static function parseXmlString(mixed $rawData): Result
+    {
+        $validationService = new ValidationService([
+            new IsStringValidator('cUF'),
+            new NotNullValidator('cUF'),
+        ]);
+        $validationServiceResponse = $validationService->verify($rawData);
+        if ($validationServiceResponse->isSuccess()) {
+            return Result::makeSuccess(new CodigoUF($rawData));
+        }
+
+        return $validationServiceResponse;
+    }
+}
