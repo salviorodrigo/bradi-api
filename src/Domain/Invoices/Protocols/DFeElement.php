28ec64146f35542627ff701d59933a1943092d38
salviorodrigo <rodrigosalvio@hotmail.com>
2025-09-19 16:45:10 -0400

    feat: add DFeElement protocol

---
+++ src/Domain/Invoices/Protocols/DFeElement.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BradiNfeApi\Domain\Invoices\Protocols;
+
+use BradiNfeApi\Common\Result;
+use BradiNfeApi\Infra\Parses\XmlToDFeParser;
+
+abstract class DFeElement
+{
+    public readonly string $value;
+    public readonly string $xmlString;
+    public readonly bool $isAutoClosedTag;
+
+    public static function xmlParser(): DFeParser
+    {
+        return new XmlToDFeParser;
+    }
+
+    abstract public static function parseXmlString(mixed $rawData): Result;
+}
