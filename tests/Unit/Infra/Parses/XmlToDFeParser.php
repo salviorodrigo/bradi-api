b581629b6618659a0439c3dd40eb1be7fa1a48b1
salviorodrigo <rodrigosalvio@hotmail.com>
2025-09-19 16:43:54 -0400

    refactor: improve tests descriptions of XmlToDFeParser

--- tests/Unit/Infra/Parses/XmlToDFeParser.php
+++ tests/Unit/Infra/Parses/XmlToDFeParser.php
@@ -37,7 +37,7 @@ describe('XmlToDFeParser', function () {
                 ->toBe(['<infNFe Id="NFe11250702393780000102550020008692371804601060" versao="4.00"><det nItem="1"><prod><xProd>OLEO DIESEL B S10</xProd><uCom>L</uCom><qCom>622.0000</qCom><vUnCom>6.13000000</vUnCom><vProd>3812.86</vProd></prod></det><det nItem="2"><prod><xProd>ARLA 32 BALDE 20L</xProd><uCom>UN</uCom><qCom>2.0000</qCom><vUnCom>99.90000000</vUnCom><vProd>199.80</vProd></prod></det></infNFe>']);
         });
 
-        test('Should be return target tag on success, if exists most than one tag', function () {
+        test('Should be return all occurrences of target tag on success, if exists most than one tag', function () {
             $fakeXmlString = '<infNFe Id="NFe11250702393780000102550020008692371804601060" versao="4.00"><det nItem="1"><prod><xProd>OLEO DIESEL B S10</xProd><uCom>L</uCom><qCom>622.0000</qCom><vUnCom>6.13000000</vUnCom><vProd>3812.86</vProd></prod></det><det nItem="2"><prod><xProd>ARLA 32 BALDE 20L</xProd><uCom>UN</uCom><qCom>2.0000</qCom><vUnCom>99.90000000</vUnCom><vProd>199.80</vProd></prod></det></infNFe>';
             $sut = new XmlToDFeParser;
 
@@ -60,7 +60,7 @@ describe('XmlToDFeParser', function () {
                 ->toBe('<infNFe Id="NFe11250702393780000102550020008692371804601060" versao="4.00"><det nItem="1"><prod><xProd>OLEO DIESEL B S10</xProd><uCom>L</uCom><qCom>622.0000</qCom><vUnCom>6.13000000</vUnCom><vProd>3812.86</vProd></prod></det><det nItem="2"><prod><xProd>ARLA 32 BALDE 20L</xProd><uCom>UN</uCom><qCom>2.0000</qCom><vUnCom>99.90000000</vUnCom><vProd>199.80</vProd></prod></det></infNFe>');
         });
 
-        test('Should be return first occurrence of target tag on success', function () {
+        test('Should be return first occurrence of target tag on success, if exist most than one occurrence', function () {
             $fakeXmlString = '<infNFe Id="NFe11250702393780000102550020008692371804601060" versao="4.00"><det nItem="1"><prod><xProd>OLEO DIESEL B S10</xProd><uCom>L</uCom><qCom>622.0000</qCom><vUnCom>6.13000000</vUnCom><vProd>3812.86</vProd></prod></det><det nItem="2"><prod><xProd>ARLA 32 BALDE 20L</xProd><uCom>UN</uCom><qCom>2.0000</qCom><vUnCom>99.90000000</vUnCom><vProd>199.80</vProd></prod></det></infNFe>';
             $sut = new XmlToDFeParser;
 
