547db5d7f49cd22ca48b73639543747b2072991e
salviorodrigo <rodrigosalvio@hotmail.com>
2025-09-19 16:39:06 -0400

    test: ensure XmlToDFeParser.getTag return target tag on success

--- tests/Unit/Infra/Parses/XmlToDFeParser.php
+++ tests/Unit/Infra/Parses/XmlToDFeParser.php
@@ -49,4 +49,15 @@ describe('XmlToDFeParser', function () {
                 ]);
         });
     });
+
+    describe('.getTag()', function () {
+        test('Should be return target tag on success', function () {
+            $fakeXmlString = '<infNFe Id="NFe11250702393780000102550020008692371804601060" versao="4.00"><det nItem="1"><prod><xProd>OLEO DIESEL B S10</xProd><uCom>L</uCom><qCom>622.0000</qCom><vUnCom>6.13000000</vUnCom><vProd>3812.86</vProd></prod></det><det nItem="2"><prod><xProd>ARLA 32 BALDE 20L</xProd><uCom>UN</uCom><qCom>2.0000</qCom><vUnCom>99.90000000</vUnCom><vProd>199.80</vProd></prod></det></infNFe>';
+            $sut = new XmlToDFeParser;
+
+            expect($sut->getTag($fakeXmlString, 'infNFe'))
+                ->toBeString()
+                ->toBe('<infNFe Id="NFe11250702393780000102550020008692371804601060" versao="4.00"><det nItem="1"><prod><xProd>OLEO DIESEL B S10</xProd><uCom>L</uCom><qCom>622.0000</qCom><vUnCom>6.13000000</vUnCom><vProd>3812.86</vProd></prod></det><det nItem="2"><prod><xProd>ARLA 32 BALDE 20L</xProd><uCom>UN</uCom><qCom>2.0000</qCom><vUnCom>99.90000000</vUnCom><vProd>199.80</vProd></prod></det></infNFe>');
+        });
+    });
 });
