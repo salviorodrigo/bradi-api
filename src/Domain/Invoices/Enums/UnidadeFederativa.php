718508be522006b50a0143932b16ab73306a130a
salviorodrigo <rodrigosalvio@hotmail.com>
2025-12-04 00:28:11 -0400

    feat: add enum according the IBGE table of federative unit codes - Section 8.1 - of 02/2025

---
+++ src/Domain/Invoices/Enums/UnidadeFederativa.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BradiNfeApi\Domain\Invoices\Enums;
+
+enum UnidadeFederativa: string
+{
+    case RO = '11';
+    case AC = '12';
+    case AM = '13';
+    case RR = '14';
+    case PA = '15';
+    case AP = '16';
+    case TO = '17';
+    case MA = '21';
+    case PI = '22';
+    case CE = '23';
+    case RN = '24';
+    case PB = '25';
+    case PE = '26';
+    case AL = '27';
+    case SE = '28';
+    case BA = '29';
+    case MG = '31';
+    case ES = '32';
+    case RJ = '33';
+    case SP = '35';
+    case PR = '41';
+    case SC = '42';
+    case RS = '43';
+    case MS = '50';
+    case MT = '51';
+    case GO = '52';
+    case DF = '53';
+}
