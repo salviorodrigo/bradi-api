19206238474a55b505b04ea9e5fa23c3d57f2c98
salviorodrigo <rodrigosalvio@hotmail.com>
2025-12-04 01:19:18 -0400

    test: ensure CodigoNF.parserXmlString fails if a non empty string value is provided

--- tests/Unit/Domain/Invoices/NFe/v4_00/ValueObjects/CodigoNFTest.php
+++ tests/Unit/Domain/Invoices/NFe/v4_00/ValueObjects/CodigoNFTest.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use BradiNfeApi\Common\Exceptions\ValidationError;
 use BradiNfeApi\Common\Result;
 use BradiNfeApi\Domain\Invoices\NFe\v4_00\ValueObjects\CodigoNF;
 
@@ -26,6 +27,54 @@ describe('CodigoNF', function () {
             expect($sut->getData()->value)->toBe('83427844');
             expect($sut->getData()->xmlString)->toBeString();
             expect($sut->getData()->xmlString)->toBe('<cNF>83427844</cNF>');
+        });
+
+        test('Should be return a failure Result if an object value is provided', function () {
+            $fakeXmlString = new stdClass;
+            $sut = CodigoNF::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be return a failure Result if a number value is provided', function () {
+            $fakeXmlString = 11;
+            $sut = CodigoNF::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be return a failure Result if an array value is provided', function () {
+            $fakeXmlString = ['<ide><cUF>11</cUF><cNF>83427844</cNF></ide>'];
+            $sut = CodigoNF::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be return a failure Result if null given', function () {
+            $fakeXmlString = null;
+            $sut = CodigoNF::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be return a failure Result if an empty string is provided', function () {
+            $fakeXmlString = '';
+            $sut = CodigoNF::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be return a failure Result if a bool string is provided', function () {
+            $fakeXmlString = true;
+            $sut = CodigoNF::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
         });
     });
 });
