eac45b200b0d4f8b00c65ee5c2db941a13b7fa61
salviorodrigo <rodrigosalvio@hotmail.com>
2025-12-04 00:28:54 -0400

    feat: add InvalidCodigoUFError

---
+++ src/Domain/Invoices/NFe/Exceptions/InvalidCodigoUFError.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BradiNfeApi\Domain\Invoices\NFe\Exceptions;
+
+use BradiNfeApi\Domain\Common\Protocols\ValidatorError;
+
+final class InvalidCodigoUFError extends ValidatorError
+{
+    public function __construct(public readonly string $fieldName)
+    {
+        $this->message = 'it must be present in the IBGE table of federative unit codes - Section 8.1.';
+    }
+}
