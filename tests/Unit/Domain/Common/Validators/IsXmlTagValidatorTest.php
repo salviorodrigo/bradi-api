1997ae232c623b51b34837abcaf050327b65b254
salviorodrigo <rodrigosalvio@hotmail.com>
2025-12-03 22:22:43 -0400

    test: ensure IsXmlTagValidator fails if a non xml string is provided

--- tests/Unit/Domain/Common/Validators/IsXmlTagValidatorTest.php
+++ tests/Unit/Domain/Common/Validators/IsXmlTagValidatorTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use BradiNfeApi\Domain\Common\Validators\Exceptions\IsNotXmlTagError;
 use BradiNfeApi\Domain\Common\Validators\IsXmlTagValidator;
 
 describe('IsStringValidator', function () {
@@ -9,6 +10,15 @@ describe('IsStringValidator', function () {
             $sutResponse = $sut->validate('<validTag>aValue</validTag>');
             expect($sutResponse->isSuccess())->toBeTruthy();
             expect($sutResponse->hasValue())->toBeFalsy();
+        });
+    });
+
+    describe('.validate()', function () {
+        test('Should be fail if a non xml string is provided', function () {
+            $sut = new IsXmlTagValidator('testField');
+            $sutResponse = $sut->validate('LoseIp<validTag>aValue</validTag>Sum');
+            expect($sutResponse->isSuccess())->toBeFalsy();
+            expect($sutResponse->getError())->toBeInstanceOf(IsNotXmlTagError::class);
         });
     });
 });
