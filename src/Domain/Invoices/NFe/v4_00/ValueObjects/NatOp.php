f93e8a633c138f57bd7221afc79ffcbb83bf15b1
salviorodrigo <rodrigosalvio@hotmail.com>
2025-12-25 11:48:49 -0400

    feat: add NatOp DFe class

---
+++ src/Domain/Invoices/NFe/v4_00/ValueObjects/NatOp.php
@@ -0,0 +1,103 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * MOC      7.0
+ * #        8
+ * ID       B04
+ * Campo    natOp
+ * Desc     Descrição da Natureza da Operação
+ * Tam      1-60
+ * OBS:
+ * Informar a natureza da operação de que decorrer a saída
+ * ou a entrada, tais como: venda, compra, transferência,
+ * devolução, importação, consignação, remessa (para fins de
+ * demonstração, de industrialização ou outra), conforme previsto
+ * na alínea 'i', inciso I, art. 19 do CONVENIO S/Nº, de 15 de
+ * dezembro de 1970.
+ */
+
+namespace BradiNfeApi\Domain\Invoices\NFe\v4_00\ValueObjects;
+
+use BradiNfeApi\Common\Exceptions\ValidationError;
+use BradiNfeApi\Common\Result;
+use BradiNfeApi\Domain\Common\Services\ValidationService;
+use BradiNfeApi\Domain\Common\Validators\IsStringValidator;
+use BradiNfeApi\Domain\Common\Validators\IsXmlTagValidator;
+use BradiNfeApi\Domain\Common\Validators\NotNullValidator;
+use BradiNfeApi\Domain\Invoices\NFe\Exceptions\InvalidNatOpError;
+use BradiNfeApi\Domain\Invoices\NFe\Exceptions\XmlElementWithAttributesError;
+use BradiNfeApi\Domain\Invoices\NFe\Exceptions\XmlElementWithElementsError;
+use BradiNfeApi\Domain\Invoices\Protocols\DFeElement;
+
+final class NatOp extends DFeElement
+{
+    public static string $tagName = 'natOp';
+
+    private function __construct(
+        public readonly string $value,
+        public readonly string $xmlString) {}
+
+    public static function parseXmlString(mixed $rawData): Result
+    {
+        $validationService = new ValidationService([
+            new IsStringValidator(self::$tagName),
+            new NotNullValidator(self::$tagName),
+            new IsXmlTagValidator(self::$tagName),
+        ]);
+        $validationServiceResponse = $validationService->verify($rawData);
+        if (! $validationServiceResponse->isSuccess()) {
+            return $validationServiceResponse;
+        }
+
+        $xmlTagString = DFeElement::xmlParser()->getTag($rawData, self::$tagName);
+        $xmlTagValue = DFeElement::xmlParser()->getTagValue($xmlTagString, self::$tagName);
+
+        if (strlen($xmlTagValue) > 60) {
+            Result::makeFailure([
+                new InvalidNatOpError(self::$tagName),
+            ]);
+        }
+
+        return Result::makeSuccess(
+            new NatOp(
+                $xmlTagValue,
+                $xmlTagString
+            )
+        );
+    }
+
+    public static function create(string $tagValue = '', array $elements = [], array $attributes = []): Result
+    {
+
+        if (count($attributes) > 0) {
+            return Result::makeFailure(
+                new ValidationError([
+                    new XmlElementWithAttributesError(self::$tagName),
+                ])
+            );
+        }
+
+        if (count($elements) > 0) {
+            return Result::makeFailure(
+                new ValidationError([
+                    new XmlElementWithElementsError(self::$tagName),
+                ])
+            );
+        }
+
+        if (strlen($tagValue) > 60) {
+            Result::makeFailure([
+                new InvalidNatOpError(self::$tagName),
+            ]);
+        }
+
+        return Result::makeSuccess(
+            new NatOp(
+                $tagValue,
+                self::generateXmlString(tagValue: $tagValue)
+            )
+        );
+    }
+}
