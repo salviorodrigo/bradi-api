fa1484c791a8f36773138d680750052716c40c12
salviorodrigo <rodrigosalvio@hotmail.com>
2025-12-26 19:49:56 -0400

    feat: add DataHoraEmissao class

---
+++ src/Domain/Invoices/NFe/Exceptions/InvalidDateTimeFormatError.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BradiNfeApi\Domain\Invoices\NFe\Exceptions;
+
+use BradiNfeApi\Domain\Common\Protocols\ValidatorError;
+
+final class InvalidDateTimeFormatError extends ValidatorError
+{
+    public function __construct(public readonly string $fieldName)
+    {
+        $this->message = 'it must be a UTC date time string like AAAA-MM-DDThh:mm:ssTZD.';
+    }
+}
