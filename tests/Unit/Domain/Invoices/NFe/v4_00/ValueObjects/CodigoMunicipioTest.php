7805a2026cc9671019358c52f45522d88b556d54
salviorodrigo <rodrigosalvio@hotmail.com>
2025-12-27 11:47:04 -0400

    test: ensure CodigoMunicipio works correctly

---
+++ tests/Unit/Domain/Invoices/NFe/v4_00/ValueObjects/CodigoMunicipioTest.php
@@ -0,0 +1,138 @@
+<?php
+
+declare(strict_types=1);
+
+use BradiNfeApi\Common\Exceptions\ValidationError;
+use BradiNfeApi\Common\Result;
+use BradiNfeApi\Domain\Invoices\NFe\v4_00\ValueObjects\CodigoMunicipio;
+
+/** Xml string example
+ * <ide>
+ *  <cMun>1100205</cMun>
+ * </ide>
+ */
+describe('CodigoMunicipio', function () {
+    describe('::parseXmlString()', function () {
+        test('Should be succeed when a valid xml string is provided', function () {
+            $fakeXmlString = '<ide><cMun>1100205</cMun></ide>';
+            $sut = CodigoMunicipio::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeTruthy();
+            expect($sut->getData())->toBeInstanceOf(CodigoMunicipio::class);
+            expect($sut->getData()->value)->toBeString();
+            expect($sut->getData()->value)->toBe('1100205');
+            expect($sut->getData()->xmlString)->toBeString();
+            expect($sut->getData()->xmlString)->toBe('<cMun>1100205</cMun>');
+        });
+
+        test('Should be return a failure Result if an object value is provided', function () {
+            $fakeXmlString = new stdClass;
+            $sut = CodigoMunicipio::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be return a failure Result if a number value is provided', function () {
+            $fakeXmlString = 11;
+            $sut = CodigoMunicipio::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be return a failure Result if an array value is provided', function () {
+            $fakeXmlString = ['<ide><cMun>1100205</cMun></ide>'];
+            $sut = CodigoMunicipio::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be return a failure Result if null given', function () {
+            $fakeXmlString = null;
+            $sut = CodigoMunicipio::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be return a failure Result if an empty string is provided', function () {
+            $fakeXmlString = '';
+            $sut = CodigoMunicipio::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be return a failure Result if a bool string is provided', function () {
+            $fakeXmlString = true;
+            $sut = CodigoMunicipio::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be fail if an invalid value is provided', function () {
+            $fakeXmlString = '<ide><cMun>1100204</cMun></ide>';
+            $sut = CodigoMunicipio::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+    });
+
+    describe('::create()', function () {
+        test('Should be succeed if a valid value is provided', function () {
+            $fakeTagValue = '1100205';
+            $sut = CodigoMunicipio::create(tagValue: $fakeTagValue);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeTruthy();
+            expect($sut->getData())->toBeInstanceOf(CodigoMunicipio::class);
+            expect($sut->getData()->value)->toBeString();
+            expect($sut->getData()->value)->toBe('1100205');
+            expect($sut->getData()->xmlString)->toBeString();
+            expect($sut->getData()->xmlString)->toBe('<cMun>1100205</cMun>');
+        });
+
+        test('Should be fail if an invalid value is provided', function () {
+            $fakeTagValue = '1100204';
+            $sut = CodigoMunicipio::create(tagValue: $fakeTagValue);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+    });
+
+    describe('::validateTagValue()', function () {
+        test('Should be true if provided value is valid', function () {
+            $fakeTagValue = '1100205';
+            $sut = CodigoMunicipio::validateTagValue($fakeTagValue);
+            expect($sut)->toBeTruthy();
+        });
+
+        test('Should be false if provided value is invalid', function () {
+            $fakeTagValue = '1100204';
+            $sut = CodigoMunicipio::validateTagValue($fakeTagValue);
+            expect($sut)->toBeFalsy();
+        });
+
+        test('Should be fail if a string whit letters is provided', function () {
+            $fakeTagValue = '1100205A';
+            $sut = CodigoMunicipio::validateTagValue($fakeTagValue);
+            expect($sut)->toBeFalsy();
+        });
+
+        test('Should be fail if a string more than seven letters is provided', function () {
+            $fakeTagValue = '11002055';
+            $sut = CodigoMunicipio::validateTagValue($fakeTagValue);
+            expect($sut)->toBeFalsy();
+        });
+
+        test('Should be fail if a string less than seven letters is provided', function () {
+            $fakeTagValue = '110020';
+            $sut = CodigoMunicipio::validateTagValue($fakeTagValue);
+            expect($sut)->toBeFalsy();
+        });
+    });
+});
