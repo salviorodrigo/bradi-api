5bd08f1f7281da7893b563ec315728343c871483
salviorodrigo <rodrigosalvio@hotmail.com>
2025-12-27 14:12:47 -0400

    docs: update AmbienteEmissao enum

--- src/Domain/Invoices/Enums/AmbienteEmissao.php
+++ src/Domain/Invoices/Enums/AmbienteEmissao.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 /*
  * Tabela de ambiente de emissao de
  * documento fiscal, de acordo com
- * MOC 7.0, campo mod.
+ * MOC 7.0, campo tpAmb.
  */
 
 namespace BradiNfeApi\Domain\Invoices\Enums;
