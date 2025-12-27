63abefd0f569e06b36c7aab42afbeaabd8dd3056
salviorodrigo <rodrigosalvio@hotmail.com>
2025-12-27 10:28:04 -0400

    test: ensure IdDestino works correctly

---
+++ tests/Unit/Domain/Invoices/NFe/v4_00/ValueObjects/IdDestinoTest.php
@@ -0,0 +1,126 @@
+<?php
+
+declare(strict_types=1);
+
+use BradiNfeApi\Common\Exceptions\ValidationError;
+use BradiNfeApi\Common\Result;
+use BradiNfeApi\Domain\Invoices\NFe\v4_00\ValueObjects\IdDestino;
+
+/** Xml string example
+ * <ide>
+ *  <idDest>1</idDest>
+ * </ide>
+ */
+describe('IdDestino', function () {
+    describe('::parseXmlString()', function () {
+        test('Should be succeed when a valid xml string is provided', function () {
+            $fakeXmlString = '<ide><idDest>1</idDest></ide>';
+            $sut = IdDestino::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeTruthy();
+            expect($sut->getData())->toBeInstanceOf(IdDestino::class);
+            expect($sut->getData()->value)->toBeString();
+            expect($sut->getData()->value)->toBe('1');
+            expect($sut->getData()->xmlString)->toBeString();
+            expect($sut->getData()->xmlString)->toBe('<idDest>1</idDest>');
+        });
+
+        test('Should be return a failure Result if an object value is provided', function () {
+            $fakeXmlString = new stdClass;
+            $sut = IdDestino::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be return a failure Result if a number value is provided', function () {
+            $fakeXmlString = 11;
+            $sut = IdDestino::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be return a failure Result if an array value is provided', function () {
+            $fakeXmlString = ['<ide><idDest>1</idDest></ide>'];
+            $sut = IdDestino::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be return a failure Result if null given', function () {
+            $fakeXmlString = null;
+            $sut = IdDestino::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be return a failure Result if an empty string is provided', function () {
+            $fakeXmlString = '';
+            $sut = IdDestino::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be return a failure Result if a bool string is provided', function () {
+            $fakeXmlString = true;
+            $sut = IdDestino::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be fail if an invalid value is provided', function () {
+            $fakeXmlString = '<ide><idDest>0</idDest></ide>';
+            $sut = IdDestino::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+    });
+
+    describe('::create()', function () {
+        test('Should be succeed if a valid value is provided', function () {
+            $fakeTagValue = '1';
+            $sut = IdDestino::create(tagValue: $fakeTagValue);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeTruthy();
+            expect($sut->getData())->toBeInstanceOf(IdDestino::class);
+            expect($sut->getData()->value)->toBeString();
+            expect($sut->getData()->value)->toBe('1');
+            expect($sut->getData()->xmlString)->toBeString();
+            expect($sut->getData()->xmlString)->toBe('<idDest>1</idDest>');
+        });
+
+        test('Should be fail if an invalid value is provided', function () {
+            $fakeTagValue = '4';
+            $sut = IdDestino::create(tagValue: $fakeTagValue);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+    });
+
+    describe('::validateTagValue()', function () {
+        test('Should be true if provided value is valid', function () {
+            $fakeTagValue = '1';
+            $sut = IdDestino::validateTagValue($fakeTagValue);
+            expect($sut)->toBeTruthy();
+        });
+
+        test('Should be fail if a string whit letters is provided', function () {
+            $fakeTagValue = 'A';
+            $sut = IdDestino::validateTagValue($fakeTagValue);
+            expect($sut)->toBeFalsy();
+        });
+
+        test('Should be fail if a string more than one letters is provided', function () {
+            $fakeTagValue = '11';
+            $sut = IdDestino::validateTagValue($fakeTagValue);
+            expect($sut)->toBeFalsy();
+        });
+    });
+});
