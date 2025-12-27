f8889dcd7c236972b954b88b5dc41f2ab737749d
salviorodrigo <rodrigosalvio@hotmail.com>
2025-12-27 10:17:29 -0400

    test: ensure TipoNF works correctly

---
+++ tests/Unit/Domain/Invoices/NFe/v4_00/ValueObjects/TipoNFTest.php
@@ -0,0 +1,126 @@
+<?php
+
+declare(strict_types=1);
+
+use BradiNfeApi\Common\Exceptions\ValidationError;
+use BradiNfeApi\Common\Result;
+use BradiNfeApi\Domain\Invoices\NFe\v4_00\ValueObjects\TipoNF;
+
+/** Xml string example
+ * <ide>
+ *  <tpNF>1</tpNF>
+ * </ide>
+ */
+describe('TipoNF', function () {
+    describe('::parseXmlString()', function () {
+        test('Should be succeed when a valid xml string is provided', function () {
+            $fakeXmlString = '<ide><tpNF>1</tpNF></ide>';
+            $sut = TipoNF::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeTruthy();
+            expect($sut->getData())->toBeInstanceOf(TipoNF::class);
+            expect($sut->getData()->value)->toBeString();
+            expect($sut->getData()->value)->toBe('1');
+            expect($sut->getData()->xmlString)->toBeString();
+            expect($sut->getData()->xmlString)->toBe('<tpNF>1</tpNF>');
+        });
+
+        test('Should be return a failure Result if an object value is provided', function () {
+            $fakeXmlString = new stdClass;
+            $sut = TipoNF::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be return a failure Result if a number value is provided', function () {
+            $fakeXmlString = 11;
+            $sut = TipoNF::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be return a failure Result if an array value is provided', function () {
+            $fakeXmlString = ['<ide><tpNF>1</tpNF></ide>'];
+            $sut = TipoNF::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be return a failure Result if null given', function () {
+            $fakeXmlString = null;
+            $sut = TipoNF::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be return a failure Result if an empty string is provided', function () {
+            $fakeXmlString = '';
+            $sut = TipoNF::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be return a failure Result if a bool string is provided', function () {
+            $fakeXmlString = true;
+            $sut = TipoNF::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be fail if an invalid value is provided', function () {
+            $fakeXmlString = '<ide><tpNF>2</tpNF></ide>';
+            $sut = TipoNF::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+    });
+
+    describe('::create()', function () {
+        test('Should be succeed if a valid value is provided', function () {
+            $fakeTagValue = '0';
+            $sut = TipoNF::create(tagValue: $fakeTagValue);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeTruthy();
+            expect($sut->getData())->toBeInstanceOf(TipoNF::class);
+            expect($sut->getData()->value)->toBeString();
+            expect($sut->getData()->value)->toBe('0');
+            expect($sut->getData()->xmlString)->toBeString();
+            expect($sut->getData()->xmlString)->toBe('<tpNF>0</tpNF>');
+        });
+
+        test('Should be fail if an invalid value is provided', function () {
+            $fakeTagValue = '2';
+            $sut = TipoNF::create(tagValue: $fakeTagValue);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+    });
+
+    describe('::validateTagValue()', function () {
+        test('Should be true if provided value is valid', function () {
+            $fakeTagValue = '1';
+            $sut = TipoNF::validateTagValue($fakeTagValue);
+            expect($sut)->toBeTruthy();
+        });
+
+        test('Should be fail if a string whit letters is provided', function () {
+            $fakeTagValue = 'A';
+            $sut = TipoNF::validateTagValue($fakeTagValue);
+            expect($sut)->toBeFalsy();
+        });
+
+        test('Should be fail if a string more than one letters is provided', function () {
+            $fakeTagValue = '55';
+            $sut = TipoNF::validateTagValue($fakeTagValue);
+            expect($sut)->toBeFalsy();
+        });
+    });
+});
