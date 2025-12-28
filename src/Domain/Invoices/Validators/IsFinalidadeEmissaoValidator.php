13c1a734848af02cc1c11c774c2cdc381f0d3cef
salviorodrigo <rodrigosalvio@hotmail.com>
2025-12-28 13:48:49 -0400

    feat: add IsFinalidadeEmissaoValidator

---
+++ src/Domain/Invoices/Validators/IsFinalidadeEmissaoValidator.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BradiNfeApi\Domain\Invoices\Validators;
+
+use BradiNfeApi\Common\Result;
+use BradiNfeApi\Domain\Common\Protocols\Validator;
+use BradiNfeApi\Domain\Invoices\Enums\FinalidadeEmissao;
+use BradiNfeApi\Domain\Invoices\NFe\Exceptions\InvalidFinalidadeNFError;
+
+final class IsFinalidadeEmissaoValidator extends Validator
+{
+    public function __construct(public readonly string $fieldName) {}
+
+    public function validate(mixed $candidate): Result
+    {
+        if (! (bool) FinalidadeEmissao::tryFrom($candidate)) {
+            return Result::makeFailure(new InvalidFinalidadeNFError($this->fieldName));
+        }
+
+        return Result::makeSuccess();
+    }
+}
+
+// TODO Make test file.
