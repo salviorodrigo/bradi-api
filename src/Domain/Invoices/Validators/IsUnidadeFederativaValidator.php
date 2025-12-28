d5b963f9b48c6f921b0613f3655acfa6819243d2
salviorodrigo <rodrigosalvio@hotmail.com>
2025-12-28 13:50:40 -0400

    feat: add IsUnidadeFederativaValidator

---
+++ src/Domain/Invoices/Validators/IsUnidadeFederativaValidator.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BradiNfeApi\Domain\Invoices\Validators;
+
+use BradiNfeApi\Common\Result;
+use BradiNfeApi\Domain\Common\Protocols\Validator;
+use BradiNfeApi\Domain\Invoices\Enums\UnidadeFederativa;
+use BradiNfeApi\Domain\Invoices\NFe\Exceptions\InvalidCodigoUFError;
+
+final class IsUnidadeFederativaValidator extends Validator
+{
+    public function __construct(public readonly string $fieldName) {}
+
+    public function validate(mixed $candidate): Result
+    {
+        if (! (bool) UnidadeFederativa::tryFrom($candidate)) {
+            return Result::makeFailure(new InvalidCodigoUFError($this->fieldName));
+        }
+
+        return Result::makeSuccess();
+    }
+}
+
+// TODO Make test file.
