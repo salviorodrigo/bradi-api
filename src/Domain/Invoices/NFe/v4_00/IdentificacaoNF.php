670def85ab4c8ffa9cb850291d1312dd175635a3
salviorodrigo <rodrigosalvio@hotmail.com>
2025-12-28 16:46:26 -0400

    feat: add IdentificacaoNF

---
+++ src/Domain/Invoices/NFe/v4_00/IdentificacaoNF.php
@@ -0,0 +1,171 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * MOC      7.0
+ * #        5
+ * ID       B01
+ * Campo    ide
+ * Desc     Informações de identificação da NF-e
+ * Tam
+ * OBS:
+ */
+
+namespace BradiNfeApi\Domain\Invoices\NFe\v4_00;
+
+use BradiNfeApi\Common\Exceptions\ValidationError;
+use BradiNfeApi\Common\Exceptions\ValidationErrorBag;
+use BradiNfeApi\Common\Result;
+use BradiNfeApi\Domain\Common\Services\ValidationService;
+use BradiNfeApi\Domain\Common\Validators\IsStringValidator;
+use BradiNfeApi\Domain\Common\Validators\IsXmlTagValidator;
+use BradiNfeApi\Domain\Common\Validators\NotNullValidator;
+use BradiNfeApi\Domain\Invoices\NFe\Exceptions\XmlElementWithValueError;
+use BradiNfeApi\Domain\Invoices\NFe\v4_00\ValueObjects\CodigoMunFG;
+use BradiNfeApi\Domain\Invoices\NFe\v4_00\ValueObjects\CodigoNF;
+use BradiNfeApi\Domain\Invoices\NFe\v4_00\ValueObjects\CodigoUF;
+use BradiNfeApi\Domain\Invoices\NFe\v4_00\ValueObjects\DataHoraEmissao;
+use BradiNfeApi\Domain\Invoices\NFe\v4_00\ValueObjects\FinalidadeNF;
+use BradiNfeApi\Domain\Invoices\NFe\v4_00\ValueObjects\IdDestino;
+use BradiNfeApi\Domain\Invoices\NFe\v4_00\ValueObjects\IndFinal;
+use BradiNfeApi\Domain\Invoices\NFe\v4_00\ValueObjects\Mod;
+use BradiNfeApi\Domain\Invoices\NFe\v4_00\ValueObjects\NatOp;
+use BradiNfeApi\Domain\Invoices\NFe\v4_00\ValueObjects\NumeroNF;
+use BradiNfeApi\Domain\Invoices\NFe\v4_00\ValueObjects\Serie;
+use BradiNfeApi\Domain\Invoices\NFe\v4_00\ValueObjects\TipoAmbiente;
+use BradiNfeApi\Domain\Invoices\NFe\v4_00\ValueObjects\TipoNF;
+use BradiNfeApi\Domain\Invoices\Protocols\DFeElement;
+use BradiNfeApi\Domain\Invoices\Validators\RequiredTagValidator;
+use Exception;
+
+final class IdentificacaoNF extends DFeElement
+{
+    public static string $tagName = 'ide';
+
+    private function __construct(
+        public readonly string $value,
+        public readonly string $xmlString,
+        public readonly CodigoUF $cUF,
+        public readonly CodigoNF $cNF,
+        public readonly NatOp $natOp,
+        public readonly Mod $mod,
+        public readonly Serie $serie,
+        public readonly NumeroNF $nNF,
+        public readonly DataHoraEmissao $dhEmi,
+        public readonly TipoNF $tpNF,
+        public readonly IdDestino $idDest,
+        public readonly CodigoMunFG $cMunFG,
+        public readonly TipoAmbiente $tpAmb,
+        public readonly FinalidadeNF $finNFe,
+        public readonly IndFinal $indFinal
+    ) {}
+
+    public static function parseXmlString(mixed $rawData): Result
+    {
+        $validationService = new ValidationService([
+            new IsStringValidator(self::$tagName),
+            new NotNullValidator(self::$tagName),
+            new IsXmlTagValidator(self::$tagName),
+            new RequiredTagValidator(self::$tagName),
+            new RequiredTagValidator('cUF'),
+            new RequiredTagValidator('cNF'),
+            new RequiredTagValidator('natOp'),
+            new RequiredTagValidator('mod'),
+            new RequiredTagValidator('serie'),
+            new RequiredTagValidator('nNF'),
+            new RequiredTagValidator('dhEmi'),
+            new RequiredTagValidator('tpNF'),
+            new RequiredTagValidator('idDest'),
+            new RequiredTagValidator('cMunFG'),
+            new RequiredTagValidator('tpImp'),
+            new RequiredTagValidator('tpEmis'),
+            new RequiredTagValidator('cDV'),
+            new RequiredTagValidator('tpAmb'),
+            new RequiredTagValidator('finNFe'),
+            new RequiredTagValidator('indFinal'),
+            new RequiredTagValidator('indPres'),
+            new RequiredTagValidator('procEmi'),
+            new RequiredTagValidator('verProc'),
+        ]);
+        $validationServiceResponse = $validationService->verify($rawData);
+        if (! $validationServiceResponse->isSuccess()) {
+            return $validationServiceResponse;
+        }
+
+        $xmlTagString = DFeElement::xmlParser()->getTag($rawData, self::$tagName);
+        $tagValue = DFeElement::xmlParser()->getTagValue($xmlTagString, self::$tagName);
+        $validationValueResponse = self::validateTagValue($tagValue);
+
+        if (! $validationValueResponse->isSuccess()) {
+            return $validationValueResponse;
+        }
+
+        $xmlElements = [
+            CodigoUF::class,
+            CodigoNF::class,
+            NatOp::class,
+            Mod::class,
+            Serie::class,
+            NumeroNF::class,
+            DataHoraEmissao::class,
+            TipoNF::class,
+            IdDestino::class,
+            CodigoMunFG::class,
+            TipoAmbiente::class,
+            FinalidadeNF::class,
+            IndFinal::class,
+        ];
+
+        $parserErrorBag = new ValidationErrorBag;
+        $xmlElementsBag = [];
+
+        foreach ($xmlElements as $element) {
+            $parsingResult = $element::parseXmlString($xmlTagString);
+            if (! $parsingResult->isSuccess()) {
+                $parserErrorBag->add($parsingResult->getError());
+            } else {
+                $xmlElementsBag[] = $parsingResult->getData();
+            }
+        }
+
+        if (count($parserErrorBag->validationErrors) > 0) {
+            return Result::makeFailure(
+                $parserErrorBag->resolve()
+            );
+        }
+
+        // TODO Validar mod para verificar obrigatoriedade de dhSaiEnt.
+        // TODO Validar tpEmis para verificar obrigatoriedade de campos de contingencia.
+        // ['dhCont', 'xJust', dhSaiEnt, indIntermed];
+
+        return Result::makeSuccess(
+            new IdentificacaoNF(
+                $tagValue,
+                $xmlTagString,
+                ...$xmlElementsBag
+            )
+        );
+    }
+
+    public static function create(string $tagValue = '', array $elements = [], array $attributes = []): Result
+    {
+
+        // TODO Verificar se todas as tags obrigatorias estao presentes
+
+        throw new Exception('Must be implemented');
+    }
+
+    public static function validateTagValue(string $tagValue): Result
+    {
+        if ($tagValue != '') {
+            return Result::makeFailure(
+                new ValidationError([
+                    new XmlElementWithValueError(self::$tagName),
+                ])
+            );
+        }
+
+        return Result::makeSuccess();
+    }
+}
