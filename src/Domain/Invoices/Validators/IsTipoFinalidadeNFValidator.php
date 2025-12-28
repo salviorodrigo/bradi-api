b04767bdaa1e00ecc4403809b05b3fb155210846
salviorodrigo <rodrigosalvio@hotmail.com>
2025-12-28 13:49:42 -0400

    feat: add IsTipoFinalidadeDFeValidator

---
+++ src/Domain/Invoices/Validators/IsTipoFinalidadeNFValidator.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BradiNfeApi\Domain\Invoices\Validators;
+
+use BradiNfeApi\Common\Result;
+use BradiNfeApi\Domain\Common\Protocols\Validator;
+use BradiNfeApi\Domain\Invoices\Enums\TipoFinalidadeNF;
+use BradiNfeApi\Domain\Invoices\NFe\Exceptions\InvalidIndFinalError;
+
+final class IsTipoFinalidadeNFValidator extends Validator
+{
+    public function __construct(public readonly string $fieldName) {}
+
+    public function validate(mixed $candidate): Result
+    {
+        if (! (bool) TipoFinalidadeNF::tryFrom($candidate)) {
+            return Result::makeFailure(new InvalidIndFinalError($this->fieldName));
+        }
+
+        return Result::makeSuccess();
+    }
+}
+
+// TODO Make test file.
