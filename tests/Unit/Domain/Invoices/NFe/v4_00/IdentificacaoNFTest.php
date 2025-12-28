d96feb6c87ea6155700cb721c5fa260a505ae10f
salviorodrigo <rodrigosalvio@hotmail.com>
2025-12-28 16:47:15 -0400

    test: esnure IdentificacaoNF.parseString() succeed if a valid xml string is provided

---
+++ tests/Unit/Domain/Invoices/NFe/v4_00/IdentificacaoNFTest.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+use BradiNfeApi\Common\Result;
+use BradiNfeApi\Domain\Invoices\NFe\v4_00\IdentificacaoNF;
+
+/** Xml string example
+ *
+ * <infNFe Id="NFe11251102393780000293550030005188581546834922" versao="4.00">
+ *  <ide>
+ *   <cUF>11</cUF>
+ *   <cNF>54683492</cNF>
+ *   <natOp>VENDA DE MERCADORIA</natOp>
+ *   <mod>55</mod>
+ *   <serie>3</serie>
+ *   <nNF>518858</nNF>
+ *   <dhEmi>2025-11-14T07:45:00-04:00</dhEmi>
+ *   <dhSaiEnt>2025-11-14T07:45:00-04:00</dhSaiEnt>
+ *   <tpNF>1</tpNF>
+ *   <idDest>1</idDest>
+ *   <cMunFG>1100205</cMunFG>
+ *   <tpImp>1</tpImp>
+ *   <tpEmis>1</tpEmis>
+ *   <cDV>2</cDV>
+ *   <tpAmb>1</tpAmb>
+ *   <finNFe>1</finNFe>
+ *   <indFinal>1</indFinal>
+ *   <indPres>1</indPres>
+ *   <procEmi>0</procEmi>
+ *   <verProc>12.1.2310</verProc>
+ * </ide>
+ * </infNFe>
+ */
+describe('IdentificacaoNF', function () {
+    describe('::parseXmlString()', function () {
+        test('Should be succeed when a valid xml string is provided', function () {
+            $fakeXmlString = '<infNFe Id="NFe11251102393780000293550030005188581546834922" versao="4.00"><ide><cUF>11</cUF><cNF>54683492</cNF><natOp>VENDA DE COMBUSTIVEL/ VENDA DE MERC. ADQ./ VENDA LUBRIFICANT</natOp><mod>55</mod><serie>3</serie><nNF>518858</nNF><dhEmi>2025-11-14T07:45:00-04:00</dhEmi><dhSaiEnt>2025-11-14T07:45:00-04:00</dhSaiEnt><tpNF>1</tpNF><idDest>1</idDest><cMunFG>1100205</cMunFG><tpImp>1</tpImp><tpEmis>1</tpEmis><cDV>2</cDV><tpAmb>1</tpAmb><finNFe>1</finNFe><indFinal>1</indFinal><indPres>1</indPres><procEmi>0</procEmi><verProc>12.1.2310</verProc></ide></infNFe>';
+            $sut = IdentificacaoNF::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeTruthy();
+            expect($sut->getData())->toBeInstanceOf(IdentificacaoNF::class);
+            expect($sut->getData()->value)->toBeString();
+            expect($sut->getData()->value)->toBe('');
+            expect($sut->getData()->xmlString)->toBeString();
+            expect($sut->getData()->xmlString)->toBe('<ide><cUF>11</cUF><cNF>54683492</cNF><natOp>VENDA DE COMBUSTIVEL/ VENDA DE MERC. ADQ./ VENDA LUBRIFICANT</natOp><mod>55</mod><serie>3</serie><nNF>518858</nNF><dhEmi>2025-11-14T07:45:00-04:00</dhEmi><dhSaiEnt>2025-11-14T07:45:00-04:00</dhSaiEnt><tpNF>1</tpNF><idDest>1</idDest><cMunFG>1100205</cMunFG><tpImp>1</tpImp><tpEmis>1</tpEmis><cDV>2</cDV><tpAmb>1</tpAmb><finNFe>1</finNFe><indFinal>1</indFinal><indPres>1</indPres><procEmi>0</procEmi><verProc>12.1.2310</verProc></ide>');
+        });
+    });
+});
