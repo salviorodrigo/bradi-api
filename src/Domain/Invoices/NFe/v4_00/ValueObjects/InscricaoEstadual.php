5c011128087a823307ade7c2e7162199cdd6f4fb
salviorodrigo <rodrigosalvio@hotmail.com>
2025-12-30 19:54:44 -0400

    feat: add InscricaoEstadual class

---
+++ src/Domain/Invoices/NFe/v4_00/ValueObjects/InscricaoEstadual.php
@@ -0,0 +1,119 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * MOC      7.0
+ * #        46
+ * ID       C17
+ * Campo    IE
+ * Desc     Inscrição Estadual
+ * Tam      2-14
+ * OBS:
+ * Informar somente os algarismos, sem os caracteres
+ * de formatação (ponto, barra, hífen, etc.).
+ */
+
+namespace BradiNfeApi\Domain\Invoices\NFe\v4_00\ValueObjects;
+
+use BradiNfeApi\Common\Exceptions\ValidationError;
+use BradiNfeApi\Common\Result;
+use BradiNfeApi\Domain\Common\Services\ValidationService;
+use BradiNfeApi\Domain\Common\Validators\IsNumericValidator;
+use BradiNfeApi\Domain\Common\Validators\IsStringValidator;
+use BradiNfeApi\Domain\Common\Validators\IsXmlTagValidator;
+use BradiNfeApi\Domain\Common\Validators\MaxStringLengthValidator;
+use BradiNfeApi\Domain\Common\Validators\MinStringLengthValidator;
+use BradiNfeApi\Domain\Common\Validators\NotNullValidator;
+use BradiNfeApi\Domain\Invoices\NFe\Exceptions\XmlElementWithAttributesError;
+use BradiNfeApi\Domain\Invoices\NFe\Exceptions\XmlElementWithElementsError;
+use BradiNfeApi\Domain\Invoices\Protocols\DFeElement;
+
+final class InscricaoEstadual extends DFeElement
+{
+    public static string $tagName = 'IE';
+
+    private function __construct(
+        public readonly string $value,
+        public readonly string $xmlString) {}
+
+    public static function parseXmlString(mixed $rawData): Result
+    {
+        $validationService = new ValidationService([
+            new IsStringValidator(self::$tagName),
+            new NotNullValidator(self::$tagName),
+            new IsXmlTagValidator(self::$tagName),
+        ]);
+        $validationServiceResponse = $validationService->verify($rawData);
+        if (! $validationServiceResponse->isSuccess()) {
+            return $validationServiceResponse;
+        }
+
+        $xmlTagString = self::xmlParser()->getTag($rawData, self::$tagName);
+        $tagValue = self::xmlParser()->getTagValue($xmlTagString, self::$tagName);
+        $validationValueResponse = self::validateTagValue($tagValue);
+
+        if (! $validationValueResponse->isSuccess()) {
+            return $validationValueResponse;
+        }
+
+        return Result::makeSuccess(
+            new self(
+                $tagValue,
+                $xmlTagString
+            )
+        );
+    }
+
+    public static function create(string $tagValue = '', array $elements = [], array $attributes = []): Result
+    {
+
+        if (count($attributes) > 0) {
+            return Result::makeFailure(
+                new ValidationError([
+                    new XmlElementWithAttributesError(self::$tagName),
+                ])
+            );
+        }
+
+        if (count($elements) > 0) {
+            return Result::makeFailure(
+                new ValidationError([
+                    new XmlElementWithElementsError(self::$tagName),
+                ])
+            );
+        }
+
+        $validationValueResponse = self::validateTagValue($tagValue);
+
+        if (! $validationValueResponse->isSuccess()) {
+            return $validationValueResponse;
+        }
+
+        return Result::makeSuccess(
+            new self(
+                $tagValue,
+                self::generateXmlString(tagValue: $tagValue)
+            )
+        );
+    }
+
+    public static function validateTagValue(string $tagValue): Result
+    {
+        $validationService = new ValidationService([
+            new IsStringValidator(self::$tagName),
+            new NotNullValidator(self::$tagName),
+            new IsNumericValidator(self::$tagName),
+            new MaxStringLengthValidator(self::$tagName, 16),
+            new MinStringLengthValidator(self::$tagName, 2),
+        ]);
+
+        $validationServiceResponse = $validationService->verify($tagValue);
+
+        if (! $validationServiceResponse->isSuccess()) {
+            return $validationServiceResponse;
+        }
+
+        return Result::makeSuccess();
+    }
+}
