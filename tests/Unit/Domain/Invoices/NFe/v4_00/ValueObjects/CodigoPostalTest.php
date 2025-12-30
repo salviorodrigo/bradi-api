e20d21e7d85fee0e5777f81b51a8d0d5293926f3
salviorodrigo <rodrigosalvio@hotmail.com>
2025-12-30 16:47:48 -0400

    test: ensure CodigoPostal works correctly

---
+++ tests/Unit/Domain/Invoices/NFe/v4_00/ValueObjects/CodigoPostalTest.php
@@ -0,0 +1,159 @@
+<?php
+
+declare(strict_types=1);
+
+use BradiNfeApi\Common\Exceptions\ValidationError;
+use BradiNfeApi\Common\Result;
+use BradiNfeApi\Domain\Invoices\NFe\v4_00\ValueObjects\CodigoPostal;
+
+/** Xml string example
+ * <ide>
+ *  <CEP>76811694</CEP>
+ * </ide>
+ */
+describe('CodigoPostal', function () {
+    describe('::parseXmlString()', function () {
+        test('Should be return a Result object with himself when a valid xml string is provided', function () {
+            $fakeXmlString = '<ide><CEP>76811694</CEP></ide>';
+            $sut = CodigoPostal::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeTruthy();
+            expect($sut->getData())->toBeInstanceOf(CodigoPostal::class);
+            expect($sut->getData()->value)->toBeString();
+            expect($sut->getData()->value)->toBe('76811694');
+            expect($sut->getData()->xmlString)->toBeString();
+            expect($sut->getData()->xmlString)->toBe('<CEP>76811694</CEP>');
+        });
+
+        test('Should be return a failure Result if an object value is provided', function () {
+            $fakeXmlString = new stdClass;
+            $sut = CodigoPostal::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be return a failure Result if a number value is provided', function () {
+            $fakeXmlString = 11;
+            $sut = CodigoPostal::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be return a failure Result if an array value is provided', function () {
+            $fakeXmlString = ['<ide><CEP>76811694</CEP></ide>'];
+            $sut = CodigoPostal::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be return a failure Result if null given', function () {
+            $fakeXmlString = null;
+            $sut = CodigoPostal::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be return a failure Result if an empty string is provided', function () {
+            $fakeXmlString = '';
+            $sut = CodigoPostal::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be return a failure Result if a bool string is provided', function () {
+            $fakeXmlString = true;
+            $sut = CodigoPostal::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be fail if a string fewer than eight letters is provided', function () {
+            $fakeXmlString = '<CEP>7681169</CEP>';
+            $sut = CodigoPostal::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+    });
+
+    describe('::create()', function () {
+        test('Should be return a Result object with success if a valid CEP value is provided', function () {
+            $fakeTagValue = '76811694';
+            $sut = CodigoPostal::create($fakeTagValue);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeTruthy();
+            expect($sut->getData())->toBeInstanceOf(CodigoPostal::class);
+            expect($sut->getData()->value)->toBeString();
+            expect(preg_match('/^(?!0{8})[0-9]{8}$/', $sut->getData()->value))->toBeTruthy();
+            expect((CodigoPostal::parseXmlString($sut->getData()->xmlString))->isSuccess())->toBeTruthy();
+        });
+
+        test('Should be fail if a tag value isn\'t provided', function () {
+            $sut = CodigoPostal::create();
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be fail if an invalid CEP value is provided', function () {
+            $fakeTagValue = '7681169';
+            $sut = CodigoPostal::create($fakeTagValue);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be fail if attributes is provided', function () {
+            $fakeTagValue = '76811694';
+            $fakeAttributes = ['fakeAttribute' => 'fakeAttributeValue'];
+            $sut = CodigoPostal::create(tagValue: $fakeTagValue, attributes: $fakeAttributes);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be fail if elements is provided', function () {
+            $fakeTagValue = '76811694';
+            $fakeElements = ['fakeElement'];
+            $sut = CodigoPostal::create(tagValue: $fakeTagValue, elements: $fakeElements);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+    });
+
+    describe('::validateTagValue()', function () {
+        test('Should be true if provided value is a CEP', function () {
+            $fakeTagValue = '76811694';
+            $sut = CodigoPostal::validateTagValue($fakeTagValue);
+            expect($sut->isSuccess())->toBeTruthy();
+        });
+
+        test('Should be fail if a string whit letters is provided', function () {
+            $fakeTagValue = '7681169A';
+            $sut = CodigoPostal::validateTagValue($fakeTagValue);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be fail if a string fewer than eight letters is provided', function () {
+            $fakeTagValue = '7681169';
+            $sut = CodigoPostal::validateTagValue($fakeTagValue);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be fail if a string more than eight letters is provided', function () {
+            $fakeTagValue = '768116944';
+            $sut = CodigoPostal::validateTagValue($fakeTagValue);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+    });
+});
