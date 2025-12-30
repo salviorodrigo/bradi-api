48d6ef3014ba5d1bd8da3f47378b5a3eb61d47ac
salviorodrigo <rodrigosalvio@hotmail.com>
2025-12-30 10:36:20 -0400

    test: ensure CadastroPF works correctly

---
+++ tests/Unit/Domain/Invoices/NFe/v4_00/ValueObjects/CadastroPFTest.php
@@ -0,0 +1,177 @@
+<?php
+
+declare(strict_types=1);
+
+use BradiNfeApi\Common\Exceptions\ValidationError;
+use BradiNfeApi\Common\Result;
+use BradiNfeApi\Domain\Invoices\NFe\v4_00\ValueObjects\CadastroPF;
+
+/** Xml string example
+ * <emit>
+ *  <CPF>00288867122</CPF>
+ * </emit>
+ */
+describe('CadastroPF', function () {
+    describe('::parseXmlString()', function () {
+        test('Should be return a Result object with himself when a valid xml string is provided', function () {
+            $fakeXmlString = '<emit><CPF>00288867122</CPF></emit>';
+            $sut = CadastroPF::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeTruthy();
+            expect($sut->getData())->toBeInstanceOf(CadastroPF::class);
+            expect($sut->getData()->value)->toBeString();
+            expect($sut->getData()->value)->toBe('00288867122');
+            expect($sut->getData()->xmlString)->toBeString();
+            expect($sut->getData()->xmlString)->toBe('<CPF>00288867122</CPF>');
+        });
+
+        test('Should be return a failure Result if an object value is provided', function () {
+            $fakeXmlString = new stdClass;
+            $sut = CadastroPF::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be return a failure Result if a number value is provided', function () {
+            $fakeXmlString = 11;
+            $sut = CadastroPF::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be return a failure Result if an array value is provided', function () {
+            $fakeXmlString = ['<emit><CPF>00288867122</CPF></emit>'];
+            $sut = CadastroPF::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be return a failure Result if null given', function () {
+            $fakeXmlString = null;
+            $sut = CadastroPF::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be return a failure Result if an empty string is provided', function () {
+            $fakeXmlString = '';
+            $sut = CadastroPF::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be return a failure Result if a bool string is provided', function () {
+            $fakeXmlString = true;
+            $sut = CadastroPF::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be fail if a string fewer than eleven letters is provided', function () {
+            $fakeXmlString = '<emit><CPF>0028886712</CPF></emit>';
+            $sut = CadastroPF::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be fail if an invalid value is provided', function () {
+            $fakeXmlString = '<emit><CPF>00288867123</CPF></emit>';
+            $sut = CadastroPF::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+    });
+
+    describe('::create()', function () {
+        test('Should be return a Result object with success if a valid value is provided', function () {
+            $fakeTagValue = '00288867122';
+            $sut = CadastroPF::create($fakeTagValue);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeTruthy();
+            expect($sut->getData())->toBeInstanceOf(CadastroPF::class);
+            expect($sut->getData()->value)->toBeString();
+        });
+
+        test('Should be fail if a tag value isn\'t provided', function () {
+            $sut = CadastroPF::create();
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be fail if an invalid CPF value is provided', function () {
+            $fakeTagValue = '00288867123';
+            $sut = CadastroPF::create($fakeTagValue);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be fail if attributes is provided', function () {
+            $fakeTagValue = '00288867122';
+            $fakeAttributes = ['fakeAttribute' => 'fakeAttributeValue'];
+            $sut = CadastroPF::create(tagValue: $fakeTagValue, attributes: $fakeAttributes);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be fail if elements is provided', function () {
+            $fakeTagValue = '00288867122';
+            $fakeElements = ['fakeElement'];
+            $sut = CadastroPF::create(tagValue: $fakeTagValue, elements: $fakeElements);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+    });
+
+    describe('::validateTagValue()', function () {
+        test('Should be true if provided value is a CPF', function () {
+            $fakeTagValue = '00288867122';
+            $sut = CadastroPF::validateTagValue($fakeTagValue);
+            expect($sut->isSuccess())->toBeTruthy();
+        });
+
+        test('Should be succeed if a zero string is provided', function () {
+            $fakeTagValue = '00000000000';
+            $sut = CadastroPF::validateTagValue($fakeTagValue);
+            expect($sut->isSuccess())->toBeTruthy();
+        });
+
+        test('Should be fail if a string whit letters is provided', function () {
+            $fakeTagValue = '00288867122A';
+            $sut = CadastroPF::validateTagValue($fakeTagValue);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be fail if a non-zero repeating numeric string is provided', function () {
+            $fakeTagValue = '11111111111';
+            $sut = CadastroPF::validateTagValue($fakeTagValue);
+            expect($sut->isSuccess())->toBeFalsy();
+        });
+
+        test('Should be fail if a string fewer than eleven letters is provided', function () {
+            $fakeTagValue = '1234567890';
+            $sut = CadastroPF::validateTagValue($fakeTagValue);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be fail if a string more than eleven letters is provided', function () {
+            $fakeTagValue = '123456789012';
+            $sut = CadastroPF::validateTagValue($fakeTagValue);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+    });
+});
