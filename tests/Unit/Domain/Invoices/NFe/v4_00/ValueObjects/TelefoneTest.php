09397baf792326ba87f73b5b2217fb37ab4b5228
salviorodrigo <rodrigosalvio@hotmail.com>
2025-12-30 19:44:39 -0400

    test: ensure Telefone works correctly

---
+++ tests/Unit/Domain/Invoices/NFe/v4_00/ValueObjects/TelefoneTest.php
@@ -0,0 +1,168 @@
+<?php
+
+declare(strict_types=1);
+
+use BradiNfeApi\Common\Exceptions\ValidationError;
+use BradiNfeApi\Common\Result;
+use BradiNfeApi\Domain\Invoices\NFe\v4_00\ValueObjects\Telefone;
+
+/** Xml string example
+ * <ide>
+ *  <fone>+5599999999999</fone>
+ * </ide>
+ */
+describe('Telefone', function () {
+    describe('::parseXmlString()', function () {
+        test('Should be return a Result object with himself when a valid xml string is provided', function () {
+            $fakeXmlString = '<ide><fone>+5599999999999</fone></ide>';
+            $sut = Telefone::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeTruthy();
+            expect($sut->getData())->toBeInstanceOf(Telefone::class);
+            expect($sut->getData()->value)->toBeString();
+            expect($sut->getData()->value)->toBe('+5599999999999');
+            expect($sut->getData()->xmlString)->toBeString();
+            expect($sut->getData()->xmlString)->toBe('<fone>+5599999999999</fone>');
+        });
+
+        test('Should be succeed if an empty string is provided', function () {
+            $fakeXmlString = '';
+            $sut = Telefone::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeTruthy();
+            expect($sut->getData())->toBeInstanceOf(Telefone::class);
+            expect($sut->getData()->value)->toBeString();
+            expect($sut->getData()->value)->toBe('');
+            expect($sut->getData()->xmlString)->toBeString();
+            expect($sut->getData()->xmlString)->toBe('');
+        });
+
+        test('Should be return a failure Result if an object value is provided', function () {
+            $fakeXmlString = new stdClass;
+            $sut = Telefone::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be return a failure Result if a number value is provided', function () {
+            $fakeXmlString = 11;
+            $sut = Telefone::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be return a failure Result if an array value is provided', function () {
+            $fakeXmlString = ['<ide><fone>+5599999999999</fone></ide>'];
+            $sut = Telefone::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be return a failure Result if null given', function () {
+            $fakeXmlString = null;
+            $sut = Telefone::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be return a failure Result if a bool string is provided', function () {
+            $fakeXmlString = true;
+            $sut = Telefone::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be fail if a string fewer than six letters is provided', function () {
+            $fakeXmlString = '<fone>10505</fone>';
+            $sut = Telefone::parseXmlString($fakeXmlString);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+    });
+
+    describe('::create()', function () {
+        test('Should be return a Result object with success if a valid fone value is provided', function () {
+            $fakeTagValue = '+5599999999999';
+            $sut = Telefone::create($fakeTagValue);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeTruthy();
+            expect($sut->getData())->toBeInstanceOf(Telefone::class);
+            expect($sut->getData()->value)->toBeString();
+            expect($sut->getData()->value)->toBe('+5599999999999');
+            expect($sut->getData()->xmlString)->toBeString();
+            expect($sut->getData()->xmlString)->toBe('<fone>+5599999999999</fone>');
+        });
+
+        test('Should be succeed if a tag value isn\'t provided', function () {
+            $sut = Telefone::create();
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeTruthy();
+            expect($sut->getData())->toBeInstanceOf(Telefone::class);
+            expect($sut->getData()->value)->toBeString();
+            expect($sut->getData()->value)->toBe('');
+            expect($sut->getData()->xmlString)->toBeString();
+            expect($sut->getData()->xmlString)->toBe('');
+        });
+
+        test('Should be fail if an invalid fone value is provided', function () {
+            $fakeTagValue = '+55999999999999';
+            $sut = Telefone::create($fakeTagValue);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be fail if attributes is provided', function () {
+            $fakeTagValue = '+5599999999999';
+            $fakeAttributes = ['fakeAttribute' => 'fakeAttributeValue'];
+            $sut = Telefone::create(tagValue: $fakeTagValue, attributes: $fakeAttributes);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be fail if elements is provided', function () {
+            $fakeTagValue = '+5599999999999';
+            $fakeElements = ['fakeElement'];
+            $sut = Telefone::create(tagValue: $fakeTagValue, elements: $fakeElements);
+            expect($sut)->toBeInstanceOf(Result::class);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+    });
+
+    describe('::validateTagValue()', function () {
+        test('Should be true if provided value is a fone', function () {
+            $fakeTagValue = '+5599999999999';
+            $sut = Telefone::validateTagValue($fakeTagValue);
+            expect($sut->isSuccess())->toBeTruthy();
+        });
+
+        test('Should be fail if a string whit letters is provided', function () {
+            $fakeTagValue = '105555A';
+            $sut = Telefone::validateTagValue($fakeTagValue);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be fail if a string fewer than six letters is provided', function () {
+            $fakeTagValue = '10855';
+            $sut = Telefone::validateTagValue($fakeTagValue);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+
+        test('Should be fail if a string more than sixteen letters is provided', function () {
+            $fakeTagValue = '+5555111111110544';
+            $sut = Telefone::validateTagValue($fakeTagValue);
+            expect($sut->isSuccess())->toBeFalsy();
+            expect($sut->getError())->toBeInstanceOf(ValidationError::class);
+        });
+    });
+});
