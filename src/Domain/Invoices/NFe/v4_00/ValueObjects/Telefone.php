8420a48b276e3ef6ca3eebb990b38e3a5f75f43b
salviorodrigo <rodrigosalvio@hotmail.com>
2025-12-30 19:44:24 -0400

    feat: add Telefone class

---
+++ src/Domain/Invoices/NFe/v4_00/ValueObjects/Telefone.php
@@ -0,0 +1,114 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * MOC      7.0
+ * #        45
+ * ID       C16
+ * Campo    fone
+ * Desc     Telefone
+ * Tam      6-14
+ * OBS:
+ * Tag opcional.
+ */
+
+namespace BradiNfeApi\Domain\Invoices\NFe\v4_00\ValueObjects;
+
+use BradiNfeApi\Common\Exceptions\ValidationError;
+use BradiNfeApi\Common\Result;
+use BradiNfeApi\Domain\Common\Services\ValidationService;
+use BradiNfeApi\Domain\Common\Validators\IsNumericValidator;
+use BradiNfeApi\Domain\Common\Validators\IsStringValidator;
+use BradiNfeApi\Domain\Common\Validators\MaxStringLengthValidator;
+use BradiNfeApi\Domain\Common\Validators\MinStringLengthValidator;
+use BradiNfeApi\Domain\Invoices\NFe\Exceptions\XmlElementWithAttributesError;
+use BradiNfeApi\Domain\Invoices\NFe\Exceptions\XmlElementWithElementsError;
+use BradiNfeApi\Domain\Invoices\Protocols\DFeElement;
+
+final class Telefone extends DFeElement
+{
+    public static string $tagName = 'fone';
+
+    private function __construct(
+        public readonly string $value,
+        public readonly string $xmlString) {}
+
+    public static function parseXmlString(mixed $rawData): Result
+    {
+        $validationService = new ValidationService([
+            new IsStringValidator(self::$tagName),
+        ]);
+        $validationServiceResponse = $validationService->verify($rawData);
+        if (! $validationServiceResponse->isSuccess()) {
+            return $validationServiceResponse;
+        }
+
+        $xmlTagString = self::xmlParser()->getTag($rawData, self::$tagName);
+        $tagValue = self::xmlParser()->getTagValue($xmlTagString, self::$tagName);
+        $validationValueResponse = self::validateTagValue($tagValue);
+
+        if (! $validationValueResponse->isSuccess()) {
+            return $validationValueResponse;
+        }
+
+        return Result::makeSuccess(
+            new self(
+                $tagValue,
+                $xmlTagString
+            )
+        );
+    }
+
+    public static function create(string $tagValue = '', array $elements = [], array $attributes = []): Result
+    {
+
+        if (count($attributes) > 0) {
+            return Result::makeFailure(
+                new ValidationError([
+                    new XmlElementWithAttributesError(self::$tagName),
+                ])
+            );
+        }
+
+        if (count($elements) > 0) {
+            return Result::makeFailure(
+                new ValidationError([
+                    new XmlElementWithElementsError(self::$tagName),
+                ])
+            );
+        }
+
+        $validationValueResponse = self::validateTagValue($tagValue);
+
+        if (! $validationValueResponse->isSuccess()) {
+            return $validationValueResponse;
+        }
+
+        return Result::makeSuccess(
+            new self(
+                $tagValue,
+                self::generateXmlString(tagValue: $tagValue)
+            )
+        );
+    }
+
+    public static function validateTagValue(string $tagValue): Result
+    {
+        $validationService = new ValidationService([
+            new IsStringValidator(self::$tagName),
+            new IsNumericValidator(self::$tagName),
+            new MaxStringLengthValidator(self::$tagName, 14),
+            new MinStringLengthValidator(self::$tagName, 6),
+        ]);
+
+        $validationServiceResponse = $validationService->verify($tagValue);
+
+        if ($validationServiceResponse->isSuccess() || $tagValue == '') {
+            return Result::makeSuccess();
+        }
+
+        return $validationServiceResponse;
+
+    }
+}
