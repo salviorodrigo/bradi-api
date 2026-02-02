6cbda4d3a7939ef5a38c8d1742fd1255e25be277
salviorodrigo <rodrigosalvio@hotmail.com>
2026-02-01 21:17:25 -0400

    feat: add CodigoBarras class

---
+++ src/Domain/Invoices/NFe/v4_00/ValueObjects/CodigoBarras.php
@@ -0,0 +1,119 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * MOC      7.0
+ * #        102
+ * ID       I03
+ * Campo    cEAN
+ * Desc     GTIN (Global Trade Item Number) do produto, antigo código EAN ou código de barras
+ * Tam      0, 8, 12, 13 ou 14
+ * OBS:
+ * Preencher com o código GTIN-8, GTIN-12, GTIN-13 ou
+ * GTIN-14 (antigos códigos EAN, UPC e DUN-14);
+ * Para produtos que não possuem código de barras com
+ * GTIN, deve ser informado o literal “SEM GTIN”; (atualizado NT 2017/001)
+ */
+
+namespace BradiNfeApi\Domain\Invoices\NFe\v4_00\ValueObjects;
+
+use BradiNfeApi\Common\Exceptions\ValidationError;
+use BradiNfeApi\Common\Result;
+use BradiNfeApi\Domain\Common\Services\ValidationService;
+use BradiNfeApi\Domain\Common\Validators\IsStringValidator;
+use BradiNfeApi\Domain\Common\Validators\IsXmlTagValidator;
+use BradiNfeApi\Domain\Common\Validators\NotNullValidator;
+use BradiNfeApi\Domain\Common\Validators\StringLengthValidator;
+use BradiNfeApi\Domain\Invoices\NFe\Exceptions\XmlElementWithAttributesError;
+use BradiNfeApi\Domain\Invoices\NFe\Exceptions\XmlElementWithElementsError;
+use BradiNfeApi\Domain\Invoices\Protocols\DFeElement;
+
+final class CodigoBarras extends DFeElement
+{
+    public static string $tagName = 'cEAN';
+
+    private function __construct(
+        public readonly string $value,
+        public readonly string $xmlString) {}
+
+    public static function parseXmlString(mixed $rawData): Result
+    {
+        $typeValidator = new ValidationService([
+            new IsStringValidator(self::$tagName),
+            new NotNullValidator(self::$tagName),
+            new IsXmlTagValidator(self::$tagName),
+        ]);
+
+        $typeValidatorResponse = $typeValidator->verify($rawData);
+        if (! $typeValidatorResponse->isSuccess()) {
+            return $typeValidatorResponse;
+        }
+
+        $xmlTagString = self::xmlParser()->getTag(strval($rawData), self::$tagName);
+        $tagValue = self::xmlParser()->getTagValue($xmlTagString, self::$tagName);
+        $validationValueResponse = self::validateTagValue($tagValue);
+        if (! $validationValueResponse->isSuccess()) {
+            return $validationValueResponse;
+        }
+
+        return Result::makeSuccess(
+            new self(
+                $tagValue,
+                $xmlTagString
+            )
+        );
+    }
+
+    public static function create(string $tagValue = '', array $elements = [], array $attributes = []): Result
+    {
+
+        if (count($attributes) > 0) {
+            return Result::makeFailure(
+                new ValidationError([
+                    new XmlElementWithAttributesError(self::$tagName),
+                ])
+            );
+        }
+
+        if (count($elements) > 0) {
+            return Result::makeFailure(
+                new ValidationError([
+                    new XmlElementWithElementsError(self::$tagName),
+                ])
+            );
+        }
+
+        if ($tagValue === '') {
+            $tagValue = 'SEM GTIN';
+        }
+
+        $validationValueResponse = self::validateTagValue($tagValue);
+        if (! $validationValueResponse->isSuccess()) {
+            return $validationValueResponse;
+        }
+
+        return Result::makeSuccess(
+            new self(
+                $tagValue,
+                self::generateXmlString(tagValue: $tagValue)
+            )
+        );
+    }
+
+    public static function validateTagValue(string $tagValue): Result
+    {
+        $validationService = new ValidationService([
+            new IsStringValidator(self::$tagName),
+            new NotNullValidator(self::$tagName),
+            new StringLengthValidator(self::$tagName, 8, 12, 13, 14),
+        ]);
+
+        $validationServiceResponse = $validationService->verify($tagValue);
+        if (! $validationServiceResponse->isSuccess()) {
+            return $validationServiceResponse;
+        }
+
+        return Result::makeSuccess();
+    }
+}
