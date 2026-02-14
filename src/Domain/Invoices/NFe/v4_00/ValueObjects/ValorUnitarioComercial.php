b36682cf94e5cc9efcaed498b121ed621bfea400
salviorodrigo <rodrigosalvio@hotmail.com>
2026-02-14 13:38:59 -0400

    feat: add ValorUnitarioComercial class

---
+++ src/Domain/Invoices/NFe/v4_00/ValueObjects/ValorUnitarioComercial.php
@@ -0,0 +1,120 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * MOC      7.0
+ * #        108
+ * ID       I09
+ * Campo    vUnCom
+ * Desc     Valor Unitário de Comercialização
+ * Tam      1-6
+ * OBS:
+ * Informar o valor unitário de comercialização do produto, campo
+ * meramente informativo, o contribuinte pode utilizar a precisão
+ * desejada (0-10 decimais). Para efeitos de cálculo, o valor unitário
+ * será obtido pela divisão do valor do produto pela quantidade
+ * comercial. (v2.0)
+ */
+
+namespace BradiNfeApi\Domain\Invoices\NFe\v4_00\ValueObjects;
+
+use BradiNfeApi\Common\Exceptions\ValidationError;
+use BradiNfeApi\Common\Result;
+use BradiNfeApi\Domain\Common\Services\ValidationService;
+use BradiNfeApi\Domain\Common\Validators\IsDecimalValidator;
+use BradiNfeApi\Domain\Common\Validators\IsStringValidator;
+use BradiNfeApi\Domain\Common\Validators\IsXmlTagValidator;
+use BradiNfeApi\Domain\Common\Validators\MaxValueValidator;
+use BradiNfeApi\Domain\Common\Validators\MinValueValidator;
+use BradiNfeApi\Domain\Common\Validators\NotNullValidator;
+use BradiNfeApi\Domain\Invoices\NFe\Exceptions\XmlElementWithAttributesError;
+use BradiNfeApi\Domain\Invoices\NFe\Exceptions\XmlElementWithElementsError;
+use BradiNfeApi\Domain\Invoices\Protocols\DFeElement;
+
+final class ValorUnitarioComercial extends DFeElement
+{
+    public static string $tagName = 'vUnCom';
+
+    private function __construct(
+        public readonly string $value,
+        public readonly string $xmlString) {}
+
+    public static function parseXmlString(mixed $rawData): Result
+    {
+        $typeValidator = new ValidationService([
+            new IsStringValidator(self::$tagName),
+            new NotNullValidator(self::$tagName),
+            new IsXmlTagValidator(self::$tagName),
+        ]);
+
+        $typeValidatorResponse = $typeValidator->verify($rawData);
+        if (! $typeValidatorResponse->isSuccess()) {
+            return $typeValidatorResponse;
+        }
+
+        $xmlTagString = self::xmlParser()->getTag(strval($rawData), self::$tagName);
+        $tagValue = self::xmlParser()->getTagValue($xmlTagString, self::$tagName);
+        $validationValueResponse = self::validateTagValue($tagValue);
+        if (! $validationValueResponse->isSuccess()) {
+            return $validationValueResponse;
+        }
+
+        return Result::makeSuccess(
+            new self(
+                $tagValue,
+                $xmlTagString
+            )
+        );
+    }
+
+    public static function create(string $tagValue = '', array $elements = [], array $attributes = []): Result
+    {
+
+        if (count($attributes) > 0) {
+            return Result::makeFailure(
+                new ValidationError([
+                    new XmlElementWithAttributesError(self::$tagName),
+                ])
+            );
+        }
+
+        if (count($elements) > 0) {
+            return Result::makeFailure(
+                new ValidationError([
+                    new XmlElementWithElementsError(self::$tagName),
+                ])
+            );
+        }
+
+        $validationValueResponse = self::validateTagValue($tagValue);
+        if (! $validationValueResponse->isSuccess()) {
+            return $validationValueResponse;
+        }
+
+        return Result::makeSuccess(
+            new self(
+                $tagValue,
+                self::generateXmlString(tagValue: $tagValue)
+            )
+        );
+    }
+
+    public static function validateTagValue(string $tagValue): Result
+    {
+        $validationService = new ValidationService([
+            new IsStringValidator(self::$tagName),
+            new NotNullValidator(self::$tagName),
+            new IsDecimalValidator(self::$tagName, 11, 10),
+            new MaxValueValidator(self::$tagName, 99999999999.9999999999),
+            new MinValueValidator(self::$tagName, 0.0000000001),
+        ]);
+
+        $validationServiceResponse = $validationService->verify($tagValue);
+        if (! $validationServiceResponse->isSuccess()) {
+            return $validationServiceResponse;
+        }
+
+        return Result::makeSuccess();
+    }
+}
