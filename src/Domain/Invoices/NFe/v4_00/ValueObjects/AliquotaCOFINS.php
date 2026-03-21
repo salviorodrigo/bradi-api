7fd91a7fb02ccf4f7854fa575aa28c70d82ec06f
salviorodrigo <rodrigosalvio@hotmail.com>
2026-03-21 08:22:57 -0400

    feat: add AliquotaCOFINS value object

---
+++ src/Domain/Invoices/NFe/v4_00/ValueObjects/AliquotaCOFINS.php
@@ -0,0 +1,103 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * MOC      7.0
+ * ID       S08
+ * Campo    pCOFINS
+ * Desc     Aliquota da COFINS (em percentual)
+ * Tam      3v2-4
+ * OBS:
+ * Percentual da aliquota da COFINS.
+ */
+
+namespace BradiNfeApi\Domain\Invoices\NFe\v4_00\ValueObjects;
+
+use BradiNfeApi\Common\Services\ValidationService;
+use BradiNfeApi\Common\ValueObjects\Result;
+use BradiNfeApi\Domain\Common\Validators\IsDecimalValidator;
+use BradiNfeApi\Domain\Common\Validators\MaxValueValidator;
+use BradiNfeApi\Domain\Common\Validators\MinValueValidator;
+use BradiNfeApi\Domain\Common\Validators\NotNullValidator;
+use BradiNfeApi\Domain\Invoices\Protocols\DFeElement;
+use BradiNfeApi\Domain\Invoices\Protocols\DFeValueElement;
+use InvalidArgumentException;
+
+final class AliquotaCOFINS extends DFeValueElement
+{
+    public static string $tagName = 'pCOFINS';
+
+    private function __construct(public readonly string $xmlString)
+    {
+        $this->value = self::xmlParser($xmlString)->getTextContent();
+    }
+
+    public static function parse(mixed $rawData, string $parentFieldURI = '', string $method = __METHOD__): Result
+    {
+        $fieldURI = $parentFieldURI == '' ? self::$tagName : $parentFieldURI . '.' . self::$tagName;
+        $typeValidatorResponse = self::validateDataType($rawData, $fieldURI, $method);
+        if ($typeValidatorResponse->isFailure()) {
+            return $typeValidatorResponse;
+        }
+
+        $xmlString = self::xmlParser(strval($rawData))->getFirst(self::$tagName);
+        $tagAttributesValidationResponse = self::validateTagAttributes($xmlString, $fieldURI, $method);
+        if ($tagAttributesValidationResponse->isFailure()) {
+            return $tagAttributesValidationResponse;
+        }
+
+        $tagElementsValidationResponse = self::validateTagElements($xmlString, $fieldURI, $method);
+        if ($tagElementsValidationResponse->isFailure()) {
+            return $tagElementsValidationResponse;
+        }
+
+        $tagValueValidationResponse = self::validateTagValue($xmlString, $fieldURI, $method);
+        if (! $tagValueValidationResponse->isSuccess()) {
+            return $tagValueValidationResponse;
+        }
+
+        return Result::makeSuccess(
+            new self(
+                $xmlString
+            )
+        );
+    }
+
+    public static function create(string $tagValue = '', array $elements = [], array $attributes = [], string $parentFieldURI = '', string $method = __METHOD__): Result
+    {
+        foreach ($attributes as $attributeName => $attributeValue) {
+            if (! is_string($attributeName)) {
+                throw new InvalidArgumentException('Attribute name must be a string. Found: ' . gettype($attributeName) . ' with value: ' . strval($attributeName));
+            }
+            if (! is_string($attributeValue)) {
+                throw new InvalidArgumentException('Attribute value must be a string. Found: ' . gettype($attributeValue) . ' with value: ' . strval($attributeValue));
+            }
+        }
+
+        foreach ($elements as $element) {
+            if (! $element instanceof DFeElement) {
+                throw new InvalidArgumentException('All elements must be instances of DFeElement. Found: ' . gettype($element) . ' with value: ' . strval($element));
+            }
+        }
+
+        return self::parse(self::generateXmlString($tagValue, $elements, $attributes), $parentFieldURI, $method);
+    }
+
+    protected static function validateTagValue(string $xmlString, string $fieldURI = '', string $method = __METHOD__): Result
+    {
+        $tagValue = self::xmlParser($xmlString)->getTextContent();
+        $validationService = new ValidationService(
+            [
+                NotNullValidator::class => [],
+                IsDecimalValidator::class => [3, 4],
+                MaxValueValidator::class => [100],
+                MinValueValidator::class => [0],
+            ],
+            $fieldURI,
+            $method
+        );
+
+        return $validationService->verify($tagValue);
+    }
+}
