b8ee12403c11a2e29c12d18a1fa105539655d380
salviorodrigo <rodrigosalvio@hotmail.com>
2026-04-05 13:55:27 -0400

    feat: add XmlIterator protocol

---
+++ src/Domain/Xml/Protocols/XmlIterator.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BradiNfeApi\Domain\Xml\Protocols;
+
+use BradiNfeApi\Domain\Common\Protocols\ApiError;
+use BradiNfeApi\Domain\Common\ValueObjects\Result;
+
+interface XmlIterator
+{
+    public mixed $candidate {get; }
+
+    public string $name {get; }
+
+    public string $value {get; }
+
+    public string $textContent {get; }
+
+    /** @param array<string,string> $attributes List of $attributeName => $attributeValue */
+    public array $attributes {get; }
+
+    /** @return array<string> List of child XML strings */
+    public array $children {get; }
+
+    public function get(string $tagName): string;
+
+    public function list(string $tagName): array;
+
+    /** @return Result<XmlIterator|ApiError> */
+    public function loadFrom(mixed $candidate): Result;
+}
