d0e6a2d68467ceeb8c5f5d2ef6b0929fdd8831cb
salviorodrigo <rodrigosalvio@hotmail.com>
2026-06-06 14:44:50 +0000

    feat: add RootTagValidator for validating XML root tags

---
+++ src/Domain/Invoices/Validators/RootTagValidator.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BradiNfeApi\Domain\Invoices\Validators;
+
+use BradiNfeApi\Domain\Common\Protocols\Validator;
+use BradiNfeApi\Domain\Common\ValueObjects\Result;
+use BradiNfeApi\Domain\Xml\ValueObjects\Element;
+use InvalidArgumentException;
+
+final class RootTagValidator implements Validator
+{
+    public function __construct(
+        public readonly string $rootTagName,
+    ) {}
+
+    public function check(mixed $candidate): Result
+    {
+        if (! $candidate instanceof Element) {
+            return Result::makeFailure(new InvalidArgumentException('candidate must be an Element instance.'));
+        }
+
+        if ($candidate->name === $this->rootTagName) {
+            return Result::makeSuccess();
+        }
+
+        return Result::makeFailure(new InvalidArgumentException(sprintf('Tag "%s" expected.', $this->rootTagName)));
+    }
+}
