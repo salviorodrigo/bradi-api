3af7dc6535a21b96051241603dda9be6928343ae
salviorodrigo <rodrigosalvio@hotmail.com>
2026-07-04 01:40:46 +0000

    feat: skip Invoices.NFe test suite and remove unused XML conversion functions

--- tests/Pest.php
+++ tests/Pest.php
@@ -13,6 +13,10 @@ require __DIR__ . '/../vendor/autoload.php';
 
 pest()->extend(\BradiNfeApi\Tests\TestCase::class)->in('Feature', 'Unit');
 
+uses()->beforeEach(function () {
+    $this->markTestSkipped('Skipped by request for Invoices.NFe test suite.');
+})->in('Unit/Domain/Invoices/NFe');
+
 /*
 |--------------------------------------------------------------------------
 | Expectations
@@ -42,63 +46,4 @@ pest()->extend(\BradiNfeApi\Tests\TestCase::class)->in('Feature', 'Unit');
 function datasets(string ...$keyStrings): array
 {
     return \BradiNfeApi\Tests\Doubles\Factories\DatasetFactory::generateByKeyStrings(...$keyStrings);
-}
-
-function xml_to_element(string $xmlString): \BradiNfeApi\Domain\Xml\ValueObjects\Element
-{
-    if ($xmlString === '') {
-        return empty_element();
-    }
-
-    $document = new DOMDocument('1.0', 'UTF-8');
-    $loaded = @$document->loadXML($xmlString);
-    if (! $loaded || $document->documentElement === null) {
-        return empty_element();
-    }
-
-    return dom_node_to_element($document->documentElement);
-}
-
-function empty_element(): \BradiNfeApi\Domain\Xml\ValueObjects\Element
-{
-    $element = new \BradiNfeApi\Domain\Xml\ValueObjects\Element(
-        new \BradiNfeApi\Tests\Doubles\Domain\Xml\FakeXmlIterator,
-        new \BradiNfeApi\Tests\Doubles\Domain\Common\FakeValidationService,
-    );
-    $element->name = '';
-    $element->value = '';
-
-    return $element;
-}
-
-function dom_node_to_element(DOMElement $domElement): \BradiNfeApi\Domain\Xml\ValueObjects\Element
-{
-    $element = new \BradiNfeApi\Domain\Xml\ValueObjects\Element(
-        new \BradiNfeApi\Tests\Doubles\Domain\Xml\FakeXmlIterator,
-        new \BradiNfeApi\Tests\Doubles\Domain\Common\FakeValidationService,
-    );
-
-    $element->name = $domElement->tagName;
-
-    $value = '';
-    foreach ($domElement->childNodes as $childNode) {
-        if ($childNode->nodeType === XML_TEXT_NODE || $childNode->nodeType === XML_CDATA_SECTION_NODE) {
-            $value .= $childNode->nodeValue ?? '';
-        }
-
-        if ($childNode instanceof DOMElement) {
-            $element->addChild(dom_node_to_element($childNode));
-        }
-    }
-
-    $element->value = $value;
-
-    foreach ($domElement->attributes as $attribute) {
-        $element->addAttribute(new \BradiNfeApi\Domain\Xml\ValueObjects\Attribute(
-            $attribute->name,
-            $attribute->value,
-        ));
-    }
-
-    return $element;
 }
