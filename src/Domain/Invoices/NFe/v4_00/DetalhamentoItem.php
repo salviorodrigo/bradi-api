9fb418f61accae9a256e2dec42ed6ea19fcc4bc2
salviorodrigo <rodrigosalvio@hotmail.com>
2026-07-08 20:44:14 +0000

    feat: adicionar classe DetalhamentoItem com validações e testes correspondentes

---
+++ src/Domain/Invoices/NFe/v4_00/DetalhamentoItem.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * MOC      7.0
+ * #        98
+ * ID       H01
+ * Campo    det
+ * Desc     Detalhamento de Produtos e Serviços
+ * Tam      1-990
+ * OBS:
+ */
+
+namespace BradiNfeApi\Domain\Invoices\NFe\v4_00;
+
+use BradiNfeApi\Domain\Common\Protocols\Validator;
+use BradiNfeApi\Domain\Invoices\Templates\DFeElement;
+use BradiNfeApi\Domain\Invoices\Traits\ValidatesDFeGroupElement;
+use BradiNfeApi\Domain\Invoices\Validators\RequiredAttributeValidator;
+use BradiNfeApi\Domain\Invoices\Validators\RequiredTagValidator;
+
+final class DetalhamentoItem extends DFeElement
+{
+    use ValidatesDFeGroupElement;
+
+    public const string FIELD_NAME = 'det';
+
+    public NumeroItem $nItem;
+    public Produto $prod;
+    public Imposto $imposto;
+
+    protected function tagElementsValidators(): array
+    {
+        return [
+            new RequiredTagValidator([ 'prod', 'imposto' ]),
+        ];
+    }
+
+    /** @return array<Validator> */
+    protected function tagAttributesValidators(): array
+    {
+        return [
+            new RequiredAttributeValidator(['nItem']),
+        ];
+    }
+}
---
+++ tests/Unit/Domain/Invoices/NFe/v4_00/DetalhamentoItemTest.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+use BradiNfeApi\Domain\Common\Protocols\ApiError;
+use BradiNfeApi\Domain\Common\ValueObjects\Result;
+use BradiNfeApi\Domain\Invoices\NFe\v4_00\DetalhamentoItem;
+use BradiNfeApi\Domain\Xml\ValueObjects\Element;
+use BradiNfeApi\Tests\Doubles\Domain\Common\FakeValidationService;
+use BradiNfeApi\Tests\TestCase;
+
+describe('DetalhamentoItem', function () {
+
+    beforeEach(function () {
+        /** @var TestCase $this */
+        $this->sut = new DetalhamentoItem;
+    });
+
+    describe('::parse()', function () {
+        test('Should succeed with dataset :dataset', function ($candidate) {
+            $xmlElement = new Element(new FakeValidationService);
+            $xmlElement->parse($candidate);
+            $sutResponse = $this->sut->parseFromXmlElement($xmlElement);
+            expect($sutResponse)->toBeInstanceOf(Result::class);
+            if ($sutResponse->isFailure()) {
+                $this->fail(json_encode($sutResponse->getError()));
+            }
+            expect($sutResponse->getData())->toBeInstanceOf(DetalhamentoItem::class);
+            expect($sutResponse->getData()->value)->toBe('');
+            expect((string) $sutResponse->getData())->toBe($candidate);
+        })->with(datasets('dfes.nfe.element_tags.' . DetalhamentoItem::FIELD_NAME . '.valid'));
+
+        test('Should fail with dataset :dataset', function ($candidate) {
+            $xmlElement = new Element(new FakeValidationService);
+            $xmlElement->parse($candidate);
+            $sutResponse = $this->sut->parseFromXmlElement($xmlElement);
+            expect($sutResponse)->toBeInstanceOf(Result::class);
+            if ($sutResponse->isSuccess()) {
+                $this->fail(json_encode($sutResponse->getData()));
+            }
+            expect($sutResponse)->toBeInstanceOf(Result::class);
+            expect($sutResponse->getError())->toBeInstanceOf(ApiError::class);
+        })->with(datasets('dfes.nfe.element_tags.' . DetalhamentoItem::FIELD_NAME . '.invalid'));
+    });
+});
