42300e06a28fb567e6ab3272315216dad584c1e7
salviorodrigo <rodrigosalvio@hotmail.com>
2026-07-08 18:53:48 +0000

    refactor: reorganizar a classe DFeElementCollection para melhorar a estrutura e a legibilidade do código

--- src/Domain/Invoices/Templates/DFeElementCollection.php
+++ src/Domain/Invoices/Templates/DFeElementCollection.php
@@ -4,34 +4,65 @@ declare(strict_types=1);
 
 namespace BradiNfeApi\Domain\Invoices\Templates;
 
+use BradiNfeApi\Domain\Common\Protocols\ApiError;
+use BradiNfeApi\Domain\Common\ValueObjects\Result;
+use BradiNfeApi\Domain\Invoices\Templates\DFeElement;
+use BradiNfeApi\Domain\Xml\ValueObjects\Element;
+use BradiNfeApi\Domain\Xml\ValueObjects\ElementList;
 use Exception;
-use InvalidArgumentException;
 
 abstract class DFeElementCollection
 {
-    public readonly string $fieldURI;
+    private const string BASE_CLASS = '';
+    /** @var DFeElement[] */
+    public array $collection = [];
+    private DFeElement $baseClass;
 
-    /**
-     * @var DFeElement[]
-     */
-    protected array $collection = [];
+    final public function __construct(private readonly string $parentFieldURI = ''){}
 
-    public function __construct(string $parentFieldURI, public readonly string $tagName)
+    /** @return Result<DFeElement|ApiError> */
+    final public function parseFromXmlElement(Element|ElementList $elementOrElementList): Result
     {
-        $this->fieldURI = $parentFieldURI === '' ? $tagName : $parentFieldURI . '.' . $tagName;
-    }
+        $elements = $this->normalizeElements($elementOrElementList);
+        foreach ($elements as $element) {
+            $this->resetBaseClass();
+            $dfeElement = $this->baseClass->parseFromXmlElement($element);
+            if ($dfeElement->isFailure()) {
+                return $dfeElement;
+            }
 
-    public function add(DFeElement $dfe, string $key): void
-    {
-        if ($dfe::TAG_NAME !== $this->tagName) {
-            throw new InvalidArgumentException(sprintf('Expected tag name "%s", got "%s".', $this->tagName, $dfe::TAG_NAME));
+            $this->collection[] = $dfeElement->getData();
         }
 
-        $this->collection[] = $dfe;
+        return Result::makeSuccess($this);
     }
 
     public function __toString()
     {
         throw new Exception('Not implemented');
     }
+
+    private function resetBaseClass(): void
+    {
+        if (class_exists(static::BASE_CLASS) === false) {
+            throw new Exception('BASE_CLASS constant must be defined in the child class.');
+        }
+
+        if (is_subclass_of(static::BASE_CLASS, DFeElement::class) === false) {
+            throw new Exception('BASE_CLASS must be a subclass of DFeElement.');
+        }
+
+        $this->baseClass = new (static::BASE_CLASS)($this->parentFieldURI);
+    }
+
+    private function normalizeElements(Element|ElementList $elementOrElementList): array
+    {
+        if ($elementOrElementList instanceof Element) {
+            return [$elementOrElementList];
+        }
+
+        return $elementOrElementList->records;
+    }
+
+    
 }
