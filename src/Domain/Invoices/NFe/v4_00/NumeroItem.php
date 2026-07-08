f1560ed71e38470547c79495f046a53a8831c0fd
salviorodrigo <rodrigosalvio@hotmail.com>
2026-07-08 19:31:21 +0000

    feat: adicionar a classe NumeroItem para validação do número do item na NFe

---
+++ src/Domain/Invoices/NFe/v4_00/NumeroItem.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * MOC      7.0
+ * #        98
+ * ID       H01
+ * Campo    nItem
+ * Desc     Número do Item
+ * Tam      1-3
+ * OBS:
+ */
+
+namespace BradiNfeApi\Domain\Invoices\NFe\v4_00;
+
+use BradiNfeApi\Domain\Common\Validators\IsNumericValidator;
+use BradiNfeApi\Domain\Common\Validators\MaxValueValidator;
+use BradiNfeApi\Domain\Common\Validators\MinValueValidator;
+use BradiNfeApi\Domain\Invoices\Templates\DFeAttribute;
+
+final class NumeroItem extends DFeAttribute
+{
+    public const string ATTRIBUTE_NAME = 'nItem';
+
+    protected function attributeValueValidators(): array
+    {
+        return [
+            new IsNumericValidator(),
+            new MinValueValidator(1),
+            new MaxValueValidator(990)
+        ];
+    }
+}
