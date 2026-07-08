e804986e67fbba4b94e9e1be32ad8a1277c3d9e5
salviorodrigo <rodrigosalvio@hotmail.com>
2026-07-08 19:31:31 +0000

    feat: adicionar testes para a classe NumeroItem e suas funcionalidades de parsing

---
+++ tests/Unit/Domain/Invoices/NFe/v4_00/NumeroItemTest.php
@@ -0,0 +1,93 @@
+<?php
+
+declare(strict_types=1);
+
+use BradiNfeApi\Domain\Common\Protocols\ApiError;
+use BradiNfeApi\Domain\Common\ValueObjects\Result;
+use BradiNfeApi\Domain\Invoices\NFe\v4_00\NumeroItem;
+use BradiNfeApi\Domain\Xml\ValueObjects\Element;
+use BradiNfeApi\Tests\Doubles\Domain\Common\FakeValidationService;
+use BradiNfeApi\Tests\TestCase;
+
+describe('NumeroItem', function () {
+    beforeEach(function () {
+        /** @var TestCase $this */
+        $this->sut = new NumeroItem('infItem');
+    });
+
+    describe('::parseFromXmlElement()', function () {
+        test('Should succeed extracting nItem attribute with valid value :value', function (string $value) {
+            $candidate = sprintf('<infItem nItem="%s"></infItem>', $value);
+            $validationService = new FakeValidationService;
+            $element = new Element($validationService);
+            $parsingResult = $element->parse($candidate);
+            if ($parsingResult->isFailure()) {
+                $this->fail(json_encode($parsingResult->getError()));
+            }
+
+            $sutResponse = $this->sut->parseFromXmlElement($parsingResult->getData());
+
+            expect($sutResponse)->toBeInstanceOf(Result::class);
+            if ($sutResponse->isFailure()) {
+                $this->fail(json_encode($sutResponse->getError()));
+            }
+
+            expect($sutResponse->getData())->toBeInstanceOf(NumeroItem::class);
+            expect($sutResponse->getData()->value)->toBe($value);
+            expect((string) $sutResponse->getData())->toBe(sprintf('nItem="%s"', $value));
+        })->with([
+            '1',
+            '500',
+            '990'
+        ]);
+
+        test('Should fail when parent tag does not match', function () {
+            $candidate = '<det nItem="10"></det>';
+            $validationService = new FakeValidationService;
+            $element = new Element($validationService);
+            $parsingResult = $element->parse($candidate);
+            if ($parsingResult->isFailure()) {
+                $this->fail(json_encode($parsingResult->getError()));
+            }
+
+            $sutResponse = $this->sut->parseFromXmlElement($parsingResult->getData());
+
+            expect($sutResponse)->toBeInstanceOf(Result::class);
+            if ($sutResponse->isSuccess()) {
+                $this->fail(json_encode($sutResponse->getData()));
+            }
+
+            expect($sutResponse->getError())->toBeInstanceOf(ApiError::class);
+        });
+
+        test('Should fail when nItem is outside allowed range or not numeric :value', function (string $value) {
+            $candidate = sprintf('<infItem nItem="%s"></infItem>', $value);
+            $validationService = new FakeValidationService;
+            $element = new Element($validationService);
+            $parsingResult = $element->parse($candidate);
+            if ($parsingResult->isFailure()) {
+                $this->fail(json_encode($parsingResult->getError()));
+            }
+
+            $sutResponse = $this->sut->parseFromXmlElement($parsingResult->getData());
+
+            expect($sutResponse)->toBeInstanceOf(Result::class);
+            if ($sutResponse->isSuccess()) {
+                $this->fail(json_encode($sutResponse->getData()));
+            }
+
+            expect($sutResponse->getError())->toBeInstanceOf(ApiError::class);
+        })->with([
+            '0',
+            '991',
+            'ABC'
+        ]);
+    });
+
+    describe('::__construct()', function () {
+        test('Should throw if parentFieldURI is empty', function () {
+            expect(fn () => new NumeroItem(''))
+                ->toThrow(RuntimeException::class);
+        });
+    });
+});
