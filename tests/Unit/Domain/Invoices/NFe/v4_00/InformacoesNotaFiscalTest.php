0b0eaee323a32787a832c74652f62a98ce609888
salviorodrigo <rodrigosalvio@hotmail.com>
2026-07-11 03:01:52 +0000

    test(nfe): assert InformacoesNotaFiscal extends DFeElement

--- tests/Unit/Domain/Invoices/NFe/v4_00/InformacoesNotaFiscalTest.php
+++ tests/Unit/Domain/Invoices/NFe/v4_00/InformacoesNotaFiscalTest.php
@@ -2,10 +2,17 @@
 
 declare(strict_types=1);
 
+use BradiNfeApi\Domain\Invoices\NFe\v4_00\InformacoesNotaFiscal;
+use BradiNfeApi\Domain\Invoices\Templates\DFeElement;
+
 describe('InformacoesNotaFiscal', function () {
-    describe('::parse()', function () {
+    describe('::class', function () {
         test('Should succeed if InformacoesNotaFiscal is declared', function () {
             expect(class_exists('BradiNfeApi\Domain\Invoices\NFe\v4_00\InformacoesNotaFiscal'))->toBeTrue();
+        });
+
+        test('Should succeed if InformacoesNotaFiscal extends DFeElement', function () {
+            expect(is_subclass_of(InformacoesNotaFiscal::class, DFeElement::class))->toBeTrue();
         });
     });
 });
