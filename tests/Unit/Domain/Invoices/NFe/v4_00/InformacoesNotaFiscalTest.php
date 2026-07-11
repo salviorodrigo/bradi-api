60db6d6335e39c906f8736bfd997064ce0088e4f
salviorodrigo <rodrigosalvio@hotmail.com>
2026-07-11 04:04:45 +0000

    test(nfe): import child element classes in InformacoesNotaFiscal test

--- tests/Unit/Domain/Invoices/NFe/v4_00/InformacoesNotaFiscalTest.php
+++ tests/Unit/Domain/Invoices/NFe/v4_00/InformacoesNotaFiscalTest.php
@@ -2,6 +2,9 @@
 
 declare(strict_types=1);
 
+use BradiNfeApi\Domain\Invoices\NFe\v4_00\Destinatario;
+use BradiNfeApi\Domain\Invoices\NFe\v4_00\DetalhamentoItem;
+use BradiNfeApi\Domain\Invoices\NFe\v4_00\Emitente;
 use BradiNfeApi\Domain\Invoices\NFe\v4_00\IdentificacaoNF;
 use BradiNfeApi\Domain\Invoices\NFe\v4_00\InformacoesNotaFiscal;
 use BradiNfeApi\Domain\Invoices\Templates\DFeElement;
@@ -22,8 +25,47 @@ describe('InformacoesNotaFiscal', function () {
         });
 
         test('Should succeed if InformacoesNotaFiscal::$ide is a IdentificacaoNF::class', function () {
+            $reflection = new ReflectionClass(InformacoesNotaFiscal::class);
+            $sut = $reflection->getProperty('ide');
+
+            expect($sut->getType()->getName())->toBe(IdentificacaoNF::class);
+
+        });
+
+        test('Should succeed if InformacoesNotaFiscal has $emit attribute', function () {
             $informacoesNotaFiscal = new InformacoesNotaFiscal;
-            expect($informacoesNotaFiscal->ide)->toBeInstanceOf(IdentificacaoNF::class);
+            expect(property_exists($informacoesNotaFiscal, 'emit'))->toBeTrue();
+        });
+
+        test('Should succeed if InformacoesNotaFiscal::$emit is a Emitente::class', function () {
+            $reflection = new ReflectionClass(InformacoesNotaFiscal::class);
+            $sut = $reflection->getProperty('emit');
+
+            expect($sut->getType()->getName())->toBe(Emitente::class);
+        });
+
+        test('Should succeed if InformacoesNotaFiscal has $dest attribute', function () {
+            $informacoesNotaFiscal = new InformacoesNotaFiscal;
+            expect(property_exists($informacoesNotaFiscal, 'dest'))->toBeTrue();
+        });
+
+        test('Should succeed if InformacoesNotaFiscal::$dest is a Destinatario::class', function () {
+            $reflection = new ReflectionClass(InformacoesNotaFiscal::class);
+            $sut = $reflection->getProperty('dest');
+
+            expect($sut->getType()->getName())->toBe(Destinatario::class);
+        });
+
+        test('Should succeed if InformacoesNotaFiscal has $det attribute', function () {
+            $informacoesNotaFiscal = new InformacoesNotaFiscal;
+            expect(property_exists($informacoesNotaFiscal, 'det'))->toBeTrue();
+        });
+
+        test('Should succeed if InformacoesNotaFiscal::$det is a DetalhamentoItem::class', function () {
+            $reflection = new ReflectionClass(InformacoesNotaFiscal::class);
+            $sut = $reflection->getProperty('det');
+
+            expect($sut->getType()->getName())->toBe(DetalhamentoItem::class);
         });
     });
 });
