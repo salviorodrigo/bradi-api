4576ad57790e4fe65df424d8e2c848488b3627b0
salviorodrigo <rodrigosalvio@hotmail.com>
2026-07-13 20:55:44 +0000

    refactor: improve code readability

--- tests/Unit/Domain/Invoices/NFe/v4_00/InformacoesNotaFiscalTest.php
+++ tests/Unit/Domain/Invoices/NFe/v4_00/InformacoesNotaFiscalTest.php
@@ -2,70 +2,69 @@
 
 declare(strict_types=1);
 
-use BradiApi\Domain\Invoices\NFe\v4_00\Destinatario;
-use BradiApi\Domain\Invoices\NFe\v4_00\DetalhamentoItem;
-use BradiApi\Domain\Invoices\NFe\v4_00\Emitente;
-use BradiApi\Domain\Invoices\NFe\v4_00\IdentificacaoNF;
 use BradiApi\Domain\Invoices\NFe\v4_00\InformacoesNotaFiscal;
 use BradiApi\Domain\Invoices\Templates\DFeElement;
 
 describe('InformacoesNotaFiscal', function () {
     describe('::class', function () {
         test('Should succeed if InformacoesNotaFiscal is declared', function () {
-            expect(class_exists('BradiApi\Domain\Invoices\NFe\v4_00\InformacoesNotaFiscal'))->toBeTrue();
+            $nameSpace = 'BradiApi\Domain\Invoices\NFe\v4_00';
+            $sut = $nameSpace . '\\InformacoesNotaFiscal';
+            expect(class_exists($sut))->toBeTrue();
         });
 
         test('Should succeed if InformacoesNotaFiscal extends DFeElement', function () {
-            expect(is_subclass_of(InformacoesNotaFiscal::class, DFeElement::class))->toBeTrue();
+            $sut = new InformacoesNotaFiscal;
+            expect(is_subclass_of($sut, DFeElement::class))->toBeTrue();
         });
 
-        test('Should succeed if InformacoesNotaFiscal has $ide attribute', function () {
-            $informacoesNotaFiscal = new InformacoesNotaFiscal;
-            expect(property_exists($informacoesNotaFiscal, 'ide'))->toBeTrue();
+        test('Should succeed if InformacoesNotaFiscal has $ide element', function () {
+            $sut = new InformacoesNotaFiscal;
+            expect($sut)->toHaveProperty('ide');
         });
 
-        test('Should succeed if InformacoesNotaFiscal::$ide is a IdentificacaoNF::class', function () {
+        test('Should succeed if $ide is a subclass of DFeElement::class', function () {
             $reflection = new ReflectionClass(InformacoesNotaFiscal::class);
-            $sut = $reflection->getProperty('ide');
-
-            expect($sut->getType()->getName())->toBe(IdentificacaoNF::class);
-
+            $ide = $reflection->getProperty('ide');
+            $sut = $ide->getType();
+            expect((is_subclass_of($sut->getName(), DFeElement::class)))->toBeTrue();
         });
 
-        test('Should succeed if InformacoesNotaFiscal has $emit attribute', function () {
-            $informacoesNotaFiscal = new InformacoesNotaFiscal;
-            expect(property_exists($informacoesNotaFiscal, 'emit'))->toBeTrue();
+        test('Should succeed if InformacoesNotaFiscal has $emit element', function () {
+            $sut = new InformacoesNotaFiscal;
+            expect($sut)->toHaveProperty('emit');
         });
 
-        test('Should succeed if InformacoesNotaFiscal::$emit is a Emitente::class', function () {
+        test('Should succeed if $emit is a subclass of DFeElement::class', function () {
             $reflection = new ReflectionClass(InformacoesNotaFiscal::class);
-            $sut = $reflection->getProperty('emit');
-
-            expect($sut->getType()->getName())->toBe(Emitente::class);
+            $emit = $reflection->getProperty('emit');
+            $sut = $emit->getType();
+            expect((is_subclass_of($sut->getName(), DFeElement::class)))->toBeTrue();
         });
 
-        test('Should succeed if InformacoesNotaFiscal has $dest attribute', function () {
-            $informacoesNotaFiscal = new InformacoesNotaFiscal;
-            expect(property_exists($informacoesNotaFiscal, 'dest'))->toBeTrue();
+        test('Should succeed if InformacoesNotaFiscal has $dest element', function () {
+            $sut = new InformacoesNotaFiscal;
+            expect($sut)->toHaveProperty('dest');
         });
 
-        test('Should succeed if InformacoesNotaFiscal::$dest is a Destinatario::class', function () {
+        test('Should succeed if $dest is a subclass of DFeElement::class', function () {
             $reflection = new ReflectionClass(InformacoesNotaFiscal::class);
-            $sut = $reflection->getProperty('dest');
-
-            expect($sut->getType()->getName())->toBe(Destinatario::class);
+            $dest = $reflection->getProperty('dest');
+            $sut = $dest->getType();
+            expect((is_subclass_of($sut->getName(), DFeElement::class)))->toBeTrue();
         });
 
-        test('Should succeed if InformacoesNotaFiscal has $det attribute', function () {
-            $informacoesNotaFiscal = new InformacoesNotaFiscal;
-            expect(property_exists($informacoesNotaFiscal, 'det'))->toBeTrue();
+        test('Should succeed if InformacoesNotaFiscal has $det element', function () {
+            $sut = new InformacoesNotaFiscal;
+            expect($sut)->toHaveProperty('det');
         });
 
-        test('Should succeed if InformacoesNotaFiscal::$det is a DetalhamentoItem::class', function () {
+        test('Should succeed if $det is a subclass of DFeElement::class', function () {
             $reflection = new ReflectionClass(InformacoesNotaFiscal::class);
-            $sut = $reflection->getProperty('det');
+            $det = $reflection->getProperty('det');
+            $sut = $det->getType();
 
-            expect($sut->getType()->getName())->toBe(DetalhamentoItem::class);
+            expect((is_subclass_of($sut->getName(), DFeElement::class)))->toBeTrue();
         });
     });
 });
