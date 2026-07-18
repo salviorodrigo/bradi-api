5655459e24dd6203651b70b833517f65cdb86cbf
salviorodrigo <rodrigosalvio@hotmail.com>
2026-07-18 23:05:02 +0000

    feat(Invoices): add AllowedAttributtesValidator::class

---
+++ src/Domain/Invoices/Validators/AllowedAttributesValidator.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BradiApi\Domain\Invoices\Validators;
+
+use BradiApi\Domain\Common\Protocols\Validator;
+use BradiApi\Domain\Common\ValueObjects\Result;
+use BradiApi\Domain\Xml\ValueObjects\Element;
+use InvalidArgumentException;
+
+final class AllowedAttributesValidator implements Validator
+{
+    public function __construct(
+        public readonly array $allowedAttributesName,
+    ) {}
+
+    public function check(mixed $candidate): Result
+    {
+        if (! $candidate instanceof Element) {
+            return Result::makeFailure(new InvalidArgumentException('candidate must be an Element instance.'));
+        }
+
+        $providedAttributesName = array_keys($candidate->attributes->records);
+        foreach ($providedAttributesName as $providedAttribute) {
+            if (! in_array($providedAttribute, $this->allowedAttributesName)) {
+                return Result::makeFailure(new InvalidArgumentException(sprintf(
+                    'attribute "%s" is not allowed.',
+                    $providedAttribute
+                )));
+            }
+        }
+
+        return Result::makeSuccess();
+    }
+}
