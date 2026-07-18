ce80fb475c2f8db67e4c6c07d74aca910618931c
salviorodrigo <rodrigosalvio@hotmail.com>
2026-07-18 22:59:51 +0000

    test(InformacoesNotaFiscal): ensure that parsing fail if unallowed tag is provided

--- src/Domain/Invoices/NFe/v4_00/InformacoesNotaFiscal.php
+++ src/Domain/Invoices/NFe/v4_00/InformacoesNotaFiscal.php
@@ -17,12 +17,15 @@ namespace BradiApi\Domain\Invoices\NFe\v4_00;
 use BradiApi\Domain\Common\Protocols\Validator;
 use BradiApi\Domain\Invoices\NFe\v4_00\ValueObjects\VersaoDocumentoFiscal;
 use BradiApi\Domain\Invoices\Templates\DFeElement;
-use BradiApi\Domain\Invoices\Validators\HasNoTextContentValidator;
+use BradiApi\Domain\Invoices\Traits\ValidatesDFeGroupElement;
+use BradiApi\Domain\Invoices\Validators\AllowedTagsValidator;
 use BradiApi\Domain\Invoices\Validators\RequiredAttributeValidator;
 use BradiApi\Domain\Invoices\Validators\RequiredTagValidator;
 
 final class InformacoesNotaFiscal extends DFeElement
 {
+    use ValidatesDFeGroupElement;
+
     public const string FIELD_NAME = 'infNFe';
 
     public VersaoDocumentoFiscal $versao;
@@ -30,12 +33,6 @@ final class InformacoesNotaFiscal extends DFeElement
     public Emitente $emit;
     public ?Destinatario $dest;
     public DetalhamentoItem $det;
-
-    /** @return array<Validator> */
-    protected function tagValueValidators(): array
-    {
-        return [new HasNoTextContentValidator];
-    }
 
     /** @return array<Validator> */
     protected function tagAttributesValidators(): array
@@ -50,6 +47,7 @@ final class InformacoesNotaFiscal extends DFeElement
     {
         return [
             new RequiredTagValidator(['ide', 'emit', 'det', 'total', 'transp', 'pag']),
+            new AllowedTagsValidator(['ide', 'emit', 'avulsa', 'dest', 'retirada', 'entrega', 'autXML', 'det', 'total', 'transp', 'cobr', 'pag', 'infIntermed', 'infAdic', 'exporta', 'compra', 'cana', 'infRespTec']),
         ];
     }
 }
