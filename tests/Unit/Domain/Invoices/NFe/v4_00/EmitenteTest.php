1c1a5922774f7df0c6aa8aeb77f8f435b52cfeac
salviorodrigo <rodrigosalvio@hotmail.com>
2026-07-19 00:55:16 +0000

    test(EmitenteTest): enhance property validation tests for Emitente class

--- tests/Unit/Domain/Invoices/NFe/v4_00/EmitenteTest.php
+++ tests/Unit/Domain/Invoices/NFe/v4_00/EmitenteTest.php
@@ -2,43 +2,327 @@
 
 declare(strict_types=1);
 
-use BradiApi\Domain\Common\Protocols\ApiError;
-use BradiApi\Domain\Common\ValueObjects\Result;
 use BradiApi\Domain\Invoices\NFe\v4_00\Emitente;
+use BradiApi\Domain\Invoices\Templates\DFeElement;
 use BradiApi\Domain\Xml\ValueObjects\Element;
-use BradiApi\Tests\TestCase;
 
 describe('Emitente', function () {
-
-    beforeEach(function () {
-        /** @var TestCase $this */
-        $this->sut = new Emitente;
+    test('Should succeed if Emitente is declared', function () {
+        $nameSpace = 'BradiApi\Domain\Invoices\NFe\v4_00';
+        $sut = $nameSpace . '\\Emitente';
+        expect(class_exists($sut))->toBeTrue();
     });
 
-    describe('::parse()', function () {
-        test('Should succeed with dataset :dataset', function ($candidate) {
-            $xmlElement = new Element;
-            $xmlElement->parse($candidate);
-            $sutResponse = $this->sut->parseFromXmlElement($xmlElement);
-            expect($sutResponse)->toBeInstanceOf(Result::class);
-            if ($sutResponse->isFailure()) {
-                $this->fail(json_encode($sutResponse->getError()));
-            }
-            expect($sutResponse->getData())->toBeInstanceOf(Emitente::class);
-            expect($sutResponse->getData()->value)->toBe('');
-            expect((string) $sutResponse->getData())->toBe($candidate);
-        })->with(datasets('dfes.nfe.element_tags.' . Emitente::FIELD_NAME . '.valid'));
+    test('Should succeed if Emitente extends DFeElement', function () {
+        $sut = new Emitente;
+        expect(is_subclass_of($sut, DFeElement::class))->toBeTrue();
+    });
 
-        test('Should fail with dataset :dataset', function ($candidate) {
-            $xmlElement = new Element;
-            $xmlElement->parse($candidate);
-            $sutResponse = $this->sut->parseFromXmlElement($xmlElement);
-            expect($sutResponse)->toBeInstanceOf(Result::class);
-            if ($sutResponse->isSuccess()) {
-                $this->fail(json_encode($sutResponse->getData()));
-            }
-            expect($sutResponse)->toBeInstanceOf(Result::class);
-            expect($sutResponse->getError())->toBeInstanceOf(ApiError::class);
-        })->with(datasets('dfes.nfe.element_tags.' . Emitente::FIELD_NAME . '.invalid'));
+    describe('properties', function () {
+        describe('$CNPJ', function () {
+            test('Should be declared', function () {
+                $sut = new Emitente;
+                expect($sut)->toHaveProperty('CNPJ');
+            });
+
+            test('Should be a subclass of DFeElement::class', function () {
+                $reflection = new ReflectionClass(Emitente::class);
+                $reflectedProperty = $reflection->getProperty('CNPJ');
+                $sut = $reflectedProperty->getType();
+                expect((is_subclass_of($sut->getName(), DFeElement::class)))->toBeTrue();
+            });
+
+            test('Should be required', function () {
+                $reflection = new ReflectionClass(Emitente::class);
+                $reflectedProperty = $reflection->getProperty('CNPJ');
+                $sut = $reflectedProperty->getType();
+                expect($sut->allowsNull())->toBeFalse();
+            });
+        });
+
+        describe('$CPF', function () {
+            test('Should be declared', function () {
+                $sut = new Emitente;
+                expect($sut)->toHaveProperty('CPF');
+            });
+
+            test('Should be a subclass of DFeElement::class', function () {
+                $reflection = new ReflectionClass(Emitente::class);
+                $reflectedProperty = $reflection->getProperty('CPF');
+                $sut = $reflectedProperty->getType();
+                expect((is_subclass_of($sut->getName(), DFeElement::class)))->toBeTrue();
+            });
+
+            test('Should be optional', function () {
+                $reflection = new ReflectionClass(Emitente::class);
+                $reflectedProperty = $reflection->getProperty('CPF');
+                $sut = $reflectedProperty->getType();
+                expect($sut->allowsNull())->toBeTrue();
+            });
+        });
+
+        describe('$xNome', function () {
+            test('Should be declared', function () {
+                $sut = new Emitente;
+                expect($sut)->toHaveProperty('xNome');
+            });
+
+            test('Should be a subclass of DFeElement::class', function () {
+                $reflection = new ReflectionClass(Emitente::class);
+                $reflectedProperty = $reflection->getProperty('xNome');
+                $sut = $reflectedProperty->getType();
+                expect((is_subclass_of($sut->getName(), DFeElement::class)))->toBeTrue();
+            });
+
+            test('Should be required', function () {
+                $reflection = new ReflectionClass(Emitente::class);
+                $reflectedProperty = $reflection->getProperty('xNome');
+                $sut = $reflectedProperty->getType();
+                expect($sut->allowsNull())->toBeFalse();
+            });
+        });
+
+        describe('$xFant', function () {
+            test('Should be declared', function () {
+                $sut = new Emitente;
+                expect($sut)->toHaveProperty('xFant');
+            });
+
+            test('Should be a subclass of DFeElement::class', function () {
+                $reflection = new ReflectionClass(Emitente::class);
+                $reflectedProperty = $reflection->getProperty('xFant');
+                $sut = $reflectedProperty->getType();
+                expect((is_subclass_of($sut->getName(), DFeElement::class)))->toBeTrue();
+            });
+
+            test('Should be optional', function () {
+                $reflection = new ReflectionClass(Emitente::class);
+                $reflectedProperty = $reflection->getProperty('xFant');
+                $sut = $reflectedProperty->getType();
+                expect($sut->allowsNull())->toBeTrue();
+            });
+        });
+
+        describe('$IE', function () {
+            test('Should be declared', function () {
+                $sut = new Emitente;
+                expect($sut)->toHaveProperty('IE');
+            });
+
+            test('Should be a subclass of DFeElement::class', function () {
+                $reflection = new ReflectionClass(Emitente::class);
+                $reflectedProperty = $reflection->getProperty('IE');
+                $sut = $reflectedProperty->getType();
+                expect((is_subclass_of($sut->getName(), DFeElement::class)))->toBeTrue();
+            });
+
+            test('Should be required', function () {
+                $reflection = new ReflectionClass(Emitente::class);
+                $reflectedProperty = $reflection->getProperty('IE');
+                $sut = $reflectedProperty->getType();
+                expect($sut->allowsNull())->toBeFalse();
+            });
+        });
+
+        describe('$IEST', function () {
+            test('Should be declared', function () {
+                $sut = new Emitente;
+                expect($sut)->toHaveProperty('IEST');
+            });
+
+            test('Should be a subclass of DFeElement::class', function () {
+                $reflection = new ReflectionClass(Emitente::class);
+                $reflectedProperty = $reflection->getProperty('IEST');
+                $sut = $reflectedProperty->getType();
+                expect((is_subclass_of($sut->getName(), DFeElement::class)))->toBeTrue();
+            });
+
+            test('Should be required', function () {
+                $reflection = new ReflectionClass(Emitente::class);
+                $reflectedProperty = $reflection->getProperty('IEST');
+                $sut = $reflectedProperty->getType();
+                expect($sut->allowsNull())->toBeTrue();
+            });
+        })->skip();
+
+        describe('$CRT', function () {
+            test('Should be declared', function () {
+                $sut = new Emitente;
+                expect($sut)->toHaveProperty('CRT');
+            });
+
+            test('Should be a subclass of DFeElement::class', function () {
+                $reflection = new ReflectionClass(Emitente::class);
+                $reflectedProperty = $reflection->getProperty('CRT');
+                $sut = $reflectedProperty->getType();
+                expect((is_subclass_of($sut->getName(), DFeElement::class)))->toBeTrue();
+            });
+
+            test('Should be required', function () {
+                $reflection = new ReflectionClass(Emitente::class);
+                $reflectedProperty = $reflection->getProperty('CRT');
+                $sut = $reflectedProperty->getType();
+                expect($sut->allowsNull())->toBeFalse();
+            });
+        })->skip();
+
+        describe('$enderEmit', function () {
+            test('Should be declared', function () {
+                $sut = new Emitente;
+                expect($sut)->toHaveProperty('enderEmit');
+            });
+
+            test('Should be a subclass of DFeElement::class', function () {
+                $reflection = new ReflectionClass(Emitente::class);
+                $reflectedProperty = $reflection->getProperty('enderEmit');
+                $sut = $reflectedProperty->getType();
+                expect((is_subclass_of($sut->getName(), DFeElement::class)))->toBeTrue();
+            });
+
+            test('Should be required', function () {
+                $reflection = new ReflectionClass(Emitente::class);
+                $reflectedProperty = $reflection->getProperty('enderEmit');
+                $sut = $reflectedProperty->getType();
+                expect($sut->allowsNull())->toBeFalse();
+            });
+        });
+
+        describe('$IM', function () {
+            test('Should be declared', function () {
+                $sut = new Emitente;
+                expect($sut)->toHaveProperty('IM');
+            });
+
+            test('Should be a subclass of DFeElement::class', function () {
+                $reflection = new ReflectionClass(Emitente::class);
+                $reflectedProperty = $reflection->getProperty('IM');
+                $sut = $reflectedProperty->getType();
+                expect((is_subclass_of($sut->getName(), DFeElement::class)))->toBeTrue();
+            });
+
+            test('Should be optional', function () {
+                $reflection = new ReflectionClass(Emitente::class);
+                $reflectedProperty = $reflection->getProperty('IM');
+                $sut = $reflectedProperty->getType();
+                expect($sut->allowsNull())->toBeTrue();
+            });
+        })->skip();
+
+        describe('CNAE', function () {
+            test('Should be declared', function () {
+                $sut = new Emitente;
+                expect($sut)->toHaveProperty('CNAE');
+            });
+
+            test('Should be a subclass of DFeElement::class', function () {
+                $reflection = new ReflectionClass(Emitente::class);
+                $reflectedProperty = $reflection->getProperty('CNAE');
+                $sut = $reflectedProperty->getType();
+                expect((is_subclass_of($sut->getName(), DFeElement::class)))->toBeTrue();
+            });
+
+            test('Should be optional', function () {
+                $reflection = new ReflectionClass(Emitente::class);
+                $reflectedProperty = $reflection->getProperty('CNAE');
+                $sut = $reflectedProperty->getType();
+                expect($sut->allowsNull())->toBeTrue();
+            });
+        })->skip();
+    });
+
+    describe('methods', function () {
+        describe('validateTagAttributes', function () {
+            test('Should fail if an attribute is provided', function () {
+                $xmlString = '<emit attribute="value"><CNPJ></CNPJ><xNome></xNome><enderEmit></enderEmit><IE></IE><CRT></CRT></emit>';
+                $xmlElement = new Element;
+                $xmlElement->parse($xmlString);
+                $identificacaoNotaFiscal = new Emitente;
+                $sut = new ReflectionMethod($identificacaoNotaFiscal, 'validateTagAttributes');
+                $sutResponse = $sut->invoke($identificacaoNotaFiscal, $xmlElement);
+                expect($sutResponse->isFailure())->toBeTrue();
+            });
+        });
+
+        describe('validateTagElements', function () {
+            test('Should succeed if all required tags id provided', function () {
+                $xmlString = '<emit><CNPJ></CNPJ><xNome></xNome><enderEmit></enderEmit><IE></IE><CRT></CRT></emit>';
+                $xmlElement = new Element;
+                $xmlElement->parse($xmlString);
+                $emitente = new Emitente;
+                $sut = new ReflectionMethod($emitente, 'validateTagElements');
+                $sutResponse = $sut->invoke($emitente, $xmlElement);
+                expect($sutResponse->isSuccess())->toBeTrue();
+            });
+
+            test('Should succeed if all tags is provided', function () {
+                $xmlString = '<emit><CNPJ></CNPJ><CPF></CPF><xNome></xNome><xFant></xFant><enderEmit></enderEmit><IE></IE><IEST></IEST><CRT></CRT><IM></IM><CNAE></CNAE></emit>';
+                $xmlElement = new Element;
+                $xmlElement->parse($xmlString);
+                $emitente = new Emitente;
+                $sut = new ReflectionMethod($emitente, 'validateTagElements');
+                $sutResponse = $sut->invoke($emitente, $xmlElement);
+                expect($sutResponse->isSuccess())->toBeTrue();
+            })->skip();
+
+            test('Should fail if CNPJ tag isnt provided', function () {
+                $xmlString = '<emit><xNome></xNome><enderEmit></enderEmit><IE></IE><CRT></CRT></emit>';
+                $xmlElement = new Element;
+                $xmlElement->parse($xmlString);
+                $emitente = new Emitente;
+                $sut = new ReflectionMethod($emitente, 'validateTagElements');
+                $sutResponse = $sut->invoke($emitente, $xmlElement);
+                expect($sutResponse->isFailure())->toBeTrue();
+            });
+
+            test('Should fail if xNome tag isnt provided', function () {
+                $xmlString = '<emit><CNPJ></CNPJ><enderEmit></enderEmit><IE></IE><CRT></CRT></emit>';
+                $xmlElement = new Element;
+                $xmlElement->parse($xmlString);
+                $emitente = new Emitente;
+                $sut = new ReflectionMethod($emitente, 'validateTagElements');
+                $sutResponse = $sut->invoke($emitente, $xmlElement);
+                expect($sutResponse->isFailure())->toBeTrue();
+            });
+
+            test('Should fail if enderEmit tag isnt provided', function () {
+                $xmlString = '<emit><CNPJ></CNPJ><xNome></xNome><IE></IE><CRT></CRT></emit>';
+                $xmlElement = new Element;
+                $xmlElement->parse($xmlString);
+                $emitente = new Emitente;
+                $sut = new ReflectionMethod($emitente, 'validateTagElements');
+                $sutResponse = $sut->invoke($emitente, $xmlElement);
+                expect($sutResponse->isFailure())->toBeTrue();
+            });
+
+            test('Should fail if IE tag isnt provided', function () {
+                $xmlString = '<emit><CNPJ></CNPJ><xNome></xNome><enderEmit></enderEmit><CRT></CRT></emit>';
+                $xmlElement = new Element;
+                $xmlElement->parse($xmlString);
+                $emitente = new Emitente;
+                $sut = new ReflectionMethod($emitente, 'validateTagElements');
+                $sutResponse = $sut->invoke($emitente, $xmlElement);
+                expect($sutResponse->isFailure())->toBeTrue();
+            });
+
+            test('Should fail if CRT tag isnt provided', function () {
+                $xmlString = '<emit><CNPJ></CNPJ><xNome></xNome><enderEmit></enderEmit><IE></IE></emit>';
+                $xmlElement = new Element;
+                $xmlElement->parse($xmlString);
+                $emitente = new Emitente;
+                $sut = new ReflectionMethod($emitente, 'validateTagElements');
+                $sutResponse = $sut->invoke($emitente, $xmlElement);
+                expect($sutResponse->isFailure())->toBeTrue();
+            });
+
+            test('Should fail if unallowed tag is provided', function () {
+                $xmlString = '<emit><unallowed></unallowed><CNPJ></CNPJ><CPF></CPF><xNome></xNome><xFant></xFant><enderEmit></enderEmit><IE></IE><IEST></IEST><CRT></CRT><IM></IM><CNAE></CNAE></emit>';
+                $xmlElement = new Element;
+                $xmlElement->parse($xmlString);
+                $emitente = new Emitente;
+                $sut = new ReflectionMethod($emitente, 'validateTagElements');
+                $sutResponse = $sut->invoke($emitente, $xmlElement);
+                expect($sutResponse->isFailure())->toBeTrue();
+            })->skip();
+        });
     });
 });
