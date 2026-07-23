8cbb511b6536f3f25578e296c7783328c541e4b7
salviorodrigo <rodrigosalvio@hotmail.com>
2026-07-23 15:55:16 +0000

    test(AliquotaCOFINSTest): enhance validation tests for numeric values and edge cases

--- tests/Unit/Domain/Invoices/NFe/v4_00/ValueObjects/AliquotaCOFINSTest.php
+++ tests/Unit/Domain/Invoices/NFe/v4_00/ValueObjects/AliquotaCOFINSTest.php
@@ -1,69 +1,123 @@
 <?php
 
 declare(strict_types=1);
-
-use BradiApi\Domain\Common\Protocols\ApiError;
 use BradiApi\Domain\Common\ValueObjects\Result;
 use BradiApi\Domain\Invoices\NFe\v4_00\ValueObjects\AliquotaCOFINS;
+use BradiApi\Domain\Invoices\Templates\DFeElement;
 use BradiApi\Domain\Xml\ValueObjects\Element;
-use BradiApi\Tests\TestCase;
 
 describe('AliquotaCOFINS', function () {
-
-    beforeEach(function () {
-        /** @var TestCase $this */
-        $this->sut = new AliquotaCOFINS('');
+    test('Should succeed if is declared', function () {
+        $nameSpace = 'BradiApi\Domain\Invoices\NFe\v4_00\ValueObjects';
+        $sut = $nameSpace . '\\AliquotaCOFINS';
+        expect(class_exists($sut))->toBeTrue();
     });
 
-    describe('::parse()', function () {
-        test('Should succeed with dataset :dataset', function ($candidate) {
-            $xmlString = $candidate === '' ? '' : '<' . AliquotaCOFINS::FIELD_NAME . ">{$candidate}</" . AliquotaCOFINS::FIELD_NAME . '>';
-            $xmlElement = new Element;
-            $xmlElement->parse($xmlString);
-            $sutResponse = $this->sut->parseFromXmlElement($xmlElement);
-            expect($sutResponse)->toBeInstanceOf(Result::class);
-            if ($sutResponse->isFailure()) {
-                $this->fail(json_encode($sutResponse->getError()));
-            }
-            expect($sutResponse->getData())->toBeInstanceOf(AliquotaCOFINS::class);
-            expect($sutResponse->getData()->value)->toBe($candidate);
-            expect((string) $sutResponse->getData())->toBe($xmlString);
-        })->with(datasets('dfes.nfe.value_tags.' . AliquotaCOFINS::FIELD_NAME . '.valid'));
+    test('Should succeed if extends DFeelement', function () {
+        $sut = new AliquotaCOFINS('parentTag');
+        expect(is_subclass_of($sut, DFeElement::class))->toBeTrue();
+    });
 
-        test('Should fail with data set :dataset', function ($candidate) {
-            $xmlString = '<' . AliquotaCOFINS::FIELD_NAME . ">{$candidate}</" . AliquotaCOFINS::FIELD_NAME . '>';
-            $xmlElement = new Element;
-            $xmlElement->parse($xmlString);
-            $sutResponse = $this->sut->parseFromXmlElement($xmlElement);
-            if ($sutResponse->isSuccess()) {
-                $this->fail(json_encode($sutResponse->getData()));
-            }
-            expect($sutResponse)->toBeInstanceOf(Result::class);
-            expect($sutResponse->getError())->toBeInstanceOf(ApiError::class);
-        })->with(datasets('dfes.nfe.value_tags.' . AliquotaCOFINS::FIELD_NAME . '.invalid'));
+    describe('properties', function () {
+        describe('FIELD_NAME', function () {
+            test('Should be set correctly', function () {
+                $reflection = new ReflectionClass(AliquotaCOFINS::class);
+                $reflectedProperty = $reflection->getConstant('FIELD_NAME');
+                expect($reflectedProperty)->toBe('pCOFINS');
+            });
+        });
+    });
 
-        test('Should fail if attributes is provided', function ($candidate) {
-            $xmlString = '<' . AliquotaCOFINS::FIELD_NAME . " fake=\"attribute\">{$candidate}</" . AliquotaCOFINS::FIELD_NAME . '>';
-            $xmlElement = new Element;
-            $xmlElement->parse($xmlString);
-            $sutResponse = $this->sut->parseFromXmlElement($xmlElement);
-            expect($sutResponse)->toBeInstanceOf(Result::class);
-            if ($sutResponse->isSuccess()) {
-                $this->fail(json_encode($sutResponse->getData()));
-            }
-            expect($sutResponse->getError())->toBeInstanceOf(ApiError::class);
-        })->with(datasets('dfes.nfe.value_tags.' . AliquotaCOFINS::FIELD_NAME . '.valid'));
+    describe('methods', function () {
+        describe('validateTagValue', function () {
+            test('Should succeed in border cases', function (string $candidate) {
+                $element = new Element;
+                $element->name = 'pCOFINS';
+                $element->value = $candidate;
+                $aliquotaCOFINS = new AliquotaCOFINS('parentTag');
+                $sut = new ReflectionMethod($aliquotaCOFINS, 'validateTagValue');
+                $sutResponse = $sut->invoke($aliquotaCOFINS, $element);
+                expect($sutResponse)->toBeInstanceOf(Result::class);
+                if ($sutResponse->isFailure()) {
+                    $this->fail(json_encode($sutResponse->getError()));
+                }
 
-        test('Should fail if elements is provided', function ($candidate) {
-            $xmlString = '<' . AliquotaCOFINS::FIELD_NAME . ">{$candidate}<fake>element</fake></" . AliquotaCOFINS::FIELD_NAME . '>';
-            $xmlElement = new Element;
-            $xmlElement->parse($xmlString);
-            $sutResponse = $this->sut->parseFromXmlElement($xmlElement);
-            expect($sutResponse)->toBeInstanceOf(Result::class);
-            if ($sutResponse->isSuccess()) {
-                $this->fail(json_encode($sutResponse->getData()));
-            }
-            expect($sutResponse->getError())->toBeInstanceOf(ApiError::class);
-        })->with(datasets('dfes.nfe.value_tags.' . AliquotaCOFINS::FIELD_NAME . '.valid'));
+                expect($sutResponse->isSuccess())->toBeTrue();
+            })->with([
+                'standard' => '7.60',
+                'with_decimals' => '12.3456',
+                'min' => '0',
+                'max' => '100',
+            ]);
+
+            test('Should fail if less than 0 is provided', function () {
+                $candidate = '-0.01';
+                $element = new Element;
+                $element->name = 'pCOFINS';
+                $element->value = $candidate;
+                $aliquotaCOFINS = new AliquotaCOFINS('parentTag');
+                $sut = new ReflectionMethod($aliquotaCOFINS, 'validateTagValue');
+                $sutResponse = $sut->invoke($aliquotaCOFINS, $element);
+                expect($sutResponse->isFailure())->toBeTrue();
+            });
+
+            test('Should fail if greater than 100 is provided', function () {
+                $candidate = '100.01';
+                $element = new Element;
+                $element->name = 'pCOFINS';
+                $element->value = $candidate;
+                $aliquotaCOFINS = new AliquotaCOFINS('parentTag');
+                $sut = new ReflectionMethod($aliquotaCOFINS, 'validateTagValue');
+                $sutResponse = $sut->invoke($aliquotaCOFINS, $element);
+                expect($sutResponse->isFailure())->toBeTrue();
+            });
+
+            test('Should fail if more than 4 decimal places is provided', function () {
+                $candidate = '12.34567';
+                $element = new Element;
+                $element->name = 'pCOFINS';
+                $element->value = $candidate;
+                $aliquotaCOFINS = new AliquotaCOFINS('parentTag');
+                $sut = new ReflectionMethod($aliquotaCOFINS, 'validateTagValue');
+                $sutResponse = $sut->invoke($aliquotaCOFINS, $element);
+                expect($sutResponse->isFailure())->toBeTrue();
+            });
+
+            test('Should fail if non-numeric value is provided', function () {
+                $candidate = 'abc';
+                $element = new Element;
+                $element->name = 'pCOFINS';
+                $element->value = $candidate;
+                $aliquotaCOFINS = new AliquotaCOFINS('parentTag');
+                $sut = new ReflectionMethod($aliquotaCOFINS, 'validateTagValue');
+                $sutResponse = $sut->invoke($aliquotaCOFINS, $element);
+                expect($sutResponse->isFailure())->toBeTrue();
+            });
+
+            test('Should fail if a numeric value with spaces is provided', function ($candidate) {
+                $element = new Element;
+                $element->name = 'pCOFINS';
+                $element->value = $candidate;
+                $aliquotaCOFINS = new AliquotaCOFINS('parentTag');
+                $sut = new ReflectionMethod($aliquotaCOFINS, 'validateTagValue');
+                $sutResponse = $sut->invoke($aliquotaCOFINS, $element);
+                expect($sutResponse->isFailure())->toBeTrue();
+            })->with([
+                'leading space' => ' 12.34',
+                'trailing space' => '12.34 ',
+                'leading and trailing space' => ' 12.34 ',
+            ]);
+
+            test('Should fail if a value with comma as decimal separator is provided', function () {
+                $candidate = '12,34';
+                $element = new Element;
+                $element->name = 'pCOFINS';
+                $element->value = $candidate;
+                $aliquotaCOFINS = new AliquotaCOFINS('parentTag');
+                $sut = new ReflectionMethod($aliquotaCOFINS, 'validateTagValue');
+                $sutResponse = $sut->invoke($aliquotaCOFINS, $element);
+                expect($sutResponse->isFailure())->toBeTrue();
+            });
+        });
     });
 });
