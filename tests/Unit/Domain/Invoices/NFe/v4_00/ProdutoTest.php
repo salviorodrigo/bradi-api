11585e1d4c3437b70c2e80a02f5a7b3992a380bf
salviorodrigo <rodrigosalvio@hotmail.com>
2026-07-24 18:31:21 +0000

    test(Produto): expand tests for properties and methods, ensuring required fields and validation

--- tests/Unit/Domain/Invoices/NFe/v4_00/ProdutoTest.php
+++ tests/Unit/Domain/Invoices/NFe/v4_00/ProdutoTest.php
@@ -3,42 +3,355 @@
 declare(strict_types=1);
 
 use BradiApi\Domain\Common\Protocols\ApiError;
-use BradiApi\Domain\Common\ValueObjects\Result;
 use BradiApi\Domain\Invoices\NFe\v4_00\Produto;
+use BradiApi\Domain\Invoices\Templates\DFeElement;
 use BradiApi\Domain\Xml\ValueObjects\Element;
 use BradiApi\Tests\TestCase;
 
 describe('Produto', function () {
+    test('Should succeed if Produto is declared', function () {
+        $nameSpace = 'BradiApi\Domain\Invoices\NFe\v4_00';
+        $sut = $nameSpace . '\\Produto';
+        expect(class_exists($sut))->toBeTrue();
+    });
+
+    test('Should succeed if Produto extends DFeElement', function () {
+        $sut = new Produto;
+        expect(is_subclass_of($sut, DFeElement::class))->toBeTrue();
+    });
 
     beforeEach(function () {
         /** @var TestCase $this */
         $this->sut = new Produto;
     });
 
-    describe('::parse()', function () {
-        test('Should succeed with dataset :dataset', function ($candidate) {
-            $xmlElement = new Element;
-            $xmlElement->parse($candidate);
-            $sutResponse = $this->sut->parseFromXmlElement($xmlElement);
-            expect($sutResponse)->toBeInstanceOf(Result::class);
-            if ($sutResponse->isFailure()) {
-                $this->fail(json_encode($sutResponse->getError()));
-            }
-            expect($sutResponse->getData())->toBeInstanceOf(Produto::class);
-            expect($sutResponse->getData()->value)->toBe('');
-            expect((string) $sutResponse->getData())->toBe($candidate);
-        })->with(datasets('dfes.nfe.element_tags.' . Produto::FIELD_NAME . '.valid'));
+    describe('properties', function () {
+        describe('$cProd', function () {
+            test('Should be declared', function () {
+                $sut = new Produto;
+                expect($sut)->toHaveProperty('cProd');
+            });
 
-        test('Should fail with dataset :dataset', function ($candidate) {
-            $xmlElement = new Element;
-            $xmlElement->parse($candidate);
-            $sutResponse = $this->sut->parseFromXmlElement($xmlElement);
-            expect($sutResponse)->toBeInstanceOf(Result::class);
-            if ($sutResponse->isSuccess()) {
-                $this->fail(json_encode($sutResponse->getData()));
-            }
-            expect($sutResponse)->toBeInstanceOf(Result::class);
-            expect($sutResponse->getError())->toBeInstanceOf(ApiError::class);
-        })->with(datasets('dfes.nfe.element_tags.' . Produto::FIELD_NAME . '.invalid'));
+            test('Should be a subclass of DFeElement::class', function () {
+                $reflection = new ReflectionClass(Produto::class);
+                $reflectedProperty = $reflection->getProperty('cProd');
+                $sut = $reflectedProperty->getType();
+                expect((is_subclass_of($sut->getName(), DFeElement::class)))->toBeTrue();
+            });
+
+            test('Should be required', function () {
+                $reflection = new ReflectionClass(Produto::class);
+                $reflectedProperty = $reflection->getProperty('cProd');
+                $sut = $reflectedProperty->getType();
+                expect($sut->allowsNull())->toBeFalse();
+            });
+        });
+
+        describe('$cEAN', function () {
+            test('Should be declared', function () {
+                $sut = new Produto;
+                expect($sut)->toHaveProperty('cEAN');
+            });
+
+            test('Should be a subclass of DFeElement::class', function () {
+                $reflection = new ReflectionClass(Produto::class);
+                $reflectedProperty = $reflection->getProperty('cEAN');
+                $sut = $reflectedProperty->getType();
+                expect((is_subclass_of($sut->getName(), DFeElement::class)))->toBeTrue();
+            });
+
+            test('Should be required', function () {
+                $reflection = new ReflectionClass(Produto::class);
+                $reflectedProperty = $reflection->getProperty('cEAN');
+                $sut = $reflectedProperty->getType();
+                expect($sut->allowsNull())->toBeFalse();
+            });
+        });
+
+        describe('$xProd', function () {
+            test('Should be declared', function () {
+                $sut = new Produto;
+                expect($sut)->toHaveProperty('xProd');
+            });
+
+            test('Should be a subclass of DFeElement::class', function () {
+                $reflection = new ReflectionClass(Produto::class);
+                $reflectedProperty = $reflection->getProperty('xProd');
+                $sut = $reflectedProperty->getType();
+                expect((is_subclass_of($sut->getName(), DFeElement::class)))->toBeTrue();
+            });
+
+            test('Should be required', function () {
+                $reflection = new ReflectionClass(Produto::class);
+                $reflectedProperty = $reflection->getProperty('xProd');
+                $sut = $reflectedProperty->getType();
+                expect($sut->allowsNull())->toBeFalse();
+            });
+        });
+
+        describe('$NCM', function () {
+            test('Should be declared', function () {
+                $sut = new Produto;
+                expect($sut)->toHaveProperty('NCM');
+            });
+
+            test('Should be a subclass of DFeElement::class', function () {
+                $reflection = new ReflectionClass(Produto::class);
+                $reflectedProperty = $reflection->getProperty('NCM');
+                $sut = $reflectedProperty->getType();
+                expect((is_subclass_of($sut->getName(), DFeElement::class)))->toBeTrue();
+            });
+
+            test('Should be required', function () {
+                $reflection = new ReflectionClass(Produto::class);
+                $reflectedProperty = $reflection->getProperty('NCM');
+                $sut = $reflectedProperty->getType();
+                expect($sut->allowsNull())->toBeFalse();
+            });
+        });
+
+        describe('$CFOP', function () {
+            test('Should be declared', function () {
+                $sut = new Produto;
+                expect($sut)->toHaveProperty('CFOP');
+            });
+
+            test('Should be a subclass of DFeElement::class', function () {
+                $reflection = new ReflectionClass(Produto::class);
+                $reflectedProperty = $reflection->getProperty('CFOP');
+                $sut = $reflectedProperty->getType();
+                expect((is_subclass_of($sut->getName(), DFeElement::class)))->toBeTrue();
+            });
+
+            test('Should be required', function () {
+                $reflection = new ReflectionClass(Produto::class);
+                $reflectedProperty = $reflection->getProperty('CFOP');
+                $sut = $reflectedProperty->getType();
+                expect($sut->allowsNull())->toBeFalse();
+            });
+        });
+
+        describe('$uCom', function () {
+            test('Should be declared', function () {
+                $sut = new Produto;
+                expect($sut)->toHaveProperty('uCom');
+            });
+
+            test('Should be a subclass of DFeElement::class', function () {
+                $reflection = new ReflectionClass(Produto::class);
+                $reflectedProperty = $reflection->getProperty('uCom');
+                $sut = $reflectedProperty->getType();
+                expect((is_subclass_of($sut->getName(), DFeElement::class)))->toBeTrue();
+            });
+
+            test('Should be required', function () {
+                $reflection = new ReflectionClass(Produto::class);
+                $reflectedProperty = $reflection->getProperty('uCom');
+                $sut = $reflectedProperty->getType();
+                expect($sut->allowsNull())->toBeFalse();
+            });
+        });
+
+        describe('$qCom', function () {
+            test('Should be declared', function () {
+                $sut = new Produto;
+                expect($sut)->toHaveProperty('qCom');
+            });
+
+            test('Should be a subclass of DFeElement::class', function () {
+                $reflection = new ReflectionClass(Produto::class);
+                $reflectedProperty = $reflection->getProperty('qCom');
+                $sut = $reflectedProperty->getType();
+                expect((is_subclass_of($sut->getName(), DFeElement::class)))->toBeTrue();
+            });
+
+            test('Should be required', function () {
+                $reflection = new ReflectionClass(Produto::class);
+                $reflectedProperty = $reflection->getProperty('qCom');
+                $sut = $reflectedProperty->getType();
+                expect($sut->allowsNull())->toBeFalse();
+            });
+        });
+
+        describe('$vUnCom', function () {
+            test('Should be declared', function () {
+                $sut = new Produto;
+                expect($sut)->toHaveProperty('vUnCom');
+            });
+
+            test('Should be a subclass of DFeElement::class', function () {
+                $reflection = new ReflectionClass(Produto::class);
+                $reflectedProperty = $reflection->getProperty('vUnCom');
+                $sut = $reflectedProperty->getType();
+                expect((is_subclass_of($sut->getName(), DFeElement::class)))->toBeTrue();
+            });
+
+            test('Should be required', function () {
+                $reflection = new ReflectionClass(Produto::class);
+                $reflectedProperty = $reflection->getProperty('vUnCom');
+                $sut = $reflectedProperty->getType();
+                expect($sut->allowsNull())->toBeFalse();
+            });
+        });
+
+        describe('$vProd', function () {
+            test('Should be declared', function () {
+                $sut = new Produto;
+                expect($sut)->toHaveProperty('vProd');
+            });
+
+            test('Should be a subclass of DFeElement::class', function () {
+                $reflection = new ReflectionClass(Produto::class);
+                $reflectedProperty = $reflection->getProperty('vProd');
+                $sut = $reflectedProperty->getType();
+                expect((is_subclass_of($sut->getName(), DFeElement::class)))->toBeTrue();
+            });
+
+            test('Should be required', function () {
+                $reflection = new ReflectionClass(Produto::class);
+                $reflectedProperty = $reflection->getProperty('vProd');
+                $sut = $reflectedProperty->getType();
+                expect($sut->allowsNull())->toBeFalse();
+            });
+        });
+    });
+
+    describe('methods', function () {
+        describe('validateTagAttributes', function () {
+            test('Should fail if an attribute is provided', function () {
+                $xmlString = '<prod attribute="value"><cProd></cProd><cEAN></cEAN><xProd></xProd><NCM></NCM><CFOP></CFOP><uCom></uCom><qCom></qCom><vUnCom></vUnCom><vProd></vProd></prod>';
+                $xmlElement = new Element;
+                $xmlElement->parse($xmlString);
+                $produto = new Produto;
+                $sut = new ReflectionMethod($produto, 'validateTagAttributes');
+                $sutResponse = $sut->invoke($produto, $xmlElement);
+                expect($sutResponse->isFailure())->toBeTrue();
+            });
+        });
+
+        describe('validateTagElements', function () {
+            test('Should succeed if all required tags is provided', function () {
+                $xmlString = '<prod><cProd></cProd><cEAN></cEAN><xProd></xProd><NCM></NCM><CFOP></CFOP><uCom></uCom><qCom></qCom><vUnCom></vUnCom><vProd></vProd></prod>';
+                $xmlElement = new Element;
+                $xmlElement->parse($xmlString);
+                $produto = new Produto;
+                $sut = new ReflectionMethod($produto, 'validateTagElements');
+                $sutResponse = $sut->invoke($produto, $xmlElement);
+                expect($sutResponse->isSuccess())->toBeTrue();
+            });
+
+            test('Should fail if cProd tag isnt provided', function () {
+                $xmlString = '<prod><cEAN></cEAN><xProd></xProd><NCM></NCM><CFOP></CFOP><uCom></uCom><qCom></qCom><vUnCom></vUnCom><vProd></vProd></prod>';
+                $xmlElement = new Element;
+                $xmlElement->parse($xmlString);
+                $produto = new Produto;
+                $sut = new ReflectionMethod($produto, 'validateTagElements');
+                $sutResponse = $sut->invoke($produto, $xmlElement);
+                expect($sutResponse->isFailure())->toBeTrue();
+            });
+
+            test('Should fail if cEAN tag isnt provided', function () {
+                $xmlString = '<prod><cProd></cProd><xProd></xProd><NCM></NCM><CFOP></CFOP><uCom></uCom><qCom></qCom><vUnCom></vUnCom><vProd></vProd></prod>';
+                $xmlElement = new Element;
+                $xmlElement->parse($xmlString);
+                $produto = new Produto;
+                $sut = new ReflectionMethod($produto, 'validateTagElements');
+                $sutResponse = $sut->invoke($produto, $xmlElement);
+                expect($sutResponse->isFailure())->toBeTrue();
+            });
+
+            test('Should fail if xProd tag isnt provided', function () {
+                $xmlString = '<prod><cProd></cProd><cEAN></cEAN><NCM></NCM><CFOP></CFOP><uCom></uCom><qCom></qCom><vUnCom></vUnCom><vProd></vProd></prod>';
+                $xmlElement = new Element;
+                $xmlElement->parse($xmlString);
+                $produto = new Produto;
+                $sut = new ReflectionMethod($produto, 'validateTagElements');
+                $sutResponse = $sut->invoke($produto, $xmlElement);
+                expect($sutResponse->isFailure())->toBeTrue();
+            });
+
+            test('Should fail if NCM tag isnt provided', function () {
+                $xmlString = '<prod><cProd></cProd><cEAN></cEAN><xProd></xProd><CFOP></CFOP><uCom></uCom><qCom></qCom><vUnCom></vUnCom><vProd></vProd></prod>';
+                $xmlElement = new Element;
+                $xmlElement->parse($xmlString);
+                $produto = new Produto;
+                $sut = new ReflectionMethod($produto, 'validateTagElements');
+                $sutResponse = $sut->invoke($produto, $xmlElement);
+                expect($sutResponse->isFailure())->toBeTrue();
+            });
+
+            test('Should fail if CFOP tag isnt provided', function () {
+                $xmlString = '<prod><cProd></cProd><cEAN></cEAN><xProd></xProd><NCM></NCM><uCom></uCom><qCom></qCom><vUnCom></vUnCom><vProd></vProd></prod>';
+                $xmlElement = new Element;
+                $xmlElement->parse($xmlString);
+                $produto = new Produto;
+                $sut = new ReflectionMethod($produto, 'validateTagElements');
+                $sutResponse = $sut->invoke($produto, $xmlElement);
+                expect($sutResponse->isFailure())->toBeTrue();
+            });
+
+            test('Should fail if uCom tag isnt provided', function () {
+                $xmlString = '<prod><cProd></cProd><cEAN></cEAN><xProd></xProd><NCM></NCM><CFOP></CFOP><qCom></qCom><vUnCom></vUnCom><vProd></vProd></prod>';
+                $xmlElement = new Element;
+                $xmlElement->parse($xmlString);
+                $produto = new Produto;
+                $sut = new ReflectionMethod($produto, 'validateTagElements');
+                $sutResponse = $sut->invoke($produto, $xmlElement);
+                expect($sutResponse->isFailure())->toBeTrue();
+            });
+
+            test('Should fail if qCom tag isnt provided', function () {
+                $xmlString = '<prod><cProd></cProd><cEAN></cEAN><xProd></xProd><NCM></NCM><CFOP></CFOP><uCom></uCom><vUnCom></vUnCom><vProd></vProd></prod>';
+                $xmlElement = new Element;
+                $xmlElement->parse($xmlString);
+                $produto = new Produto;
+                $sut = new ReflectionMethod($produto, 'validateTagElements');
+                $sutResponse = $sut->invoke($produto, $xmlElement);
+                expect($sutResponse->isFailure())->toBeTrue();
+            });
+
+            test('Should fail if vUnCom tag isnt provided', function () {
+                $xmlString = '<prod><cProd></cProd><cEAN></cEAN><xProd></xProd><NCM></NCM><CFOP></CFOP><uCom></uCom><qCom></qCom><vProd></vProd></prod>';
+                $xmlElement = new Element;
+                $xmlElement->parse($xmlString);
+                $produto = new Produto;
+                $sut = new ReflectionMethod($produto, 'validateTagElements');
+                $sutResponse = $sut->invoke($produto, $xmlElement);
+                expect($sutResponse->isFailure())->toBeTrue();
+            });
+
+            test('Should fail if vProd tag isnt provided', function () {
+                $xmlString = '<prod><cProd></cProd><cEAN></cEAN><xProd></xProd><NCM></NCM><CFOP></CFOP><uCom></uCom><qCom></qCom><vUnCom></vUnCom></prod>';
+                $xmlElement = new Element;
+                $xmlElement->parse($xmlString);
+                $produto = new Produto;
+                $sut = new ReflectionMethod($produto, 'validateTagElements');
+                $sutResponse = $sut->invoke($produto, $xmlElement);
+                expect($sutResponse->isFailure())->toBeTrue();
+            });
+
+            test('Should fail if an unallowed tag is provided', function () {
+                $xmlString = '<prod><unallowed></unallowed><cProd></cProd><cEAN></cEAN><xProd></xProd><NCM></NCM><CFOP></CFOP><uCom></uCom><qCom></qCom><vUnCom></vUnCom><vProd></vProd></prod>';
+                $xmlElement = new Element;
+                $xmlElement->parse($xmlString);
+                $produto = new Produto;
+                $sut = new ReflectionMethod($produto, 'validateTagElements');
+                $sutResponse = $sut->invoke($produto, $xmlElement);
+                expect($sutResponse->isFailure())->toBeTrue();
+            })->skip();
+        });
+
+        describe('validateTagValue', function () {
+            test('Should fail if a value is provided', function () {
+                $xmlString = '<prod>value</prod>';
+                $xmlElement = new Element;
+                $xmlElement->parse($xmlString);
+                $produto = new Produto;
+                $sut = new ReflectionMethod($produto, 'validateTagValue');
+                $sutResponse = $sut->invoke($produto, $xmlElement);
+                expect($sutResponse->isFailure())->toBeTrue();
+                expect($sutResponse->getError())->toBeInstanceOf(ApiError::class);
+            });
+        });
     });
 });
