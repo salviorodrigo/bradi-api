5b7ba5eedccb667b902eb287ebf577f5ce35c0aa
salviorodrigo <rodrigosalvio@hotmail.com>
2026-07-25 21:01:50 +0000

    test(Icms): add skiped tests for ICMS10 to ICMS90 properties and ensure they are optional

--- tests/Unit/Domain/Invoices/NFe/v4_00/IcmsTest.php
+++ tests/Unit/Domain/Invoices/NFe/v4_00/IcmsTest.php
@@ -46,6 +46,342 @@ describe('Icms', function () {
                 expect($sut->allowsNull())->toBeTrue();
             });
         });
+
+        describe('$ICMS10', function () {
+            test('Should be declared', function () {
+                $sut = new Icms;
+                expect($sut)->toHaveProperty('ICMS10');
+            });
+
+            test('Should be a subclass of DFeElement::class', function () {
+                $reflection = new ReflectionClass(Icms::class);
+                $reflectedProperty = $reflection->getProperty('ICMS10');
+                $sut = $reflectedProperty->getType();
+                expect((is_subclass_of($sut->getName(), DFeElement::class)))->toBeTrue();
+            });
+
+            test('Should be optional', function () {
+                $reflection = new ReflectionClass(Icms::class);
+                $reflectedProperty = $reflection->getProperty('ICMS10');
+                $sut = $reflectedProperty->getType();
+                expect($sut->allowsNull())->toBeTrue();
+            });
+        })->skip();
+
+        describe('$ICMS20', function () {
+            test('Should be declared', function () {
+                $sut = new Icms;
+                expect($sut)->toHaveProperty('ICMS20');
+            });
+
+            test('Should be a subclass of DFeElement::class', function () {
+                $reflection = new ReflectionClass(Icms::class);
+                $reflectedProperty = $reflection->getProperty('ICMS20');
+                $sut = $reflectedProperty->getType();
+                expect((is_subclass_of($sut->getName(), DFeElement::class)))->toBeTrue();
+            });
+
+            test('Should be optional', function () {
+                $reflection = new ReflectionClass(Icms::class);
+                $reflectedProperty = $reflection->getProperty('ICMS20');
+                $sut = $reflectedProperty->getType();
+                expect($sut->allowsNull())->toBeTrue();
+            });
+        })->skip();
+
+        describe('$ICMS30', function () {
+            test('Should be declared', function () {
+                $sut = new Icms;
+                expect($sut)->toHaveProperty('ICMS30');
+            });
+
+            test('Should be a subclass of DFeElement::class', function () {
+                $reflection = new ReflectionClass(Icms::class);
+                $reflectedProperty = $reflection->getProperty('ICMS30');
+                $sut = $reflectedProperty->getType();
+                expect((is_subclass_of($sut->getName(), DFeElement::class)))->toBeTrue();
+            });
+
+            test('Should be optional', function () {
+                $reflection = new ReflectionClass(Icms::class);
+                $reflectedProperty = $reflection->getProperty('ICMS30');
+                $sut = $reflectedProperty->getType();
+                expect($sut->allowsNull())->toBeTrue();
+            });
+        })->skip();
+
+        describe('$ICMS40', function () {
+            test('Should be declared', function () {
+                $sut = new Icms;
+                expect($sut)->toHaveProperty('ICMS40');
+            });
+
+            test('Should be a subclass of DFeElement::class', function () {
+                $reflection = new ReflectionClass(Icms::class);
+                $reflectedProperty = $reflection->getProperty('ICMS40');
+                $sut = $reflectedProperty->getType();
+                expect((is_subclass_of($sut->getName(), DFeElement::class)))->toBeTrue();
+            });
+
+            test('Should be optional', function () {
+                $reflection = new ReflectionClass(Icms::class);
+                $reflectedProperty = $reflection->getProperty('ICMS40');
+                $sut = $reflectedProperty->getType();
+                expect($sut->allowsNull())->toBeTrue();
+            });
+        })->skip();
+
+        describe('$ICMS51', function () {
+            test('Should be declared', function () {
+                $sut = new Icms;
+                expect($sut)->toHaveProperty('ICMS51');
+            });
+
+            test('Should be a subclass of DFeElement::class', function () {
+                $reflection = new ReflectionClass(Icms::class);
+                $reflectedProperty = $reflection->getProperty('ICMS51');
+                $sut = $reflectedProperty->getType();
+                expect((is_subclass_of($sut->getName(), DFeElement::class)))->toBeTrue();
+            });
+
+            test('Should be optional', function () {
+                $reflection = new ReflectionClass(Icms::class);
+                $reflectedProperty = $reflection->getProperty('ICMS51');
+                $sut = $reflectedProperty->getType();
+                expect($sut->allowsNull())->toBeTrue();
+            });
+        })->skip();
+
+        describe('$ICMS60', function () {
+            test('Should be declared', function () {
+                $sut = new Icms;
+                expect($sut)->toHaveProperty('ICMS60');
+            });
+
+            test('Should be a subclass of DFeElement::class', function () {
+                $reflection = new ReflectionClass(Icms::class);
+                $reflectedProperty = $reflection->getProperty('ICMS60');
+                $sut = $reflectedProperty->getType();
+                expect((is_subclass_of($sut->getName(), DFeElement::class)))->toBeTrue();
+            });
+
+            test('Should be optional', function () {
+                $reflection = new ReflectionClass(Icms::class);
+                $reflectedProperty = $reflection->getProperty('ICMS60');
+                $sut = $reflectedProperty->getType();
+                expect($sut->allowsNull())->toBeTrue();
+            });
+        })->skip();
+
+        describe('$ICMS70', function () {
+            test('Should be declared', function () {
+                $sut = new Icms;
+                expect($sut)->toHaveProperty('ICMS70');
+            });
+
+            test('Should be a subclass of DFeElement::class', function () {
+                $reflection = new ReflectionClass(Icms::class);
+                $reflectedProperty = $reflection->getProperty('ICMS70');
+                $sut = $reflectedProperty->getType();
+                expect((is_subclass_of($sut->getName(), DFeElement::class)))->toBeTrue();
+            });
+
+            test('Should be optional', function () {
+                $reflection = new ReflectionClass(Icms::class);
+                $reflectedProperty = $reflection->getProperty('ICMS70');
+                $sut = $reflectedProperty->getType();
+                expect($sut->allowsNull())->toBeTrue();
+            });
+        })->skip();
+
+        describe('$ICMS90', function () {
+            test('Should be declared', function () {
+                $sut = new Icms;
+                expect($sut)->toHaveProperty('ICMS90');
+            });
+
+            test('Should be a subclass of DFeElement::class', function () {
+                $reflection = new ReflectionClass(Icms::class);
+                $reflectedProperty = $reflection->getProperty('ICMS90');
+                $sut = $reflectedProperty->getType();
+                expect((is_subclass_of($sut->getName(), DFeElement::class)))->toBeTrue();
+            });
+
+            test('Should be optional', function () {
+                $reflection = new ReflectionClass(Icms::class);
+                $reflectedProperty = $reflection->getProperty('ICMS90');
+                $sut = $reflectedProperty->getType();
+                expect($sut->allowsNull())->toBeTrue();
+            });
+        })->skip();
+
+        describe('$ICMSPart', function () {
+            test('Should be declared', function () {
+                $sut = new Icms;
+                expect($sut)->toHaveProperty('ICMSPart');
+            });
+
+            test('Should be a subclass of DFeElement::class', function () {
+                $reflection = new ReflectionClass(Icms::class);
+                $reflectedProperty = $reflection->getProperty('ICMSPart');
+                $sut = $reflectedProperty->getType();
+                expect((is_subclass_of($sut->getName(), DFeElement::class)))->toBeTrue();
+            });
+
+            test('Should be optional', function () {
+                $reflection = new ReflectionClass(Icms::class);
+                $reflectedProperty = $reflection->getProperty('ICMSPart');
+                $sut = $reflectedProperty->getType();
+                expect($sut->allowsNull())->toBeTrue();
+            });
+        })->skip();
+
+        describe('$ICMSST', function () {
+            test('Should be declared', function () {
+                $sut = new Icms;
+                expect($sut)->toHaveProperty('ICMSST');
+            });
+
+            test('Should be a subclass of DFeElement::class', function () {
+                $reflection = new ReflectionClass(Icms::class);
+                $reflectedProperty = $reflection->getProperty('ICMSST');
+                $sut = $reflectedProperty->getType();
+                expect((is_subclass_of($sut->getName(), DFeElement::class)))->toBeTrue();
+            });
+
+            test('Should be optional', function () {
+                $reflection = new ReflectionClass(Icms::class);
+                $reflectedProperty = $reflection->getProperty('ICMSST');
+                $sut = $reflectedProperty->getType();
+                expect($sut->allowsNull())->toBeTrue();
+            });
+        })->skip();
+
+        describe('$ICMSSN101', function () {
+            test('Should be declared', function () {
+                $sut = new Icms;
+                expect($sut)->toHaveProperty('ICMSSN101');
+            });
+
+            test('Should be a subclass of DFeElement::class', function () {
+                $reflection = new ReflectionClass(Icms::class);
+                $reflectedProperty = $reflection->getProperty('ICMSSN101');
+                $sut = $reflectedProperty->getType();
+                expect((is_subclass_of($sut->getName(), DFeElement::class)))->toBeTrue();
+            });
+
+            test('Should be optional', function () {
+                $reflection = new ReflectionClass(Icms::class);
+                $reflectedProperty = $reflection->getProperty('ICMSSN101');
+                $sut = $reflectedProperty->getType();
+                expect($sut->allowsNull())->toBeTrue();
+            });
+        })->skip();
+
+        describe('$ICMSSN102', function () {
+            test('Should be declared', function () {
+                $sut = new Icms;
+                expect($sut)->toHaveProperty('ICMSSN102');
+            });
+
+            test('Should be a subclass of DFeElement::class', function () {
+                $reflection = new ReflectionClass(Icms::class);
+                $reflectedProperty = $reflection->getProperty('ICMSSN102');
+                $sut = $reflectedProperty->getType();
+                expect((is_subclass_of($sut->getName(), DFeElement::class)))->toBeTrue();
+            });
+
+            test('Should be optional', function () {
+                $reflection = new ReflectionClass(Icms::class);
+                $reflectedProperty = $reflection->getProperty('ICMSSN102');
+                $sut = $reflectedProperty->getType();
+                expect($sut->allowsNull())->toBeTrue();
+            });
+        })->skip();
+
+        describe('$ICMSSN201', function () {
+            test('Should be declared', function () {
+                $sut = new Icms;
+                expect($sut)->toHaveProperty('ICMSSN201');
+            });
+
+            test('Should be a subclass of DFeElement::class', function () {
+                $reflection = new ReflectionClass(Icms::class);
+                $reflectedProperty = $reflection->getProperty('ICMSSN201');
+                $sut = $reflectedProperty->getType();
+                expect((is_subclass_of($sut->getName(), DFeElement::class)))->toBeTrue();
+            });
+
+            test('Should be optional', function () {
+                $reflection = new ReflectionClass(Icms::class);
+                $reflectedProperty = $reflection->getProperty('ICMSSN201');
+                $sut = $reflectedProperty->getType();
+                expect($sut->allowsNull())->toBeTrue();
+            });
+        })->skip();
+
+        describe('$ICMSSN202', function () {
+            test('Should be declared', function () {
+                $sut = new Icms;
+                expect($sut)->toHaveProperty('ICMSSN202');
+            });
+
+            test('Should be a subclass of DFeElement::class', function () {
+                $reflection = new ReflectionClass(Icms::class);
+                $reflectedProperty = $reflection->getProperty('ICMSSN202');
+                $sut = $reflectedProperty->getType();
+                expect((is_subclass_of($sut->getName(), DFeElement::class)))->toBeTrue();
+            });
+
+            test('Should be optional', function () {
+                $reflection = new ReflectionClass(Icms::class);
+                $reflectedProperty = $reflection->getProperty('ICMSSN202');
+                $sut = $reflectedProperty->getType();
+                expect($sut->allowsNull())->toBeTrue();
+            });
+        })->skip();
+
+        describe('$ICMSSN500', function () {
+            test('Should be declared', function () {
+                $sut = new Icms;
+                expect($sut)->toHaveProperty('ICMSSN500');
+            });
+
+            test('Should be a subclass of DFeElement::class', function () {
+                $reflection = new ReflectionClass(Icms::class);
+                $reflectedProperty = $reflection->getProperty('ICMSSN500');
+                $sut = $reflectedProperty->getType();
+                expect((is_subclass_of($sut->getName(), DFeElement::class)))->toBeTrue();
+            });
+
+            test('Should be optional', function () {
+                $reflection = new ReflectionClass(Icms::class);
+                $reflectedProperty = $reflection->getProperty('ICMSSN500');
+                $sut = $reflectedProperty->getType();
+                expect($sut->allowsNull())->toBeTrue();
+            });
+        })->skip();
+
+        describe('$ICMSSN900', function () {
+            test('Should be declared', function () {
+                $sut = new Icms;
+                expect($sut)->toHaveProperty('ICMSSN900');
+            });
+
+            test('Should be a subclass of DFeElement::class', function () {
+                $reflection = new ReflectionClass(Icms::class);
+                $reflectedProperty = $reflection->getProperty('ICMSSN900');
+                $sut = $reflectedProperty->getType();
+                expect((is_subclass_of($sut->getName(), DFeElement::class)))->toBeTrue();
+            });
+
+            test('Should be optional', function () {
+                $reflection = new ReflectionClass(Icms::class);
+                $reflectedProperty = $reflection->getProperty('ICMSSN900');
+                $sut = $reflectedProperty->getType();
+                expect($sut->allowsNull())->toBeTrue();
+            });
+        })->skip();
     });
 
     describe('methods', function () {
