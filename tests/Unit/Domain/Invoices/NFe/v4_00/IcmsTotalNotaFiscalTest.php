db3f3e5c16043a172a2cbaa394ddf202314ee042
salviorodrigo <rodrigosalvio@hotmail.com>
2026-07-26 21:27:59 +0000

    test(IcmsTotalNotaFiscal): ensure tag validation fails if anyone required tag is missing

--- tests/Unit/Domain/Invoices/NFe/v4_00/IcmsTotalNotaFiscalTest.php
+++ tests/Unit/Domain/Invoices/NFe/v4_00/IcmsTotalNotaFiscalTest.php
@@ -55,6 +55,196 @@ describe('IcmsTotalNotaFiscal', function () {
                 expect($sutResponse->isSuccess())->toBeTrue();
             });
 
+            test('Should fail if vBC is missing', function () {
+                $xmlString = '<ICMSTot><vICMS/><vICMSDeson/><vFCPUFDest/><vICMSUFDest/><vICMSUFRemet/><vFCP/><vBCST/><vST/><vFCPST/><vFCPSTRet/><vProd/><vFrete/><vSeg/><vDesc/><vII/><vIPI/><vIPIDevol/><vPIS/><vCOFINS/><vOutro/><vNF/><vTotTrib/></ICMSTot>';
+                $xmlElement = new Element;
+                $xmlElement->parse($xmlString);
+                $targetClass = new IcmsTotalNotaFiscal;
+                $sut = new ReflectionMethod($targetClass, 'validateTagElements');
+                $sutResponse = $sut->invoke($targetClass, $xmlElement);
+                expect($sutResponse->isFailure())->toBeTrue();
+            });
+
+            test('Should fail if vICMS is missing', function () {
+                $xmlString = '<ICMSTot><vBC/><vICMSDeson/><vFCPUFDest/><vICMSUFDest/><vICMSUFRemet/><vFCP/><vBCST/><vST/><vFCPST/><vFCPSTRet/><vProd/><vFrete/><vSeg/><vDesc/><vII/><vIPI/><vIPIDevol/><vPIS/><vCOFINS/><vOutro/><vNF/><vTotTrib/></ICMSTot>';
+                $xmlElement = new Element;
+                $xmlElement->parse($xmlString);
+                $targetClass = new IcmsTotalNotaFiscal;
+                $sut = new ReflectionMethod($targetClass, 'validateTagElements');
+                $sutResponse = $sut->invoke($targetClass, $xmlElement);
+                expect($sutResponse->isFailure())->toBeTrue();
+            });
+
+            test('Should fail if vICMSDeson is missing', function () {
+                $xmlString = '<ICMSTot><vBC/><vICMS/><vFCPUFDest/><vICMSUFDest/><vICMSUFRemet/><vFCP/><vBCST/><vST/><vFCPST/><vFCPSTRet/><vProd/><vFrete/><vSeg/><vDesc/><vII/><vIPI/><vIPIDevol/><vPIS/><vCOFINS/><vOutro/><vNF/><vTotTrib/></ICMSTot>';
+                $xmlElement = new Element;
+                $xmlElement->parse($xmlString);
+                $targetClass = new IcmsTotalNotaFiscal;
+                $sut = new ReflectionMethod($targetClass, 'validateTagElements');
+                $sutResponse = $sut->invoke($targetClass, $xmlElement);
+                expect($sutResponse->isFailure())->toBeTrue();
+            });
+
+            test('Should fail if vFCP is missing', function () {
+                $xmlString = '<ICMSTot><vBC/><vICMS/><vICMSDeson/><vFCPUFDest/><vICMSUFDest/><vICMSUFRemet/><vBCST/><vST/><vFCPST/><vFCPSTRet/><vProd/><vFrete/><vSeg/><vDesc/><vII/><vIPI/><vIPIDevol/><vPIS/><vCOFINS/><vOutro/><vNF/><vTotTrib/></ICMSTot>';
+                $xmlElement = new Element;
+                $xmlElement->parse($xmlString);
+                $targetClass = new IcmsTotalNotaFiscal;
+                $sut = new ReflectionMethod($targetClass, 'validateTagElements');
+                $sutResponse = $sut->invoke($targetClass, $xmlElement);
+                expect($sutResponse->isFailure())->toBeTrue();
+            });
+
+            test('Should fail if vBCST is missing', function () {
+                $xmlString = '<ICMSTot><vBC/><vICMS/><vICMSDeson/><vFCPUFDest/><vICMSUFDest/><vICMSUFRemet/><vFCP/><vST/><vFCPST/><vFCPSTRet/><vProd/><vFrete/><vSeg/><vDesc/><vII/><vIPI/><vIPIDevol/><vPIS/><vCOFINS/><vOutro/><vNF/><vTotTrib/></ICMSTot>';
+                $xmlElement = new Element;
+                $xmlElement->parse($xmlString);
+                $targetClass = new IcmsTotalNotaFiscal;
+                $sut = new ReflectionMethod($targetClass, 'validateTagElements');
+                $sutResponse = $sut->invoke($targetClass, $xmlElement);
+                expect($sutResponse->isFailure())->toBeTrue();
+            });
+
+            test('Should fail if vST is missing', function () {
+                $xmlString = '<ICMSTot><vBC/><vICMS/><vICMSDeson/><vFCPUFDest/><vICMSUFDest/><vICMSUFRemet/><vFCP/><vBCST/><vFCPST/><vFCPSTRet/><vProd/><vFrete/><vSeg/><vDesc/><vII/><vIPI/><vIPIDevol/><vPIS/><vCOFINS/><vOutro/><vNF/><vTotTrib/></ICMSTot>';
+                $xmlElement = new Element;
+                $xmlElement->parse($xmlString);
+                $targetClass = new IcmsTotalNotaFiscal;
+                $sut = new ReflectionMethod($targetClass, 'validateTagElements');
+                $sutResponse = $sut->invoke($targetClass, $xmlElement);
+                expect($sutResponse->isFailure())->toBeTrue();
+            });
+
+            test('Should fail if vFCPST is missing', function () {
+                $xmlString = '<ICMSTot><vBC/><vICMS/><vICMSDeson/><vFCPUFDest/><vICMSUFDest/><vICMSUFRemet/><vFCP/><vBCST/><vST/><vFCPSTRet/><vProd/><vFrete/><vSeg/><vDesc/><vII/><vIPI/><vIPIDevol/><vPIS/><vCOFINS/><vOutro/><vNF/><vTotTrib/></ICMSTot>';
+                $xmlElement = new Element;
+                $xmlElement->parse($xmlString);
+                $targetClass = new IcmsTotalNotaFiscal;
+                $sut = new ReflectionMethod($targetClass, 'validateTagElements');
+                $sutResponse = $sut->invoke($targetClass, $xmlElement);
+                expect($sutResponse->isFailure())->toBeTrue();
+            });
+
+            test('Should fail if vFCPSTRet is missing', function () {
+                $xmlString = '<ICMSTot><vBC/><vICMS/><vICMSDeson/><vFCPUFDest/><vICMSUFDest/><vICMSUFRemet/><vFCP/><vBCST/><vST/><vFCPST/><vProd/><vFrete/><vSeg/><vDesc/><vII/><vIPI/><vIPIDevol/><vPIS/><vCOFINS/><vOutro/><vNF/><vTotTrib/></ICMSTot>';
+                $xmlElement = new Element;
+                $xmlElement->parse($xmlString);
+                $targetClass = new IcmsTotalNotaFiscal;
+                $sut = new ReflectionMethod($targetClass, 'validateTagElements');
+                $sutResponse = $sut->invoke($targetClass, $xmlElement);
+                expect($sutResponse->isFailure())->toBeTrue();
+            });
+
+            test('Should fail if vProd is missing', function () {
+                $xmlString = '<ICMSTot><vBC/><vICMS/><vICMSDeson/><vFCPUFDest/><vICMSUFDest/><vICMSUFRemet/><vFCP/><vBCST/><vST/><vFCPST/><vFCPSTRet/><vFrete/><vSeg/><vDesc/><vII/><vIPI/><vIPIDevol/><vPIS/><vCOFINS/><vOutro/><vNF/><vTotTrib/></ICMSTot>';
+                $xmlElement = new Element;
+                $xmlElement->parse($xmlString);
+                $targetClass = new IcmsTotalNotaFiscal;
+                $sut = new ReflectionMethod($targetClass, 'validateTagElements');
+                $sutResponse = $sut->invoke($targetClass, $xmlElement);
+                expect($sutResponse->isFailure())->toBeTrue();
+            });
+
+            test('Should fail if vFrete is missing', function () {
+                $xmlString = '<ICMSTot><vBC/><vICMS/><vICMSDeson/><vFCPUFDest/><vICMSUFDest/><vICMSUFRemet/><vFCP/><vBCST/><vST/><vFCPST/><vFCPSTRet/><vProd/><vSeg/><vDesc/><vII/><vIPI/><vIPIDevol/><vPIS/><vCOFINS/><vOutro/><vNF/><vTotTrib/></ICMSTot>';
+                $xmlElement = new Element;
+                $xmlElement->parse($xmlString);
+                $targetClass = new IcmsTotalNotaFiscal;
+                $sut = new ReflectionMethod($targetClass, 'validateTagElements');
+                $sutResponse = $sut->invoke($targetClass, $xmlElement);
+                expect($sutResponse->isFailure())->toBeTrue();
+            });
+
+            test('Should fail if vSeg is missing', function () {
+                $xmlString = '<ICMSTot><vBC/><vICMS/><vICMSDeson/><vFCPUFDest/><vICMSUFDest/><vICMSUFRemet/><vFCP/><vBCST/><vST/><vFCPST/><vFCPSTRet/><vProd/><vFrete/><vDesc/><vII/><vIPI/><vIPIDevol/><vPIS/><vCOFINS/><vOutro/><vNF/><vTotTrib/></ICMSTot>';
+                $xmlElement = new Element;
+                $xmlElement->parse($xmlString);
+                $targetClass = new IcmsTotalNotaFiscal;
+                $sut = new ReflectionMethod($targetClass, 'validateTagElements');
+                $sutResponse = $sut->invoke($targetClass, $xmlElement);
+                expect($sutResponse->isFailure())->toBeTrue();
+            });
+
+            test('Should fail if vDesc is missing', function () {
+                $xmlString = '<ICMSTot><vBC/><vICMS/><vICMSDeson/><vFCPUFDest/><vICMSUFDest/><vICMSUFRemet/><vFCP/><vBCST/><vST/><vFCPST/><vFCPSTRet/><vProd/><vFrete/><vSeg/><vII/><vIPI/><vIPIDevol/><vPIS/><vCOFINS/><vOutro/><vNF/><vTotTrib/></ICMSTot>';
+                $xmlElement = new Element;
+                $xmlElement->parse($xmlString);
+                $targetClass = new IcmsTotalNotaFiscal;
+                $sut = new ReflectionMethod($targetClass, 'validateTagElements');
+                $sutResponse = $sut->invoke($targetClass, $xmlElement);
+                expect($sutResponse->isFailure())->toBeTrue();
+            });
+
+            test('Should fail if vII is missing', function () {
+                $xmlString = '<ICMSTot><vBC/><vICMS/><vICMSDeson/><vFCPUFDest/><vICMSUFDest/><vICMSUFRemet/><vFCP/><vBCST/><vST/><vFCPST/><vFCPSTRet/><vProd/><vFrete/><vSeg/><vDesc/><vIPI/><vIPIDevol/><vPIS/><vCOFINS/><vOutro/><vNF/><vTotTrib/></ICMSTot>';
+                $xmlElement = new Element;
+                $xmlElement->parse($xmlString);
+                $targetClass = new IcmsTotalNotaFiscal;
+                $sut = new ReflectionMethod($targetClass, 'validateTagElements');
+                $sutResponse = $sut->invoke($targetClass, $xmlElement);
+                expect($sutResponse->isFailure())->toBeTrue();
+            });
+
+            test('Should fail if vIPI is missing', function () {
+                $xmlString = '<ICMSTot><vBC/><vICMS/><vICMSDeson/><vFCPUFDest/><vICMSUFDest/><vICMSUFRemet/><vFCP/><vBCST/><vST/><vFCPST/><vFCPSTRet/><vProd/><vFrete/><vSeg/><vDesc/><vII/><vIPIDevol/><vPIS/><vCOFINS/><vOutro/><vNF/><vTotTrib/></ICMSTot>';
+                $xmlElement = new Element;
+                $xmlElement->parse($xmlString);
+                $targetClass = new IcmsTotalNotaFiscal;
+                $sut = new ReflectionMethod($targetClass, 'validateTagElements');
+                $sutResponse = $sut->invoke($targetClass, $xmlElement);
+                expect($sutResponse->isFailure())->toBeTrue();
+            });
+
+            test('Should fail if vIPIDevol is missing', function () {
+                $xmlString = '<ICMSTot><vBC/><vICMS/><vICMSDeson/><vFCPUFDest/><vICMSUFDest/><vICMSUFRemet/><vFCP/><vBCST/><vST/><vFCPST/><vFCPSTRet/><vProd/><vFrete/><vSeg/><vDesc/><vII/><vIPI/><vPIS/><vCOFINS/><vOutro/><vNF/><vTotTrib/></ICMSTot>';
+                $xmlElement = new Element;
+                $xmlElement->parse($xmlString);
+                $targetClass = new IcmsTotalNotaFiscal;
+                $sut = new ReflectionMethod($targetClass, 'validateTagElements');
+                $sutResponse = $sut->invoke($targetClass, $xmlElement);
+                expect($sutResponse->isFailure())->toBeTrue();
+            });
+
+            test('Should fail if vPIS is missing', function () {
+                $xmlString = '<ICMSTot><vBC/><vICMS/><vICMSDeson/><vFCPUFDest/><vICMSUFDest/><vICMSUFRemet/><vFCP/><vBCST/><vST/><vFCPST/><vFCPSTRet/><vProd/><vFrete/><vSeg/><vDesc/><vII/><vIPI/><vIPIDevol/><vCOFINS/><vOutro/><vNF/><vTotTrib/></ICMSTot>';
+                $xmlElement = new Element;
+                $xmlElement->parse($xmlString);
+                $targetClass = new IcmsTotalNotaFiscal;
+                $sut = new ReflectionMethod($targetClass, 'validateTagElements');
+                $sutResponse = $sut->invoke($targetClass, $xmlElement);
+                expect($sutResponse->isFailure())->toBeTrue();
+            });
+
+            test('Should fail if vCOFINS is missing', function () {
+                $xmlString = '<ICMSTot><vBC/><vICMS/><vICMSDeson/><vFCPUFDest/><vICMSUFDest/><vICMSUFRemet/><vFCP/><vBCST/><vST/><vFCPST/><vFCPSTRet/><vProd/><vFrete/><vSeg/><vDesc/><vII/><vIPI/><vIPIDevol/><vPIS/><vOutro/><vNF/><vTotTrib/></ICMSTot>';
+                $xmlElement = new Element;
+                $xmlElement->parse($xmlString);
+                $targetClass = new IcmsTotalNotaFiscal;
+                $sut = new ReflectionMethod($targetClass, 'validateTagElements');
+                $sutResponse = $sut->invoke($targetClass, $xmlElement);
+                expect($sutResponse->isFailure())->toBeTrue();
+            });
+
+            test('Should fail if vOutro is missing', function () {
+                $xmlString = '<ICMSTot><vBC/><vICMS/><vICMSDeson/><vFCPUFDest/><vICMSUFDest/><vICMSUFRemet/><vFCP/><vBCST/><vST/><vFCPST/><vFCPSTRet/><vProd/><vFrete/><vSeg/><vDesc/><vII/><vIPI/><vIPIDevol/><vPIS/><vCOFINS/><vNF/><vTotTrib/></ICMSTot>';
+                $xmlElement = new Element;
+                $xmlElement->parse($xmlString);
+                $targetClass = new IcmsTotalNotaFiscal;
+                $sut = new ReflectionMethod($targetClass, 'validateTagElements');
+                $sutResponse = $sut->invoke($targetClass, $xmlElement);
+                expect($sutResponse->isFailure())->toBeTrue();
+            });
+
+            test('Should fail if vNF is missing', function () {
+                $xmlString = '<ICMSTot><vBC/><vICMS/><vICMSDeson/><vFCPUFDest/><vICMSUFDest/><vICMSUFRemet/><vFCP/><vBCST/><vST/><vFCPST/><vFCPSTRet/><vProd/><vFrete/><vSeg/><vDesc/><vII/><vIPI/><vIPIDevol/><vPIS/><vCOFINS/><vOutro/><vTotTrib/></ICMSTot>';
+                $xmlElement = new Element;
+                $xmlElement->parse($xmlString);
+                $targetClass = new IcmsTotalNotaFiscal;
+                $sut = new ReflectionMethod($targetClass, 'validateTagElements');
+                $sutResponse = $sut->invoke($targetClass, $xmlElement);
+                expect($sutResponse->isFailure())->toBeTrue();
+            });
+
             test('Should fail if an unallowed tag is provided', function () {
                 $xmlString = '<ICMSTot><unallowedTag/><ICMSTot/><ISSQNtot/><retTrib/></ICMSTot>';
                 $xmlElement = new Element;
