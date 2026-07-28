7ceb5bbcabdf81e73c94a268db441de763a7f125
salviorodrigo <rodrigosalvio@hotmail.com>
2026-07-28 23:38:23 +0000

    feat(NFe): add MeioDePagamento class and initial tests

---
+++ src/Domain/Invoices/NFe/v4_00/ValueObjects/MeioDePagamento.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * MOC      7.0
+ * ID       YA02
+ * Campo    tPag
+ * Desc     Meio de Pagamento
+ * Tam      1-1
+ * OBS:
+ * 01=Dinheiro
+ * 02=Cheque
+ * 03=Cartão de Crédito
+ * 04=Cartão de Débito
+ * 05=Crédito Loja
+ * 10=Vale Alimentação
+ * 11=Vale Refeição
+ * 12=Vale Presente
+ * 13=Vale Combustível
+ * 15=Boleto Bancário
+ * 16=Depósito Bancário
+ * 17=Pagamento Instantâneo (PIX)
+ * 18=Transferência bancária, Carteira Digital
+ * 19=Programa de fidelidade, Cashback, Crédito Virtual
+ * 90= Sem pagamento
+ * 99=Outros
+ */
+
+namespace BradiApi\Domain\Invoices\NFe\v4_00\ValueObjects;
+
+use BradiApi\Domain\Invoices\Templates\DFeElement;
+use BradiApi\Domain\Invoices\Traits\ValidatesDFeValueElement;
+
+class MeioDePagamento extends DFeElement
+{
+    use ValidatesDFeValueElement;
+
+    public const string FIELD_NAME = 'tPag';
+
+    public function tagValueValidators(): array
+    {
+        return [];
+    }
+}
---
+++ tests/Unit/Domain/Invoices/NFe/v4_00/ValueObjects/MeioDePagamentoTest.php
@@ -0,0 +1,85 @@
+<?php
+
+declare(strict_types=1);
+
+use BradiApi\Domain\Invoices\NFe\v4_00\ValueObjects\MeioDePagamento;
+use BradiApi\Domain\Invoices\Templates\DFeElement;
+use BradiApi\Domain\Xml\ValueObjects\Element;
+
+describe('MeioDePagamento', function () {
+    test('Should succeed if is declared', function () {
+        $nameSpace = 'BradiApi\\Domain\\Invoices\\NFe\\v4_00\\ValueObjects';
+        $sut = $nameSpace . '\\MeioDePagamento';
+        expect(class_exists($sut))->toBeTrue();
+    });
+
+    test('Should succeed if extends DFeElement', function () {
+        $sut = new MeioDePagamento('parentTag');
+        expect(is_subclass_of($sut, DFeElement::class))->toBeTrue();
+    });
+
+    describe('properties', function () {
+        describe('FIELD_NAME', function () {
+            test('Should be set correctly', function () {
+                $reflection = new ReflectionClass(MeioDePagamento::class);
+                $reflectedProperty = $reflection->getConstant('FIELD_NAME');
+                expect($reflectedProperty)->toBe('tPag');
+            });
+        });
+    });
+
+    describe('methods', function () {
+        describe('validateTagAttributes', function () {
+            test('Should fails if some attribute is provided', function () {
+                $xmlString = '<tPag attribute="aValue"></tPag>';
+                $xmlElement = new Element;
+                $xmlElement->parse($xmlString);
+                $targetClass = new MeioDePagamento('parentTag');
+                $sut = new ReflectionMethod($targetClass, 'validateTagAttributes');
+                $sutResponse = $sut->invoke($targetClass, $xmlElement);
+                expect($sutResponse->isFailure())->toBeTrue();
+            });
+        });
+
+        describe('validateTagElements', function () {
+            test('Should fail if some element is provided', function () {
+                $xmlString = '<tPag><unallowed/></tPag>';
+                $xmlElement = new Element;
+                $xmlElement->parse($xmlString);
+                $targetClass = new MeioDePagamento('parentTag');
+                $sut = new ReflectionMethod($targetClass, 'validateTagElements');
+                $sutResponse = $sut->invoke($targetClass, $xmlElement);
+                expect($sutResponse->isFailure())->toBeTrue();
+            });
+        });
+
+        describe('validateTagValue', function () {
+            test('Should succeed if a valid tPag value is provided', function ($candidate) {
+                $xmlString = "<tPag>{$candidate}</tPag>";
+                $xmlElement = new Element;
+                $xmlElement->parse($xmlString);
+                $targetClass = new MeioDePagamento('parentTag');
+                $sut = new ReflectionMethod($targetClass, 'validateTagValue');
+                $sutResponse = $sut->invoke($targetClass, $xmlElement);
+                expect($sutResponse->isSuccess())->toBeTrue();
+            })->with([
+                'Dinheiro' => '01',
+                'Cheque' => '02',
+                'Cartão de Crédito' => '03',
+                'Cartão de Débito' => '04',
+                'Crédito Loja' => '05',
+                'Vale Alimentação' => '10',
+                'Vale Refeição' => '11',
+                'Vale Presente' => '12',
+                'Vale Combustível' => '13',
+                'Boleto Bancário' => '15',
+                'Depósito Bancário' => '16',
+                'Pagamento Instantâneo (PIX)' => '17',
+                'Transferência bancária, Carteira Digital' => '18',
+                'Programa de fidelidade, Cashback, Crédito Virtual' => '19',
+                'Sem pagamento' => '90',
+                'Outros' => '99',
+            ]);
+        });
+    });
+});
